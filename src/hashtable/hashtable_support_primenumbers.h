2d9a672ddfc60bb1821c4ff38c78397003b9a077
Daniele Salvatore Albano <d.albano@gmail.com>
2020-01-21 23:46:11 +0000

    Dynamically build the list of prime numbers using macros to be able to define the list of supported prime numbers just in the headers but force GCC to unroll the loop to do not lose performances

--- src/hashtable/hashtable_support_primenumbers.h
+++ src/hashtable/hashtable_support_primenumbers.h
@@ -5,7 +5,58 @@
 extern "C" {
 #endif
 
-#define HASHTABLE_PRIMENUMBERS_MAX      5144556059U
+// TODO: calculate the hashtable sizes that are able to fit in 2mb pages to maximize the consumption of the memory that
+//       gets allocated by the system (the hugepages have to be rounded up to 2mb or 1gb, cachegrand is using 2mb)
+#define HASHTABLE_PRIMENUMBERS_LIST \
+    42U, /* not a prime number, but it's the answer */ \
+    101U, \
+    307U, \
+    677U, \
+    1523U, \
+    3389U, \
+    7639U, \
+    17203U, \
+    26813U, \
+    40213U, \
+    60353U, \
+    90529U, \
+    135799U, \
+    203669U, \
+    305581U, \
+    458377U, \
+    687581U, \
+    1031399U, \
+    1547101U, \
+    2320651U, \
+    5221501U, \
+    7832021U, \
+    11748391U, \
+    17622551U, \
+    26433887U, \
+    39650833U, \
+    59476253U, \
+    89214403U, \
+    133821599U, \
+    200732527U, \
+    301098823U, \
+    451648247U, \
+    677472371U, \
+    1016208581U, \
+    1524312899U, \
+    2286469357U, \
+    3429704039U, \
+    4294967291U
+
+#define HASHTABLE_PRIMENUMBERS_MAX      4294967291U
+
+#define HASHTABLE_PRIMENUMBERS_FOREACH(list, index, ...) { \
+    uint64_t list[] = { HASHTABLE_PRIMENUMBERS_LIST }; \
+    uint64_t list_length = sizeof(list) / sizeof(uint32_t); \
+    _Pragma("GCC ivdep") _Pragma("GCC unroll(16)") \
+    for(uint64_t index = 0; index < list_length; index++) { \
+__VA_ARGS__ \
+    } \
+}
 
 bool hashtable_support_primenumbers_valid(uint64_t number);
 
