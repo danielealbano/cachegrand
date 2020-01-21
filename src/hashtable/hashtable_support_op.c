6774471f8d6f3f038ba11d73435cf691d3ff02b9
Daniele Salvatore Albano <d.albano@gmail.com>
2020-01-21 23:44:52 +0000

    Force loops unroll in hashtable_support_op.c

--- src/hashtable/hashtable_support_op.c
+++ src/hashtable/hashtable_support_op.c
@@ -11,6 +11,7 @@
 #include "hashtable_support_index.h"
 #include "hashtable_support_op.h"
 
+#pragma GCC optimization ("unroll-loops")
 // TODO: refactor to merge the functions hashtable_support_op_search_key and
 //       hashtable_support_op_search_key_or_create_new and reorganize the code
 
