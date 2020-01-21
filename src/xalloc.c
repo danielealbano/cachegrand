eee09768c7b5eb3da40f7ffa7c738a13172ca358
Daniele Salvatore Albano <d.albano@gmail.com>
2020-01-21 23:42:06 +0000

    Add support mmap-ing the memory with the THP support

--- src/xalloc.c
+++ src/xalloc.c
@@ -1,9 +1,20 @@
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #include "log.h"
 #include "fatal.h"
 
 static const char* TAG = "xalloc";
+
+size_t xalloc_page_align(size_t size) {
+    long alignment = sysconf(_SC_PAGESIZE);
+
+    size_t hugepages_alignment = alignment; //2 * 1024 * 1024;
+    size = size - (size % hugepages_alignment) + hugepages_alignment;
+
+    return size;
+}
 
 void* xalloc_aligned(size_t alignment, size_t size) {
     void* memptr;
@@ -39,4 +50,35 @@ void* xalloc(size_t size) {
 
 void xfree(void* memptr) {
     free(memptr);
+}
+
+void* xalloc_hugepages(size_t size) {
+    void* memptr;
+
+    size = xalloc_page_align(size);
+
+    memptr = mmap(
+            NULL,
+            size,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS,
+            -1,
+            0);
+
+    if (memptr < 0) {
+        FATAL(TAG, "Unable to allocate the requested memory %d", size);
+    }
+
+    if (madvise(
+            memptr,
+            size,
+            MADV_RANDOM | MADV_HUGEPAGE) == -1) {
+        FATAL(TAG, "Unable to advise the kernel for the data at %p with size %ld", memptr, size);
+    }
+
+    return memptr;
+}
+
+int xfree_hugepages(void* memptr, size_t size) {
+    return munmap(memptr, xalloc_page_align(size));
 }
--- src/xalloc.h
+++ src/xalloc.h
@@ -5,9 +5,12 @@
 extern "C" {
 #endif
 
+size_t xalloc_page_align(size_t size);
 void* xalloc_aligned(size_t alignment, size_t size);
 void* xalloc(size_t size);
 void xfree(void* memptr);
+void* xalloc_hugepages(size_t size);
+int xfree_hugepages(void* memptr, size_t size);
 
 #ifdef __cplusplus
 }
