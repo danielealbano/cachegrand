f4792c5305af71f8da45b044af7bee3bd39eebfd
Daniele Salvatore Albano <d.albano@gmail.com>
2020-02-09 12:46:37 +0300

    Fix export of the C function for C++

--- src/random.h
+++ src/random.h
@@ -1,6 +1,10 @@
 #ifndef CACHEGRAND_RANDOM_H
 #define CACHEGRAND_RANDOM_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct random_init_state {
     uint64_t s;
 };
@@ -15,5 +19,9 @@ typedef struct random_state random_state_t;
 uint64_t random_init_internal_seed(random_init_state_t *state);{
 random_state_t random_init(uint64_t seed);
 uint64_t random_generate(random_state_t *state);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //CACHEGRAND_RANDOM_H
