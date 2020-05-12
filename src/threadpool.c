36276834a5eaec1eb75299e36fab1ecee0c29b45
Daniele Salvatore Albano <d.albano@gmail.com>
2020-05-12 15:54:31 +0100

    Add threadpool skeleton c/h files

---
+++ src/threadpool.c
@@ -0,0 +1,10 @@
+//
+// Created by Daniele Salvatore Albano on 2019-12-16.
+//
+
+#include "threadpool.h"
+
+void threadpool_init()
+{
+    return;
+}
---
+++ src/threadpool.h
@@ -0,0 +1,14 @@
+#ifndef CACHEGRAND_THREADPOOL_H
+#define CACHEGRAND_THREADPOOL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void threadpool_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CACHEGRAND_THREADPOOL_H
