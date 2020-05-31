6722f4018b489c05bbe4bb5bdee6af93dd62fde0
Daniele Salvatore Albano <d.albano@gmail.com>
2020-05-31 22:54:35 +0100

    Drop more code related to the cacheline-based hashtable

--- src/hashtable/hashtable_config.c
+++ src/hashtable/hashtable_config.c
@@ -9,25 +9,6 @@
 #include "hashtable_config.h"
 #include "hashtable_support_primenumbers.h"
 
-// TODO: a BST can be implemented to fetch the cachelines_to_probe to apply looking for the hashtable_size
-//       greater or equal to the primenumber
-void hashtable_config_prefill_cachelines_to_probe_with_defaults(hashtable_config_t* hashtable_config) {
-    memset((void*)hashtable_config->cachelines_to_probe, 0, sizeof(hashtable_config->cachelines_to_probe));
-
-    HASHTABLE_PRIMENUMBERS_FOREACH(primenumbers, primenumbers_index, primenumber, {
-        uint16_t cachelines_to_probe = 0;
-        HASHTABLE_CONFIG_CACHELINES_PRIMENUMBERS_MAP_FOREACH(map, map_index, map_value, {
-            cachelines_to_probe = map_value.cachelines_to_probe;
-            if (map_value.hashtable_size >= primenumber) {
-                break;
-            }
-        })
-
-        hashtable_config->cachelines_to_probe[primenumbers_index].hashtable_size = primenumber;
-        hashtable_config->cachelines_to_probe[primenumbers_index].cachelines_to_probe = cachelines_to_probe;
-    })
-}
-
 hashtable_config_t* hashtable_config_init() {
     hashtable_config_t* hashtable_config = (hashtable_config_t*)xalloc_alloc(sizeof(hashtable_config_t));
 
