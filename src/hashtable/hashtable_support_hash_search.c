30e13c2871e9f83cf29db457cae76ba4c2f4ec0f
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-01 00:55:42 +0100

    Replace avx2 search algorithm implementation with the branchless provided by @chtz ( https://stackoverflow.com/a/62123631/169278 )

--- src/hashtable/hashtable_support_hash_search.c
+++ src/hashtable/hashtable_support_hash_search.c
@@ -1,36 +1,30 @@
 #include <stdint.h>
-#include <string.h>
 #include <immintrin.h>
 
 #include "cpu.h"
 #include "hashtable_support_hash_search.h"
 
+/**
+ * AVX2 branchless hash search by @chtz
+ *
+ * https://stackoverflow.com/a/62123631/169278
+ **/
+__attribute__((noinline)) int8_t hashtable_support_hash_search_avx2(uint32_t hash, volatile uint32_t* hashes) {
 #if defined(__AVX2__)
-int8_t hashtable_support_hash_search_avx2(uint32_t hash, volatile uint32_t* hashes) {
-    uint32_t compacted_result_mask, leading_zeroes;
-    __m256i cmp_vector, ring_vector, result_mask_vector;
-    int8_t found_index = -1;
+    uint32_t compacted_result_mask = 0;
+    __m256i cmp_vector = _mm256_set1_epi32(hash);
 
-    if (hashes[0] == hash) {
-        return 0;
+    // The second load, load from the 6th uint32 to the 14th uint32, _mm256_loadu_si256 always loads 8 x uint32
+    for(uint8_t base_index = 0; base_index < 12; base_index += 6) {
+        __m256i ring_vector = _mm256_loadu_si256((__m256i*) (hashes + base_index));
+
+        __m256i result_mask_vector = _mm256_cmpeq_epi32(ring_vector, cmp_vector);
+        compacted_result_mask |= (uint32_t)_mm256_movemask_ps(_mm256_castsi256_ps(result_mask_vector)) << (base_index);
     }
-
-    cmp_vector = _mm256_set1_epi32(hash);
-    for(uint8_t base_index = 0; base_index < 16; base_index += 8) {
-        ring_vector = _mm256_stream_load_si256((__m256i*) (hashes + base_index));
-
-        result_mask_vector = _mm256_cmpeq_epi32(ring_vector, cmp_vector);
-        compacted_result_mask = _mm256_movemask_epi8(result_mask_vector);
-
-        if (compacted_result_mask != 0) {
-            leading_zeroes = 32 - __builtin_clz(compacted_result_mask);
-            found_index = base_index + (leading_zeroes >> 2u) - 1;
-            break;
-        }
-    }
-
-    return found_index > 13 ? -1 : found_index;
-}
+    int32_t leading_zeros = __lzcnt32(compacted_result_mask);
+    return (31 - leading_zeros);
+#else
+    // do nothing
 #endif
 }
 
