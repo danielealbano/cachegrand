32103acb574d0ca9c21c16dd37adf1abe5744ebe
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-02 20:36:09 +0100

    Add the AVX search algorithm to the auto-selection function

--- src/hashtable/hashtable_support_hash_search.c
+++ src/hashtable/hashtable_support_hash_search.c
@@ -1,5 +1,4 @@
 #include <stdint.h>
-#include <immintrin.h>
 
 #include "cpu.h"
 #include "hashtable_support_hash_search.h"
@@ -12,6 +11,12 @@
 void hashtable_support_hash_search_select_instruction_set() {
     hashtable_support_hash_search = hashtable_support_hash_search_loop;
 
+#if defined(PSNIP_CPU_ARCH_X86_64)
+    HASHTABLE_SUPPORT_HASH_SEARCH_INIT_INSTRUCTION_SET_CHECK(
+            PSNIP_CPU_FEATURE_X86_AVX, hashtable_support_hash_search_avx);
     HASHTABLE_SUPPORT_HASH_SEARCH_INIT_INSTRUCTION_SET_CHECK(
             PSNIP_CPU_FEATURE_X86_AVX2, hashtable_support_hash_search_avx2);
+#elif defined(PSNIP_CPU_ARCH_ARM64)
+
+#endif
 }
--- src/hashtable/hashtable_support_hash_search.h
+++ src/hashtable/hashtable_support_hash_search.h
@@ -8,6 +8,7 @@ extern "C" {
 typedef int8_t (*hashtable_support_hash_search_fp_t)(uint32_t hash, uint32_t* hashes);
 
 extern int8_t hashtable_support_hash_search_avx2(uint32_t hash, uint32_t* hashes);
+extern int8_t hashtable_support_hash_search_avx(uint32_t hash, uint32_t* hashes);
 extern int8_t hashtable_support_hash_search_loop(uint32_t hash, uint32_t* hashes);
 
 void hashtable_support_hash_search_select_instruction_set();
