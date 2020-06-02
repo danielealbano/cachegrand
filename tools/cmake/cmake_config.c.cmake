bf482bfe26839dfe780fe08a780fb58acea478e9
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-02 23:11:45 +0100

    Split the buildstep invoked as custom dependency target in its own cmake script

--- tools/cmake/cmake_config.c.cmake
+++ tools/cmake/cmake_config.c.cmake
@@ -1,37 +1,15 @@
-find_package(Git REQUIRED)
-if(NOT GIT_FOUND)
-    message(FATAL_ERROR "git not found!")
-endif()
+set(CACHEGRAND_CMAKE_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+set(CACHEGRAND_CMAKE_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+set(CACHEGRAND_CMAKE_CONFIG_C_SRC ${CMAKE_CURRENT_BINARY_DIR}/cmake_config.c)
 
-# Fetch the git version ([tag|commitid](-dirty))
-execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --always --dirty
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_VARIABLE CACHEGRAND_VERSION_GIT
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+include(cmake_config.c.buildstep)
 
-# Fetch the build date/time
-execute_process(COMMAND "date" "+%Y-%m-%d"
-        OUTPUT_VARIABLE CACHEGRAND_BUILD_DATE
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND "date" --utc +%H:%M:%S
-        OUTPUT_VARIABLE CACHEGRAND_BUILD_TIME
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-set(CACHEGRAND_BUILD_DATE_TIME "${CACHEGRAND_BUILD_DATE}T${CACHEGRAND_BUILD_TIME}Z")
-
-message(STATUS "Git Version: ${CACHEGRAND_VERSION_GIT}")
-message(STATUS "Build date/time: ${CACHEGRAND_BUILD_DATE_TIME}")
-
-message(STATUS "Updating cmake_config.c")
-configure_file(
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/cmake_config.c.in"
-        "${CMAKE_CURRENT_SOURCE_DIR}/src/cmake_config.c"
-        @ONLY)
-message(STATUS "Updating cmake_config.c -- done")
-
-if(NOT SKIP_CUSTOM_TARGET)
-    add_custom_target(refresh_cmake_config_c
-        COMMAND cmake -DSKIP_CUSTOM_TARGET=1 -P ${CMAKE_CURRENT_SOURCE_DIR}/tools/cmake/cmake_config.c.cmake >/dev/null
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-endif()
+add_custom_target(__internal_refresh_cmake_config_c
+    COMMAND
+        cmake
+            -DCACHEGRAND_ARCH_TARGET=${CACHEGRAND_ARCH_TARGET}
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DCACHEGRAND_CMAKE_SOURCE_DIR="${PROJECT_SOURCE_DIR}"
+            -DCACHEGRAND_CMAKE_BINARY_DIR="${CMAKE_CURRENT_BINARY_DIR}"
+            -P "${CMAKE_CURRENT_SOURCE_DIR}/tools/cmake/cmake_config.c.buildstep.cmake" >/dev/null
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
