0a6e1188c474cbd6e6f35e5571fa12b6731af497
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-06 16:07:52 +0100

    Refactor hashtable data initialization code to support the new data structures

--- src/hashtable/hashtable_data.c
+++ src/hashtable/hashtable_data.c
@@ -19,22 +19,43 @@ hashtable_data_t* hashtable_data_init(hashtable_bucket_count_t buckets_count) {
         return NULL;
     }
 
-    size_t hashes_size = sizeof(hashtable_bucket_hash_t) * buckets_count_real;
-    size_t keys_values_size = sizeof(hashtable_bucket_key_value_t) * buckets_count_real;
-
     hashtable_data_t* hashtable_data = (hashtable_data_t*)xalloc_alloc(sizeof(hashtable_data_t));
 
     hashtable_data->buckets_count = buckets_count;
-    hashtable_data->hashes_size = hashes_size;
-    hashtable_data->keys_values_size = keys_values_size;
-    hashtable_data->hashes = (hashtable_bucket_hash_atomic_t*)xalloc_mmap_alloc(hashes_size);
-    hashtable_data->keys_values = (hashtable_bucket_key_value_t*)xalloc_mmap_alloc(keys_values_size);
+    hashtable_data->buckets_size = sizeof(hashtable_bucket_t) * buckets_count;
+
+    hashtable_data->buckets = (hashtable_bucket_t*)xalloc_mmap_alloc(hashtable_data->buckets_size);
 
     return hashtable_data;
 }
 
-extern inline void hashtable_data_free(volatile hashtable_data_t* hashtable_data) {
-    xalloc_mmap_free((void*)hashtable_data->hashes, hashtable_data->hashes_size);
-    xalloc_mmap_free((void*)hashtable_data->keys_values, hashtable_data->keys_values_size);
+void hashtable_data_free_buckets_chain_rings(volatile hashtable_bucket_t* bucket) {
+    volatile hashtable_bucket_chain_ring_t* chain_ring = bucket->chain_first_ring;
+    while(chain_ring != NULL) {
+        volatile hashtable_bucket_chain_ring_t* next = chain_ring->next_ring;
+
+        xalloc_free((void*)chain_ring);
+
+        chain_ring = next;
+    }
+}
+
+void hashtable_data_free_buckets(hashtable_data_t* hashtable_data) {
+    for(hashtable_bucket_index_t index = 0; index < hashtable_data->buckets_count; index++) {
+        if (&hashtable_data->buckets[index]) {
+            continue;
+        }
+
+        volatile hashtable_bucket_t* bucket = &hashtable_data->buckets[index];
+
+        hashtable_data_free_buckets_chain_rings(bucket);
+
+        xalloc_mmap_free((void*)bucket, sizeof(hashtable_bucket_t));
+    }
+}
+
+void hashtable_data_free(hashtable_data_t* hashtable_data) {
+    hashtable_data_free_buckets(hashtable_data);
+    xalloc_mmap_free((void*)hashtable_data->buckets, hashtable_data->buckets_size);
     xalloc_free((void*)hashtable_data);
 }
