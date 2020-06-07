699fc98038e145a59c3aae07b65f10abe5d59433
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-07 18:39:48 +0100

    Update the get op benchmark of the hashtable to support the new structure and catch failures

--- benches/bench-hashtable-op-get.cpp
+++ benches/bench-hashtable-op-get.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <benchmark/benchmark.h>
 
+#include "xalloc.h"
 #include "hashtable/hashtable.h"
 #include "hashtable/hashtable_config.h"
 #include "hashtable/hashtable_support_index.h"
@@ -11,7 +12,8 @@
 #include "../tests/fixtures-hashtable.h"
 
 #define HASHTABLE_OP_GET_BENCHS_ARGS \
-   Arg(1522)->Arg(135798)->Arg(1031398)
+   Arg(1522U)->Arg(135798U)->Arg(1031398U)->Arg(17622551U)->Arg(89214403U)->Arg(133821599U) \
+   ->Iterations(10000000);
 
 static void hashtable_op_get_notfound(benchmark::State& state) {
     hashtable_config_t* hashtable_config;
@@ -39,6 +41,8 @@ static void hashtable_op_get_found_key_inline(benchmark::State& state) {
     hashtable_config_t* hashtable_config;
     hashtable_t* hashtable;
     hashtable_value_data_t value;
+    bool result;
+    char error_message[150] = {0};
 
     hashtable_config = hashtable_config_init();
     hashtable_config->initial_size = state.range(0);
@@ -46,19 +50,30 @@ static void hashtable_op_get_found_key_inline(benchmark::State& state) {
 
     hashtable = hashtable_init(hashtable_config);
 
-    HASHTABLE_BUCKET_HASH_KEY_VALUE_SET_KEY_INLINE(
-            test_index_1_buckets_count_53,
+    HASHTABLE_BUCKET_NEW_KEY_INLINE(
+            test_key_1_hash % hashtable->ht_current->buckets_count,
             test_key_1_hash,
             test_key_1,
             test_key_1_len,
-            test_value_1)
+            test_value_1);
 
     for (auto _ : state) {
-        hashtable_op_get(
+        benchmark::DoNotOptimize((result = hashtable_op_get(
                 hashtable,
                 test_key_1,
                 test_key_1_len,
-                &value);
+                &value)));
+
+        if (!result) {
+            sprintf(
+                    error_message,
+                    "Unable to get the key <%s> with index <%ld> for the thread <%d>",
+                    test_key_1,
+                    test_key_1_hash % hashtable->ht_current->buckets_size,
+                    state.thread_index);
+            state.SkipWithError(error_message);
+            break;
+        }
     }
 
     hashtable_free(hashtable);
@@ -69,6 +84,8 @@ static void hashtable_op_get_found_key_external(benchmark::State& state) {
     hashtable_config_t* hashtable_config;
     hashtable_t* hashtable;
     hashtable_value_data_t value;
+    bool result;
+    char error_message[150] = {0};
 
     hashtable_config = hashtable_config_init();
     hashtable_config->initial_size = state.range(0);
@@ -76,19 +93,30 @@ static void hashtable_op_get_found_key_external(benchmark::State& state) {
 
     hashtable = hashtable_init(hashtable_config);
 
-    HASHTABLE_BUCKET_HASH_KEY_VALUE_SET_KEY_EXTERNAL(
-            test_index_1_buckets_count_53,
+    HASHTABLE_BUCKET_NEW_KEY_EXTERNAL(
+            test_key_1_hash % hashtable->ht_current->buckets_count,
             test_key_1_hash,
             test_key_1,
             test_key_1_len,
             test_value_1);
 
     for (auto _ : state) {
-        hashtable_op_get(
+        benchmark::DoNotOptimize((result = hashtable_op_get(
                 hashtable,
                 test_key_1,
                 test_key_1_len,
-                &value);
+                &value)));
+
+        if (!result) {
+            sprintf(
+                    error_message,
+                    "Unable to get the key <%s> with index <%ld> for the thread <%d>",
+                    test_key_1,
+                    test_key_1_hash % hashtable->ht_current->buckets_size,
+                    state.thread_index);
+            state.SkipWithError(error_message);
+            break;
+        }
     }
 
     hashtable_free(hashtable);
