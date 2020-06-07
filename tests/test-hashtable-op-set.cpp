1cf4cbc7595b3dad54b9ae49af8b4974a7546150
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-08 00:24:34 +0100

    Update the hashtable_op_set tests

--- tests/test-hashtable-op-set.cpp
+++ tests/test-hashtable-op-set.cpp
@@ -14,43 +14,43 @@ TEST_CASE("hashtable_op_set.c", "[hashtable][hashtable_op][hashtable_op_set]") {
     SECTION("hashtable_op_set") {
         SECTION("set 1 bucket") {
             HASHTABLE(buckets_initial_count_5, false, {
-                hashtable_bucket_index_t index_neighborhood_begin, index_neighborhood_end;
-
-                hashtable_support_index_calculate_neighborhood_from_hash(
-                        hashtable->ht_current->buckets_count,
-                        test_key_1_hash,
-                        hashtable->ht_current->cachelines_to_probe,
-                        &index_neighborhood_begin,
-                        &index_neighborhood_end);
-
                 REQUIRE(hashtable_op_set(
                         hashtable,
                         test_key_1,
                         test_key_1_len,
                         test_value_1));
 
-                REQUIRE(hashtable->ht_current->hashes[index_neighborhood_begin] == test_key_1_hash);
-                REQUIRE(hashtable->ht_current->keys_values[index_neighborhood_begin].flags ==
+                hashtable_bucket_index_t bucket_index = hashtable_support_index_from_hash(
+                        hashtable->ht_current->buckets_count,
+                        test_key_1_hash);
+
+                volatile hashtable_bucket_t* bucket =
+                        &hashtable->ht_current->buckets[bucket_index];
+
+                volatile hashtable_bucket_chain_ring_t* chain_ring = bucket->chain_first_ring;
+
+                // Check if the write lock has been released
+                REQUIRE(bucket->write_lock == 0);
+
+                // Check if the first slot of the chain ring contains the correct key/value
+                REQUIRE(chain_ring->half_hashes[0] == test_key_1_hash_half);
+                REQUIRE(chain_ring->keys_values[0].flags ==
                         (HASHTABLE_BUCKET_KEY_VALUE_FLAG_FILLED | HASHTABLE_BUCKET_KEY_VALUE_FLAG_KEY_INLINE));
                 REQUIRE(strncmp(
-                        (char*)hashtable->ht_current->keys_values[index_neighborhood_begin].inline_key.data,
+                        (char*)chain_ring->keys_values[0].inline_key.data,
                         test_key_1,
                         test_key_1_len) == 0);
-                REQUIRE(hashtable->ht_current->keys_values[index_neighborhood_begin].data == test_value_1);
+                REQUIRE(chain_ring->keys_values[0].data == test_value_1);
+
+                // Check if the subsequent element has been affected by the changes
+                REQUIRE(chain_ring->half_hashes[1] == 0);
+                REQUIRE(chain_ring->keys_values[1].flags == 0);
+                REQUIRE(chain_ring->keys_values[1].inline_key.data[0] == 0);
             })
         }
 
-        SECTION("set and update 1 bucket") {
+        SECTION("set and update 1 slot") {
             HASHTABLE(buckets_initial_count_5, false, {
-                hashtable_bucket_index_t index_neighborhood_begin, index_neighborhood_end;
-
-                hashtable_support_index_calculate_neighborhood_from_hash(
-                        hashtable->ht_current->buckets_count,
-                        test_key_1_hash,
-                        hashtable->ht_current->cachelines_to_probe,
-                        &index_neighborhood_begin,
-                        &index_neighborhood_end);
-
                 REQUIRE(hashtable_op_set(
                         hashtable,
                         test_key_1,
@@ -63,40 +63,34 @@ TEST_CASE("hashtable_op_set.c", "[hashtable][hashtable_op][hashtable_op_set]") {
                         test_key_1_len,
                         test_value_2));
 
-                REQUIRE(hashtable->ht_current->hashes[index_neighborhood_begin] == test_key_1_hash);
-                REQUIRE(hashtable->ht_current->keys_values[index_neighborhood_begin].flags ==
+                hashtable_bucket_index_t bucket_index = hashtable_support_index_from_hash(
+                        hashtable->ht_current->buckets_count,
+                        test_key_1_hash);
+
+                volatile hashtable_bucket_t* bucket =
+                        &hashtable->ht_current->buckets[bucket_index];
+
+                volatile hashtable_bucket_chain_ring_t* chain_ring = bucket->chain_first_ring;
+
+                // Check if the first slot of the chain ring contains the correct key/value
+                REQUIRE(chain_ring->half_hashes[0] == test_key_1_hash_half);
+                REQUIRE(chain_ring->keys_values[0].flags ==
                         (HASHTABLE_BUCKET_KEY_VALUE_FLAG_FILLED | HASHTABLE_BUCKET_KEY_VALUE_FLAG_KEY_INLINE));
                 REQUIRE(strncmp(
-                        (char*)hashtable->ht_current->keys_values[index_neighborhood_begin].inline_key.data,
+                        (char*)chain_ring->keys_values[0].inline_key.data,
                         test_key_1,
                         test_key_1_len) == 0);
-                REQUIRE(hashtable->ht_current->keys_values[index_neighborhood_begin].data == test_value_2);
+                REQUIRE(chain_ring->keys_values[0].data == test_value_2);
+
+                // Check if the subsequent element has been affected by the changes
+                REQUIRE(chain_ring->half_hashes[1] == 0);
+                REQUIRE(chain_ring->keys_values[1].flags == 0);
+                REQUIRE(chain_ring->keys_values[1].inline_key.data[0] == 0);
             })
         }
 
-        SECTION("set 2 buckets") {
+        SECTION("set 2 slots") {
             HASHTABLE(buckets_initial_count_5, false, {
-                hashtable_bucket_index_t index_1_neighborhood_begin, index_1_neighborhood_end;
-                hashtable_bucket_index_t index_2_neighborhood_begin, index_2_neighborhood_end;
-
-                hashtable_support_index_calculate_neighborhood_from_hash(
-                        hashtable->ht_current->buckets_count,
-                        test_key_1_hash,
-                        hashtable->ht_current->cachelines_to_probe,
-                        &index_1_neighborhood_begin,
-                        &index_1_neighborhood_end);
-
-                hashtable_support_index_calculate_neighborhood_from_hash(
-                        hashtable->ht_current->buckets_count,
-                        test_key_2_hash,
-                        hashtable->ht_current->cachelines_to_probe,
-                        &index_2_neighborhood_begin,
-                        &index_2_neighborhood_end);
-
-                if (index_1_neighborhood_begin == index_2_neighborhood_begin) {
-                    index_2_neighborhood_begin++;
-                }
-
                 REQUIRE(hashtable_op_set(
                         hashtable,
                         test_key_1,
@@ -109,80 +103,40 @@ TEST_CASE("hashtable_op_set.c", "[hashtable][hashtable_op][hashtable_op_set]") {
                         test_key_2_len,
                         test_value_2));
 
-                REQUIRE(hashtable->ht_current->hashes[index_1_neighborhood_begin] == test_key_1_hash);
-                REQUIRE(hashtable->ht_current->keys_values[index_1_neighborhood_begin].flags ==
+                hashtable_bucket_index_t bucket_index1 = hashtable_support_index_from_hash(
+                        hashtable->ht_current->buckets_count,
+                        test_key_1_hash);
+
+                volatile hashtable_bucket_chain_ring_t* chain_ring1 =
+                        hashtable->ht_current->buckets[bucket_index1].chain_first_ring;
+
+                hashtable_bucket_index_t bucket_index2 = hashtable_support_index_from_hash(
+                        hashtable->ht_current->buckets_count,
+                        test_key_1_hash);
+
+                volatile hashtable_bucket_chain_ring_t* chain_ring2 =
+                        hashtable->ht_current->buckets[bucket_index2].chain_first_ring;
+
+                // Check if the first slot of the chain ring contains the correct key/value
+                REQUIRE(chain_ring1->half_hashes[0] == test_key_1_hash_half);
+                REQUIRE(chain_ring1->keys_values[0].flags ==
                         (HASHTABLE_BUCKET_KEY_VALUE_FLAG_FILLED | HASHTABLE_BUCKET_KEY_VALUE_FLAG_KEY_INLINE));
                 REQUIRE(strncmp(
-                        (char*)hashtable->ht_current->keys_values[index_1_neighborhood_begin].inline_key.data,
+                        (char*)chain_ring1->keys_values[0].inline_key.data,
                         test_key_1,
                         test_key_1_len) == 0);
-                REQUIRE(hashtable->ht_current->keys_values[index_1_neighborhood_begin].data == test_value_1);
+                REQUIRE(chain_ring1->keys_values[0].data == test_value_1);
 
-                REQUIRE(hashtable->ht_current->hashes[index_2_neighborhood_begin] == test_key_2_hash);
-                REQUIRE(hashtable->ht_current->keys_values[index_2_neighborhood_begin].flags ==
+                // Check if the first slot of the chain ring contains the correct key/value
+                REQUIRE(chain_ring2->half_hashes[0] == test_key_2_hash_half);
+                REQUIRE(chain_ring2->keys_values[0].flags ==
                         (HASHTABLE_BUCKET_KEY_VALUE_FLAG_FILLED | HASHTABLE_BUCKET_KEY_VALUE_FLAG_KEY_INLINE));
                 REQUIRE(strncmp(
-                        (char*)hashtable->ht_current->keys_values[index_2_neighborhood_begin].inline_key.data,
+                        (char*)chain_ring2->keys_values[0].inline_key.data,
                         test_key_2,
                         test_key_2_len) == 0);
-                REQUIRE(hashtable->ht_current->keys_values[index_2_neighborhood_begin].data == test_value_2);
+                REQUIRE(chain_ring2->keys_values[0].data == test_value_2);
             })
         }
-
-        SECTION("set delete set 1 bucket") {
-            HASHTABLE(buckets_initial_count_5, false, {
-                hashtable_bucket_index_t index_1_neighborhood_begin, index_1_neighborhood_end;
-                hashtable_bucket_index_t index_2_neighborhood_begin, index_2_neighborhood_end;
-
-                hashtable_support_index_calculate_neighborhood_from_hash(
-                        hashtable->ht_current->buckets_count,
-                        test_key_1_hash,
-                        hashtable->ht_current->cachelines_to_probe,
-                        &index_1_neighborhood_begin,
-                        &index_1_neighborhood_end);
-
-                hashtable_support_index_calculate_neighborhood_from_hash(
-                        hashtable->ht_current->buckets_count,
-                        test_key_2_hash,
-                        hashtable->ht_current->cachelines_to_probe,
-                        &index_2_neighborhood_begin,
-                        &index_2_neighborhood_end);
-
-                if (index_1_neighborhood_begin == index_2_neighborhood_begin) {
-                    index_2_neighborhood_begin++;
-                }
-
-                REQUIRE(hashtable_op_set(
-                        hashtable,
-                        test_key_1,
-                        test_key_1_len,
-                        test_value_1));
-
-                REQUIRE(hashtable_op_set(
-                        hashtable,
-                        test_key_2,
-                        test_key_2_len,
-                        test_value_2));
-
-                REQUIRE(hashtable->ht_current->hashes[index_1_neighborhood_begin] == test_key_1_hash);
-                REQUIRE(hashtable->ht_current->keys_values[index_1_neighborhood_begin].flags ==
-                        (HASHTABLE_BUCKET_KEY_VALUE_FLAG_FILLED | HASHTABLE_BUCKET_KEY_VALUE_FLAG_KEY_INLINE));
-                REQUIRE(strncmp(
-                        (char*)hashtable->ht_current->keys_values[index_1_neighborhood_begin].inline_key.data,
-                        test_key_1,
-                        test_key_1_len) == 0);
-                REQUIRE(hashtable->ht_current->keys_values[index_1_neighborhood_begin].data == test_value_1);
-
-                REQUIRE(hashtable->ht_current->hashes[index_2_neighborhood_begin] == test_key_2_hash);
-                REQUIRE(hashtable->ht_current->keys_values[index_2_neighborhood_begin].flags ==
-                        (HASHTABLE_BUCKET_KEY_VALUE_FLAG_FILLED | HASHTABLE_BUCKET_KEY_VALUE_FLAG_KEY_INLINE));
-                REQUIRE(strncmp(
-                        (char*)hashtable->ht_current->keys_values[index_2_neighborhood_begin].inline_key.data,
-                        test_key_2,
-                        test_key_2_len) == 0);
-                REQUIRE(hashtable->ht_current->keys_values[index_2_neighborhood_begin].data == test_value_2);
-            })
-        }
-
     }
 }
