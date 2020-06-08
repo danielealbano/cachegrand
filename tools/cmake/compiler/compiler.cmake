5248f3ad7b98e3d7b03aaeb96fc2b5bd916e3b86
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-08 18:27:40 +0100

    Enforce -O3 compilation flag when building for non-debug and fix style

--- tools/cmake/compiler/compiler.cmake
+++ tools/cmake/compiler/compiler.cmake
@@ -3,9 +3,10 @@ set(CMAKE_CXX_STANDARD 11)
 
 if (CMAKE_BUILD_TYPE MATCHES Debug)
     add_definitions(-DDEBUG=1)
-    add_compile_options (-g -O0 -fno-inline)
+    add_compile_options(-g -O0 -fno-inline)
 else()
     add_definitions(-DNDEBUG=1)
+    add_compile_options(-O3)
 endif()
 
 if(CMAKE_CROSSCOMPILING)
