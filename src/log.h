584799e9aed520626539adc2f8459037262eb57d
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-13 16:17:25 +0100

    Add the ability to log debug internal messages that print out extra verbose infos (like file path, relative to the build, current function and current line number). Add also the relative LOG_DI macro function for simple invocation

--- src/log.h
+++ src/log.h
@@ -22,6 +22,13 @@ extern "C" {
 #define LOG_D(tag, message, ...) \
     log_message(tag, LOG_LEVEL_DEBUG, message, __VA_ARGS__)
 
+#if DEBUG == 1
+#define LOG_DI(...) \
+    log_message_debug(CACHEGRAND_SRC_PATH, __func__, __LINE__, __VA_ARGS__)
+#else
+#define LOG_DI(...) /* Internal debug logs disabled */
+#endif // DEBUG == 1
+
 enum log_level {
     LOG_LEVEL_ERROR,
     LOG_LEVEL_RECOVERABLE,
@@ -29,6 +36,7 @@ enum log_level {
     LOG_LEVEL_INFO,
     LOG_LEVEL_VERBOSE,
     LOG_LEVEL_DEBUG,
+    LOG_LEVEL_DEBUG_INTERNALS,
 };
 typedef enum log_level log_level_t;
 
@@ -36,6 +44,7 @@ const char* log_level_to_string(log_level_t level);
 char* log_message_timestamp(char* t_str);
 void log_message_internal(const char* tag, log_level_t level, const char* message, va_list args);
 void log_set_log_level(log_level_t level);
+void log_message_debug(const char* src_path, const char* src_func, int src_line, const char* message, ...);
 void log_message(const char* tag, log_level_t level, const char* message, ...);
 
 #ifdef __cplusplus
