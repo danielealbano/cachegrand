cf9700271c3bd5411c175d4541211a9501dbabe6
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-14 22:08:52 +0100

    Drop the hardcoded test fixture to use the same chunk, the tests will relay on test_same_hash_mod_fixtures_generate to generate them at runtime

--- tests/fixtures-hashtable.h
+++ tests/fixtures-hashtable.h
@@ -18,39 +18,8 @@ uint64_t buckets_count_42 = 42;
 uint64_t buckets_count_101 = 101;
 uint64_t buckets_count_307 = 307;
 
-hashtable_bucket_hash_t test_hash_zero = 0;
-
-typedef struct test_key_same_bucket test_key_same_bucket_t;
-struct test_key_same_bucket {
-    const char* key;
-    hashtable_key_size_t key_len;
-    hashtable_bucket_hash_t key_hash;
-    hashtable_bucket_hash_half_t key_hash_half;
-};
-
-test_key_same_bucket_t test_key_1_same_bucket[] = {
-    { "test key 1_same_bucket_42_0036", 30, 0x3a1a6793c5984390, 0x3a1a6793 }, // match n. 1
-    { "test key 1_same_bucket_42_0092", 30, 0x03887e20e6eadae0, 0x03887e20 }, // match n. 2
-    { "test key 1_same_bucket_42_0118", 30, 0x1c51dba0c77bbe38, 0x1c51dba0 }, // match n. 3
-    { "test key 1_same_bucket_42_0197", 30, 0xa369dfa4e169ab2c, 0xa369dfa4 }, // match n. 4
-    { "test key 1_same_bucket_42_0201", 30, 0x57e289ab7575a73a, 0x57e289ab }, // match n. 5
-    { "test key 1_same_bucket_42_0207", 30, 0xdbae2432f8ed753e, 0xdbae2432 }, // match n. 6
-    { "test key 1_same_bucket_42_0218", 30, 0x0c0ed27de374bc7a, 0x0c0ed27d }, // match n. 7
-    { "test key 1_same_bucket_42_0341", 30, 0x91a0132bf462567e, 0x91a0132b }, // match n. 8
-    { "test key 1_same_bucket_42_0349", 30, 0xe56c946e6b9813b4, 0xe56c946e }, // match n. 9
-    { "test key 1_same_bucket_42_0398", 30, 0xe41de88bd413c22a, 0xe41de88b }, // match n. 10
-    { "test key 1_same_bucket_42_0429", 30, 0x4aee1521c64a17e0, 0x4aee1521 }, // match n. 11
-    { "test key 1_same_bucket_42_0470", 30, 0x3277baf71196d346, 0x3277baf7 }, // match n. 12
-    { "test key 1_same_bucket_42_0530", 30, 0xe66714bdf2aafcb4, 0xe66714bd }, // match n. 13
-    { "test key 1_same_bucket_42_0556", 30, 0xebac069200629f06, 0xebac0692 }, // match n. 14
-    { "test key 1_same_bucket_42_0557", 30, 0xe494be9a586d6d7e, 0xe494be9a }, // match n. 15
-    { "test key 1_same_bucket_42_0561", 30, 0xcfc203062c2954c0, 0xcfc20306 }, // match n. 16
-    { "test key 1_same_bucket_42_0579", 30, 0x288a94b8695698fe, 0x288a94b8 }, // match n. 17
-    { "test key 1_same_bucket_42_0652", 30, 0xba123c897ad07e2e, 0xba123c89 }, // match n. 18
-    { "test key 1_same_bucket_42_0663", 30, 0x390f6330d912acca, 0x390f6330 }, // match n. 19
-    { "test key 1_same_bucket_42_0669", 30, 0xfc14fa325168f272, 0xfc14fa32 }, // match n. 20
-    { nullptr, 0, 0, 0 }
-};
+char test_key_same_bucket_key_prefix_external[] = "same_bucket_key_not_inline_";
+char test_key_same_bucket_key_prefix_inline[] = "sb_key_inline_";
 
 char test_key_1[] = "test key 1";
 hashtable_key_size_t test_key_1_len = 10;
