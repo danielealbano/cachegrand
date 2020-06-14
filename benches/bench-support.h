285025149e6961e71ae0d3e161d1445b16bdf076
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-14 22:12:06 +0100

    Update the bench-support.h header to match the functionalities now offered in bench-support.c

--- benches/bench-support.h
+++ benches/bench-support.h
@@ -5,8 +5,22 @@
 extern "C" {
 #endif
 
-bool check_if_too_many_threads_per_core(int threads, int max_per_core);
-void set_thread_affinity(int thread_index);
+#define BENCHES_MAX_THREADS_PER_CORE        4
+
+void bench_support_collect_hashtable_stats_and_update_state(
+    benchmark::State& state,
+    hashtable_t* hashtable);
+
+void bench_support_collect_hashtable_stats(
+        hashtable_t* hashtable,
+        uint64_t* return_used_buckets,
+        double* return_load_factor_buckets,
+        double* return_used_avg_bucket_slots,
+        uint64_t* return_used_max_bucket_slots);
+
+bool bench_support_check_if_too_many_threads_per_core(
+        int threads,
+        int max_per_core);
 
 #ifdef __cplusplus
 }
