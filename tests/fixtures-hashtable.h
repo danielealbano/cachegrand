05a7ecbe22eab9304235cc8d42a38dd1ef55e880
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-18 22:12:10 +0100

    Fix how keys for the hashtable are created in the tests fixtures

--- tests/fixtures-hashtable.h
+++ tests/fixtures-hashtable.h
@@ -80,6 +80,7 @@ hashtable_bucket_index_t test_index_2_buckets_count_42 = test_key_2_hash % bucke
         HASHTABLE_KEY_VALUE_FLAG_FILLED | HASHTABLE_KEY_VALUE_FLAG_KEY_INLINE; \
     strncpy((char*)&HASHTABLE_KEYS_VALUES(chunk_index, chunk_slot_index).inline_key.data, key, HASHTABLE_KEY_INLINE_MAX_LENGTH);
 
+#if CACHEGRAND_HASHTABLE_KEY_CHECK_FULL == 1
 #define HASHTABLE_SET_KEY_EXTERNAL_BY_INDEX(chunk_index, chunk_slot_index, hash, key, key_size, value) \
     HASHTABLE_SET_INDEX_SHARED(chunk_index, chunk_slot_index, hash, value); \
     HASHTABLE_KEYS_VALUES(chunk_index, chunk_slot_index).flags = \
@@ -87,10 +88,16 @@ hashtable_bucket_index_t test_index_2_buckets_count_42 = test_key_2_hash % bucke
     HASHTABLE_KEYS_VALUES(chunk_index, chunk_slot_index).external_key.data = \
         key; \
     HASHTABLE_KEYS_VALUES(chunk_index, chunk_slot_index).external_key.size = \
-        key_size; \
+        key_size;
+#else
+#define HASHTABLE_SET_KEY_EXTERNAL_BY_INDEX(chunk_index, chunk_slot_index, hash, key, key_size, value) \
+    HASHTABLE_SET_INDEX_SHARED(chunk_index, chunk_slot_index, hash, value); \
+    HASHTABLE_KEYS_VALUES(chunk_index, chunk_slot_index).flags = \
+        HASHTABLE_KEY_VALUE_FLAG_FILLED; \
     HASHTABLE_KEYS_VALUES(chunk_index, chunk_slot_index).prefix_key.size = \
         key_size; \
     strncpy((char*)&HASHTABLE_KEYS_VALUES(chunk_index, chunk_slot_index).prefix_key.data, key, HASHTABLE_KEY_PREFIX_SIZE);
+#endif
 
 #ifdef __cplusplus
 }
