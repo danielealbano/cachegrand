fcdb7a88c6b6030dabad0d1216933221d918ff98
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-18 22:41:41 +0100

    Update the warning when the benchmark is started about the duration of the bench

--- benches/main.cpp
+++ benches/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv) {
     fprintf(stdout, "Be aware, benchmarking is configured to:\n");
     fprintf(stdout, "- spin up 4 threads per core and pin the threads\n");
     fprintf(stdout, "- consume up to 120GB of memory when testing the 2bln key hashtable size\n");
+    fprintf(stdout, "- the keys used for the benches are regenerated at every run, it's very time consuming with the big hashtables\n");
     fprintf(stdout, "\n");
 
     ::benchmark::Initialize(&argc, argv);
