cd91dbbca526768249c361068f8860ca18d77a48
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-18 22:24:55 +0100

    Add a warning message about the expectation of the benchmark

--- benches/main.cpp
+++ benches/main.cpp
@@ -5,6 +5,11 @@
 int main(int argc, char** argv) {
     signal_handler_sigsegv_init();
 
+    fprintf(stdout, "Be aware, benchmarking is configured to:\n");
+    fprintf(stdout, "- spin up 4 threads per core and pin the threads\n");
+    fprintf(stdout, "- consume up to 120GB of memory when testing the 2bln key hashtable size\n");
+    fprintf(stdout, "\n");
+
     ::benchmark::Initialize(&argc, argv);
     if (::benchmark::ReportUnrecognizedArguments(argc, argv)) {
         return 1;
