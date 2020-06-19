5dfbf8c80e7412d21460d514280dd9cdf25f877b
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-19 22:32:46 +0100

    Update the code to use the pre-generated keys but ensure that the bench has enough of them to proceed, there is no testing for the benches

--- benches/bench-hashtable-op-set.cpp
+++ benches/bench-hashtable-op-set.cpp
@@ -17,27 +17,27 @@
 
 #include "bench-support.h"
 
-#define SET_BENCH_ARGS_HT_SIZE_AND_KEYS(keys_gen_func_name) \
-    Args({0x0000FFFFu, 75, keys_gen_func_name})-> \
-    Args({0x000FFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x001FFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x007FFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x00FFFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x01FFFFFFu, 50, keys_gen_func_name})-> \
-    Args({0x01FFFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x07FFFFFFu, 50, keys_gen_func_name})-> \
-    Args({0x07FFFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x0FFFFFFFu, 50, keys_gen_func_name})-> \
-    Args({0x0FFFFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x1FFFFFFFu, 50, keys_gen_func_name})-> \
-    Args({0x1FFFFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x3FFFFFFFu, 50, keys_gen_func_name})-> \
-    Args({0x3FFFFFFFu, 75, keys_gen_func_name})-> \
-    Args({0x7FFFFFFFu, 50, keys_gen_func_name})-> \
-    Args({0x7FFFFFFFu, 75, keys_gen_func_name})
 #define KEYSET_MAX_SIZE             (double)0x7FFFFFFFu * (double)0.75
 #define KEYSET_GENERATOR_METHOD     TEST_SUPPORT_RANDOM_KEYS_GEN_FUNC_RANDOM_STR_RANDOM_LENGTH
 
+#define SET_BENCH_ARGS_HT_SIZE_AND_KEYS() \
+    Args({0x0000FFFFu, 75})-> \
+    Args({0x000FFFFFu, 75})-> \
+    Args({0x001FFFFFu, 75})-> \
+    Args({0x007FFFFFu, 75})-> \
+    Args({0x00FFFFFFu, 75})-> \
+    Args({0x01FFFFFFu, 50})-> \
+    Args({0x01FFFFFFu, 75})-> \
+    Args({0x07FFFFFFu, 50})-> \
+    Args({0x07FFFFFFu, 75})-> \
+    Args({0x0FFFFFFFu, 50})-> \
+    Args({0x0FFFFFFFu, 75})-> \
+    Args({0x1FFFFFFFu, 50})-> \
+    Args({0x1FFFFFFFu, 75})-> \
+    Args({0x3FFFFFFFu, 50})-> \
+    Args({0x3FFFFFFFu, 75})-> \
+    Args({0x7FFFFFFFu, 50})-> \
+    Args({0x7FFFFFFFu, 75})
 
 #define SET_BENCH_THREADS \
     Threads(1)-> \
@@ -55,9 +55,9 @@
     Repetitions(10)->\
     DisplayAggregatesOnly(true)
 
-#define CONFIGURE_BENCH_MT_HT_SIZE_AND_KEYS(keys_gen_func_name) \
+#define CONFIGURE_BENCH_MT_HT_SIZE_AND_KEYS() \
     UseRealTime()-> \
-    SET_BENCH_ARGS_HT_SIZE_AND_KEYS(keys_gen_func_name)-> \
+    SET_BENCH_ARGS_HT_SIZE_AND_KEYS()-> \
     SET_BENCH_ITERATIONS-> \
     SET_BENCH_THREADS
 
@@ -84,13 +84,9 @@ static void hashtable_op_set_keyset_cleanup_notatest(benchmark::State& state) {
 
 static void hashtable_op_set_new(benchmark::State& state) {
     static hashtable_t* hashtable;
-    static char* keys;
-    double requested_load_factor;
-    static uint64_t keys_count = 0;
+    static uint64_t requested_keyset_size;
+    bool result;
     char error_message[150] = {0};
-    static uint8_t keys_generator_method = UINT8_MAX;
-    uint64_t hashtable_initial_size = state.range(0);
-    uint8_t requested_load_factor_perc = state.range(1);
 
     if (bench_support_check_if_too_many_threads_per_core(state.threads, BENCHES_MAX_THREADS_PER_CORE)) {
         sprintf(error_message, "Too many threads per core, max allowed <%d>", BENCHES_MAX_THREADS_PER_CORE);
@@ -99,39 +95,36 @@ static void hashtable_op_set_new(benchmark::State& state) {
     }
 
     if (state.thread_index == 0) {
-        hashtable = test_support_init_hashtable(hashtable_initial_size);
-        requested_load_factor = (double)requested_load_factor_perc / 100;
-        uint64_t new_keys_count = (double)hashtable->ht_current->buckets_count * requested_load_factor;
-        if (new_keys_count != keys_count || keys_generator_method != state.range(2)) {
-            if (keys_count > 0) {
-                test_support_free_keys(keys, keys_count);
-            }
+        hashtable = test_support_init_hashtable(state.range(0));
 
-            keys_generator_method = state.range(2);
-
-            LOG_DI("keys_count changed, from <%lu> to <%lu>, regenerating keys with generator <%d>",
-                    keys_count,
-                    new_keys_count,
-                    keys_generator_method);
-
-            keys_count = new_keys_count;
-            keys = test_support_init_keys(keys_count, keys_generator_method);
+        double requested_load_factor = (double)state.range(1) / 100;
+        requested_keyset_size = (double)hashtable->ht_current->buckets_count * requested_load_factor;
+        if (requested_keyset_size > keyset_size) {
+            sprintf(
+                    error_message,
+                    "The requested keyset size of <%lu> is greater than the one available of <%lu>, can't continue",
+                    requested_keyset_size,
+                    keyset_size);
+            state.SkipWithError(error_message);
+            return;
         }
+
+        // Flush the cpu DCACHE related to the portion of the keyset used by the bench before starting the test
+        test_support_flush_data_cache(keyset, TEST_SUPPORT_RANDOM_KEYS_MAX_LENGTH_WITH_NULL * requested_keyset_size);
     }
 
     test_support_set_thread_affinity(state.thread_index);
 
     for (auto _ : state) {
-        for(long int i = state.thread_index; i < keys_count; i += state.threads) {
-            uint64_t keys_offset = TEST_SUPPORT_RANDOM_KEYS_MAX_LENGTH_WITH_NULL * i;
-            char* key = keys + keys_offset;
+        for(long int i = state.thread_index; i < requested_keyset_size; i += state.threads) {
+            uint64_t keyset_offset = TEST_SUPPORT_RANDOM_KEYS_MAX_LENGTH_WITH_NULL * i;
+            char* key = keyset + keyset_offset;
 
-
-            bool result = hashtable_op_set(
+            benchmark::DoNotOptimize((result = hashtable_op_set(
                     hashtable,
                     key,
                     strlen(key),
-                    i);
+                    i)));
 
             if (!result) {
                 sprintf(
@@ -171,13 +164,9 @@ static void hashtable_op_set_new(benchmark::State& state) {
 
 static void hashtable_op_set_update(benchmark::State& state) {
     static hashtable_t* hashtable;
-    static char* keys;
-    double requested_load_factor;
-    static uint64_t keys_count = 0;
+    static uint64_t requested_keyset_size;
+    bool result;
     char error_message[150] = {0};
-    static uint8_t keys_generator_method = UINT8_MAX;
-    uint64_t hashtable_initial_size = state.range(0);
-    uint8_t requested_load_factor_perc = state.range(1);
 
     if (bench_support_check_if_too_many_threads_per_core(state.threads, BENCHES_MAX_THREADS_PER_CORE)) {
         sprintf(error_message, "Too many threads per core, max allowed <%d>", BENCHES_MAX_THREADS_PER_CORE);
@@ -186,48 +175,45 @@ static void hashtable_op_set_update(benchmark::State& state) {
     }
 
     if (state.thread_index == 0) {
-        hashtable = test_support_init_hashtable(hashtable_initial_size);
-        requested_load_factor = (double)requested_load_factor_perc / 100;
-        uint64_t new_keys_count = (double)hashtable->ht_current->buckets_count * requested_load_factor;
-        if (new_keys_count != keys_count || keys_generator_method != state.range(2)) {
-            if (keys_count > 0) {
-                test_support_free_keys(keys, keys_count);
-            }
+        hashtable = test_support_init_hashtable(state.range(0));
 
-            keys_generator_method = state.range(2);
-
-            LOG_DI("keys_count changed, from <%lu> to <%lu>, regenerating keys with generator <%d>",
-                   keys_count,
-                   new_keys_count,
-                   keys_generator_method);
-
-            keys_count = new_keys_count;
-            keys = test_support_init_keys(keys_count, keys_generator_method);
-            bool result = test_support_hashtable_prefill(hashtable, keys, test_value_1, state.range(1));
-
-            if (!result) {
-                hashtable_free(hashtable);
-                test_support_free_keys(keys, keys_count);
-
-                sprintf(error_message, "Unable to prefill the hashtable with <%lu> keys", keys_count);
-                state.SkipWithError(error_message);
-                return;
-            }
+        double requested_load_factor = (double)state.range(1) / 100;
+        requested_keyset_size = (double)hashtable->ht_current->buckets_count * requested_load_factor;
+        if (requested_keyset_size > keyset_size) {
+            sprintf(
+                    error_message,
+                    "The requested keyset size of <%lu> is greater than the one available of <%lu>, can't continue",
+                    requested_keyset_size,
+                    keyset_size);
+            state.SkipWithError(error_message);
+            return;
         }
-    }
 
+        bool result = test_support_hashtable_prefill(hashtable, keyset, test_value_1, requested_keyset_size);
+
+        if (!result) {
+            hashtable_free(hashtable);
+
+            sprintf(error_message, "Unable to prefill the hashtable with <%lu> keys", requested_keyset_size);
+            state.SkipWithError(error_message);
+            return;
+        }
+
+        // Flush the cpu DCACHE related to the portion of the keyset used by the bench before starting the test
+        test_support_flush_data_cache(keyset, TEST_SUPPORT_RANDOM_KEYS_MAX_LENGTH_WITH_NULL * requested_keyset_size);
+    }
 
     test_support_set_thread_affinity(state.thread_index);
 
     for (auto _ : state) {
-        for(long int i = state.thread_index; i < state.range(1); i += state.threads) {
-            char* key = keys + (TEST_SUPPORT_RANDOM_KEYS_MAX_LENGTH_WITH_NULL * i);
+        for(long int i = state.thread_index; i < requested_keyset_size; i += state.threads) {
+            char* key = keyset + (TEST_SUPPORT_RANDOM_KEYS_MAX_LENGTH_WITH_NULL * i);
 
-            bool result = hashtable_op_set(
+            benchmark::DoNotOptimize((result = hashtable_op_set(
                     hashtable,
                     key,
                     strlen(key),
-                    test_value_1);
+                    i)));
 
             if (!result) {
                 sprintf(
