955bab4d3a80f82fda616fce9dbd725007e54003
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-20 15:42:38 +0100

    Refactor the delete implementation to support the new structure

--- src/hashtable/hashtable_op_delete.c
+++ src/hashtable/hashtable_op_delete.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #include "memory_fences.h"
+#include "log.h"
 #include "exttypes.h"
 #include "spinlock.h"
 
@@ -20,11 +21,15 @@ bool hashtable_op_delete(
         hashtable_key_data_t* key,
         hashtable_key_size_t key_size) {
     hashtable_hash_t hash;
-    hashtable_bucket_index_t bucket_index;
-    volatile hashtable_key_value_t* bucket_key_value;
+    hashtable_chunk_index_t chunk_index = 0;
+    hashtable_chunk_slot_index_t chunk_slot_index = 0;
+    hashtable_key_value_volatile_t* key_value;
     bool deleted = false;
-/*
+
     hash = hashtable_support_hash_calculate(key, key_size);
+
+    LOG_DI("key (%d) = %s", key_size, key);
+    LOG_DI("hash = 0x%016x", hash);
 
     volatile hashtable_data_t* hashtable_data_list[] = {
             hashtable->ht_current,
@@ -38,7 +43,11 @@ bool hashtable_op_delete(
             hashtable_data_index++) {
         volatile hashtable_data_t *hashtable_data = hashtable_data_list[hashtable_data_index];
 
-        if (hashtable_data == NULL) {
+        LOG_DI("hashtable_data_index = %u", hashtable_data_index);
+        LOG_DI("hashtable_data = 0x%016x", hashtable_data);
+
+        if (hashtable_data_index > 0 && (!hashtable->is_resizing || hashtable_data == NULL)) {
+            LOG_DI("not resizing, skipping check on the current hashtable_data");
             continue;
         }
 
@@ -47,16 +56,28 @@ bool hashtable_op_delete(
                 key,
                 key_size,
                 hash,
-                &bucket_key_value) == false) {
+                &chunk_index,
+                &chunk_slot_index,
+                &key_value) == false) {
+            LOG_DI("key not found, continuing");
             continue;
         }
 
-        hashtable_data->hashes[bucket_index] = 0;
-        bucket_key_value->flags = HASHTABLE_BUCKET_KEY_VALUE_FLAG_DELETED;
+        LOG_DI("key found, deleting hash and setting flags to deleted");
+
+        // It first sets the half hash to zero and only after it resets the flags to deleted.
+        hashtable_data->half_hashes_chunk[chunk_index].half_hashes[chunk_slot_index] = 0;
+        HASHTABLE_MEMORY_FENCE_STORE();
+
+        key_value->flags = HASHTABLE_KEY_VALUE_FLAG_DELETED;
         HASHTABLE_MEMORY_FENCE_STORE();
 
         deleted = true;
     }
-*/
+
+    LOG_DI("deleted = %s", deleted ? "YES" : "NO");
+    LOG_DI("chunk_index = 0x%016x", chunk_index);
+    LOG_DI("chunk_slot_index = 0x%016x", chunk_slot_index);
+
     return deleted;
 }
