c0dda24d019bd074773a58fc419f29a871f5abb7
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-20 19:03:27 +0100

    Currently there is no append-only store so the algorithm implementation is crash-prone, better add some memory barriers that will reduce the risk. Once the append-only store will be implemented most of this code can be dropped.

--- src/hashtable/hashtable_op_delete.c
+++ src/hashtable/hashtable_op_delete.c
@@ -8,6 +8,7 @@
 #include "log.h"
 #include "exttypes.h"
 #include "spinlock.h"
+#include "xalloc.h"
 
 #include "hashtable/hashtable.h"
 #include "hashtable/hashtable_support_index.h"
@@ -21,6 +22,7 @@ bool hashtable_op_delete(
         hashtable_key_data_t* key,
         hashtable_key_size_t key_size) {
     hashtable_hash_t hash;
+    hashtable_key_value_flags_t key_value_flags = 0;
     hashtable_chunk_index_t chunk_index = 0;
     hashtable_chunk_slot_index_t chunk_slot_index = 0;
     hashtable_half_hashes_chunk_volatile_t* half_hashes_chunk;
@@ -66,13 +68,32 @@ bool hashtable_op_delete(
 
         LOG_DI("key found, deleting hash and setting flags to deleted");
 
-        half_hashes_chunk = hashtable_data->half_hashes_chunk[chunk_index];
+        half_hashes_chunk = &hashtable_data->half_hashes_chunk[chunk_index];
 
+        // The get operation is not using locks so the memory fences are needed as well
         spinlock_lock(&half_hashes_chunk->write_lock, true);
 
-        half_hashes_chunk->half_hashes[chunk_slot_index] = 0;
         half_hashes_chunk->metadata.is_full = 0;
+
+        half_hashes_chunk->half_hashes[chunk_slot_index] = 0;
+
+        HASHTABLE_MEMORY_FENCE_STORE();
+
+        key_value_flags = key_value->flags;
         key_value->flags = HASHTABLE_KEY_VALUE_FLAG_DELETED;
+
+        HASHTABLE_MEMORY_FENCE_STORE();
+
+        if (!HASHTABLE_KEY_VALUE_HAS_FLAG(key_value_flags, HASHTABLE_KEY_VALUE_FLAG_KEY_INLINE)) {
+            // Even if we have memory fences here, hashtable_op_get may read from the memory that it's going to be
+            // de-allocated.
+            // Even if it never happened so far even under extremely high concurrency (tested up to 64 logical core with
+            // 2048 threads on an AMD EPYC 7502P) it can potentially happen.
+            // The append only store will solve this problem once it will be implemented, for the version 0.1 this
+            // potentially crash-causing implementation will do well-enough.
+            xalloc_free(key_value->external_key.data);
+            key_value->external_key.size = 0;
+        }
 
         spinlock_unlock(&half_hashes_chunk->write_lock);
 
