d62567362e3dba4b7503a663d53668095d7f48f8
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-21 17:36:27 +0100

    Add the code to detect if the host has support for the _mm_clflushopt instruction

--- tools/cmake/arch/arch-x86_64.cmake
+++ tools/cmake/arch/arch-x86_64.cmake
@@ -1,4 +1,5 @@
 include(CheckCCompilerFlag)
+include(CheckCXXSourceCompiles)
 
 check_c_compiler_flag(-mavx2                              COMPILER_HAS_MAVX2_FLAG)
 check_c_compiler_flag(-mavx                               COMPILER_HAS_MAVX_FLAG)
@@ -6,3 +7,27 @@ check_c_compiler_flag(-mno-avx256-split-unaligned-load    COMPILER_HAS_MNO_AVX25
 check_c_compiler_flag(-mbmi                               COMPILER_HAS_MBMI_FLAG)
 check_c_compiler_flag(-mlzcnt                             COMPILER_HAS_MLZCNT_FLAG)
 check_c_compiler_flag(-mclflushopt                        COMPILER_HAS_CLFLUSHOPT_FLAG)
+
+if (COMPILER_HAS_CLFLUSHOPT_FLAG)
+    message(STATUS "Compiler has -mclflushopt -- yes")
+
+    set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+    list(APPEND CMAKE_REQUIRED_FLAGS "-mclflushopt -march=native")
+    check_cxx_source_compiles("
+#include <immintrin.h>
+int main() {
+    char temp[64] = {0};
+    _mm_clflushopt(&temp);
+    return 0;
+}"
+            HOST_HAS_MM_CLFLUSHOPT)
+    set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
+
+    if (HOST_HAS_MM_CLFLUSHOPT)
+        message(STATUS "Host supports _mm_clflushopt -- yes")
+    else()
+        message(STATUS "Host supports _mm_clflushopt -- no")
+    endif()
+else()
+    message(STATUS "Compiler has -mclflushopt -- no")
+endif()
