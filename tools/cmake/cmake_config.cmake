974d48648d4245e104a7e705d733a4d618515cf3
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-21 14:29:01 +0100

    Refactor the build system to dynamically generate cmake_config.h at configure time, required to get macros for the dependencies

---
+++ tools/cmake/cmake_config.cmake
@@ -0,0 +1,27 @@
+set(CACHEGRAND_CMAKE_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+set(CACHEGRAND_CMAKE_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+string(LENGTH "${CMAKE_SOURCE_DIR}/" CMAKE_SOURCE_DIR_LENGTH)
+
+# Generate the cmake_config.c
+include(cmake_config.buildstep)
+add_custom_target(__internal_refresh_cmake_config
+    COMMAND
+        cmake
+            -DCACHEGRAND_ARCH_TARGET=${CACHEGRAND_ARCH_TARGET}
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DCACHEGRAND_CMAKE_SOURCE_DIR="${PROJECT_SOURCE_DIR}"
+            -DCACHEGRAND_CMAKE_BINARY_DIR="${CMAKE_CURRENT_BINARY_DIR}"
+            -P "${CMAKE_CURRENT_SOURCE_DIR}/tools/cmake/cmake_config.buildstep.cmake" >/dev/null
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+
+# Generate the cmake_config.h
+message(STATUS "Updating cmake_config.h")
+configure_file(
+        "${CACHEGRAND_CMAKE_SOURCE_DIR}/src/cmake_config.h.in"
+        "${CACHEGRAND_CMAKE_BINARY_DIR}/cmake_config.h"
+        @ONLY)
+message(STATUS "Updating cmake_config.h -- done")
+
+# Automatically include cmake_config.c and cmake_config.h in the build
+set(CACHEGRAND_CMAKE_CONFIG_C_SRC ${CMAKE_CURRENT_BINARY_DIR}/cmake_config.c)
+add_compile_options(-include ${CACHEGRAND_CMAKE_BINARY_DIR}/cmake_config.h)
