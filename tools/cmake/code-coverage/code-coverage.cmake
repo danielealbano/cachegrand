ae0f5d56d18099d2245e3ef41a51e5f3b1175c22
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-21 20:45:19 +0100

    Ensure that the code coverage generation script is executable and do not fail if the directory for the code coverage files is not already there

--- tools/cmake/code-coverage/code-coverage.cmake
+++ tools/cmake/code-coverage/code-coverage.cmake
@@ -9,8 +9,9 @@ function(target_add_code_coverage TARGET)
 
     add_custom_target(
             ${CODECOVERAGE_TARGET_NAME}
-            COMMAND test -d code-coverage && rm -rf code-coverage
+            COMMAND test -d code-coverage && rm -rf code-coverage || true
             COMMAND mkdir -p code-coverage
+            COMMAND chmod 700 ${PROJECT_SOURCE_DIR}/tools/code_coverage_cmake_support/code_coverage_cmake_support.sh
             COMMAND ${PROJECT_SOURCE_DIR}/tools/code_coverage_cmake_support/code_coverage_cmake_support.sh -o ${CODECOVERAGE_TARGET_BINARY_DIRECTORY} -c ${CODECOVERAGE_OUTPUT_PATH}
             COMMAND echo "-- Code coverage files have been output to ${CODECOVERAGE_OUTPUT_PATH}"
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
