87cf22f09a437c92f3848c350343c8c6c431deec
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-21 17:01:44 +0100

    Check if the compiler has the -mclflushopt flag when compiling for x86_64

--- tools/cmake/arch/arch-x86_64.cmake
+++ tools/cmake/arch/arch-x86_64.cmake
@@ -5,3 +5,4 @@ check_cxx_compiler_flag(-mavx                               COMPILER_HAS_MAVX_FL
 check_cxx_compiler_flag(-mno-avx256-split-unaligned-load    COMPILER_HAS_MNO_AVX256_SPLIT_UNALIGNED_LOAD_FLAG)
 check_cxx_compiler_flag(-mbmi                               COMPILER_HAS_MBMI_FLAG)
 check_cxx_compiler_flag(-mlzcnt                             COMPILER_HAS_MLZCNT_FLAG)
+check_cxx_compiler_flag(-mclflushopt                        COMPILER_HAS_CLFLUSHOPT_FLAG)
