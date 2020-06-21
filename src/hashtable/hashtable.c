6ed5c17a872daf94f5ba068ba4ae4a063c73f610
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-21 22:45:18 +0100

    Update hashtable.c and hashtable_data.c to use pow2.h and the new functions

--- src/hashtable/hashtable.c
+++ src/hashtable/hashtable.c
@@ -5,28 +5,14 @@
 #include "exttypes.h"
 #include "spinlock.h"
 #include "xalloc.h"
+#include "pow2.h"
 
 #include "hashtable/hashtable.h"
 #include "hashtable/hashtable_config.h"
 #include "hashtable/hashtable_data.h"
 
-//https://jameshfisher.com/2018/03/30/round-up-power-2/
-uint64_t next_pow2m1(uint64_t x) {
-    x |= x>>1;
-    x |= x>>2;
-    x |= x>>4;
-    x |= x>>8;
-    x |= x>>16;
-    x |= x>>32;
-
-    return x;
-}
-uint64_t next_pow2(uint64_t x) {
-    return next_pow2m1(x-1)+1;
-}
-
 hashtable_t* hashtable_init(hashtable_config_t* hashtable_config) {
-    hashtable_bucket_count_t buckets_count = next_pow2(hashtable_config->initial_size);
+    hashtable_bucket_count_t buckets_count = pow2_next(hashtable_config->initial_size);
     hashtable_t* hashtable = (hashtable_t*)xalloc_alloc(sizeof(hashtable_t));
     hashtable_data_t* hashtable_data = hashtable_data_init(buckets_count);
 
