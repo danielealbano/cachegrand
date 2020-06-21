6c07d39764baf79ef3df10e7f4deebc5f456e117
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-21 17:57:01 +0100

    Test  the host for AVX and AVX2

--- tools/cmake/arch/arch-x86_64.cmake
+++ tools/cmake/arch/arch-x86_64.cmake
@@ -8,6 +8,50 @@ check_c_compiler_flag(-mbmi                               COMPILER_HAS_MBMI_FLAG
 check_c_compiler_flag(-mlzcnt                             COMPILER_HAS_MLZCNT_FLAG)
 check_c_compiler_flag(-mclflushopt                        COMPILER_HAS_CLFLUSHOPT_FLAG)
 
+if (COMPILER_HAS_MAVX2_FLAG)
+    set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+    list(APPEND CMAKE_REQUIRED_FLAGS "-mavx2")
+    check_cxx_source_compiles("
+#include <stdint.h>
+#include <immintrin.h>
+int main() {
+    uint32_t hash1 = 1234;
+    uint32_t hash2 = 4321;
+    __m256i cmp_vector1 = _mm256_set1_epi32(hash1);
+    __m256i cmp_vector2 = _mm256_set1_epi32(hash2);
+    __m256i result_mask_vector = _mm256_cmpeq_epi32(cmp_vector1, cmp_vector2);
+    return 0;
+}"
+            HOST_HAS_AVX2)
+    set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
+
+    if (NOT HOST_HAS_AVX2)
+        set(HOST_HAS_AVX2 0)
+    endif()
+endif()
+
+if (COMPILER_HAS_MAVX_FLAG)
+    set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+    list(APPEND CMAKE_REQUIRED_FLAGS "-mavx -mno-avx2")
+    check_cxx_source_compiles("
+#include <stdint.h>
+#include <immintrin.h>
+int main() {
+    uint32_t hash1 = 1234;
+    uint32_t hash2 = 4321;
+    __m256 cmp_vector1 = _mm256_castsi256_ps(_mm256_set1_epi32(hash1));
+    __m256 cmp_vector2 = _mm256_castsi256_ps(_mm256_set1_epi32(hash2));
+    __m256 result_mask_vector = _mm256_cmp_ps(cmp_vector1, cmp_vector2, _CMP_EQ_OQ);
+    return 0;
+}"
+            HOST_HAS_AVX)
+    set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
+
+    if (NOT HOST_HAS_AVX)
+        set(HOST_HAS_AVX 0)
+    endif()
+endif()
+
 if (COMPILER_HAS_CLFLUSHOPT_FLAG)
     set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
     list(APPEND CMAKE_REQUIRED_FLAGS "-mclflushopt -march=native")
