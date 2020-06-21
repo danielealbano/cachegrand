8fb914f5371cda96cc83ce310091339caf12c995
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-21 22:57:53 +0100

    Add a skeleton for the liburing (iouring) implementation of the network channel

---
+++ src/network/channel/network_channel_iouring.c
@@ -0,0 +1,7 @@
+#include <liburing.h>
+
+#include "network_channel_iouring.h"
+
+void network_channel_iouring_init() {
+    // do nothing
+}
---
+++ src/network/channel/network_channel_iouring.h
@@ -0,0 +1,19 @@
+#ifndef CACHEGRAND_NETWORK_CHANNEL_IOURING_H
+#define CACHEGRAND_NETWORK_CHANNEL_IOURING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct network_channel_iouring network_channel_iouring_t;
+struct network_channel_iouring {
+
+};
+
+void network_channel_iouring_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CACHEGRAND_NETWORK_CHANNEL_IOURING_H
