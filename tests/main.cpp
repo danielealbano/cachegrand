ca163094c068cb544d2542f8d4351d29edf816c0
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-21 18:08:05 +0100

    Implement own main for Catch2 and add support for the sigsegv signal handler to report a stack trace of the failure

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,2 +1,17 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+#include "signal_handler_sigsegv.h"
+
+int main(int argc, char* argv[]) {
+    Catch::Session session;
+
+    signal_handler_sigsegv_init();
+
+    int returnCode = session.applyCommandLine( argc, argv );
+    if( returnCode != 0 ) {
+        return returnCode;
+    }
+
+    return session.run();
+}
