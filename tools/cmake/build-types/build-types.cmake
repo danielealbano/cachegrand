5efa5fad83fbf50c36233d0e8cd196b238b9c936
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-24 22:29:37 +0100

    Add the ability to select the hash algorithm at runtime instead of pre-selecting it via build type

--- tools/cmake/build-types/build-types.cmake
+++ tools/cmake/build-types/build-types.cmake
@@ -1,22 +1,14 @@
 if (CMAKE_BUILD_TYPE MATCHES Debug)
     add_definitions(-DDEBUG=1)
-    add_definitions(-DHASHTABLE_HASH_ALGORITHM_SELECTED=1)
     add_compile_options(-g -O0 -fno-inline)
 
     if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
         link_libraries(gcov)
     endif()
 
-    message(STATUS "Release build")
-elseif (CMAKE_BUILD_TYPE MATCHES ReleaseBench)
-    add_definitions(-DNDEBUG=1)
-    add_definitions(-DHASHTABLE_HASH_ALGORITHM_SELECTED=2)
-    add_compile_options(-g -O3)
-
-    message(STATUS "Release Benches build (using CRC32C algorithm)")
+    message(STATUS "Debug build")
 elseif (CMAKE_BUILD_TYPE MATCHES Release)
     add_definitions(-DNDEBUG=1)
-    add_definitions(-DHASHTABLE_HASH_ALGORITHM_SELECTED=1)
     add_compile_options(-g -O3)
 
     message(STATUS "Release build")
