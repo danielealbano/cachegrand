e94c094d1a1d7c1ac1e444885600153df3d89ef1
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-26 23:04:52 +0100

    Implement some macros to declare function constructors / destructors using .ctors and .dtors in elf objects

--- src/misc.h
+++ src/misc.h
@@ -14,6 +14,8 @@ extern "C" {
 #define likely(x)      __builtin_expect(!!(x), 1)
 #define unlikely(x)    __builtin_expect(!!(x), 0)
 
+#define ELF_SECTION( S ) __attribute__ ((section ( S )))
+
 #define concat_(a, b)   a ## _ ## b
 #define concat(a, b)    concat_(a, b)
 
@@ -29,6 +31,17 @@ extern "C" {
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a < _b ? _a : _b; })
+
+#define FUNCTION_CTOR_DTOR(SECTION_TYPE, SECTION_TYPE_STR, NAME, FUNC_BODY) \
+    static void concat(NAME, SECTION_TYPE) (){ \
+        FUNC_BODY \
+    } \
+    \
+    static void (*concat(concat(NAME, SECTION_TYPE), fp))(void) ELF_SECTION(SECTION_TYPE_STR) = \
+        concat(NAME, SECTION_TYPE); \
+
+#define FUNCTION_CTOR(NAME, ...) FUNCTION_CTOR_DTOR(ctors, ".ctors", NAME, __VA_ARGS__)
+#define FUNCTION_DTOR(NAME, ...) FUNCTION_CTOR_DTOR(dtors, ".dtors", NAME, __VA_ARGS__)
 
 #ifdef __cplusplus
 }
