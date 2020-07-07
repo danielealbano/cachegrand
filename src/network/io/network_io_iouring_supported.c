5e4a46b5ecc215bd17b9b804f8988d339c4b717b
Daniele Salvatore Albano <d.albano@gmail.com>
2020-07-07 17:22:50 +0200

    Initial implementation of some support code to check if io_uring is available within the kernel

---
+++ src/network/io/network_io_iouring_supported.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "misc.h"
+#include "log.h"
+
+#include "network_io_iouring_supported.h"
+
+const char* kallsyms_path = "/proc/kallsyms";
+const char* expected_symbol_name = "io_uring_create";
+
+LOG_PRODUCER_CREATE_LOCAL_DEFAULT("network_io_iouring_supported", network_io_iouring_supported)
+
+bool network_io_iouring_supported_fetch_kallsyms_symbol_name(
+        FILE* fd,
+        char* buffer,
+        size_t buffer_size) {
+    int res;
+    unsigned long long address;
+    char type;
+
+    if (buffer_size < 500) {
+        return false;
+    }
+
+    if ((res = fscanf(fd, "%llx %c %499s\n", &address, &type, buffer)) != 3) {
+        if (res != EOF && fgets(buffer, 500, fd) == NULL) {
+            LOG_E(LOG_PRODUCER_DEFAULT, "Unable to read data from <%s>", kallsyms_path);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool network_io_iouring_supported_is_expected_symbol_name(
+        char* buffer,
+        size_t buffer_size) {
+    return strncmp(buffer, expected_symbol_name, buffer_size) == 0;
+}
+
+FILE* network_io_iouring_supported_open_kallsyms() {
+    FILE *fd = fopen(kallsyms_path, "r");
+
+    if (fd == NULL) {
+        LOG_E(LOG_PRODUCER_DEFAULT, "Unable to open <%s>", kallsyms_path);
+        LOG_E_OS_ERROR(LOG_PRODUCER_DEFAULT);
+    }
+
+    return fd;
+}
+
+bool network_io_iouring_supported() {
+    bool ret = false;
+    char name[500] = {0};
+    FILE *fd;
+
+    if ((fd = network_io_iouring_supported_open_kallsyms()) == NULL) {
+        return ret;
+    }
+
+    while(!feof(fd)) {
+        if (network_io_iouring_supported_fetch_kallsyms_symbol_name(
+                fd,
+                name,
+                sizeof(name)) == false) {
+            break;
+        }
+
+        if (network_io_iouring_supported_is_expected_symbol_name(
+                name,
+                sizeof(name))) {
+            ret = true;
+            break;
+        }
+    }
+
+    fclose(fd);
+
+    return ret;
+}
