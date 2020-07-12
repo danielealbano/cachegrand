5b7477b958f3870a165a5efa9b87a472a8882983
Daniele Salvatore Albano <d.albano@gmail.com>
2020-07-12 20:49:23 +0100

    Move the dependencies inside the 3rdparty and add the liburing cmake file to build liburing via cmake (still depending on the liburing configure to create the expected additional headers)

---
+++ 3rdparty/liburing.cmake
@@ -0,0 +1,38 @@
+include(ExternalProject)
+file(GLOB SRC_FILES_LIBURING "liburing/src/*.c")
+
+# The configure script generates a few files and expects an in-place build so we need to set the BUILD_INSOURCE flag
+# to 1 to make it work.
+# This will also allow the target_include_directories to work properly
+set(LIBURING_BUILD_PATH "${CMAKE_BINARY_DIR}/_deps/src/liburing-install-build")
+set(LIBURING_CONFIGURE_INCLUDE_PATH "${LIBURING_BUILD_PATH}/src/include")
+file(MAKE_DIRECTORY ${LIBURING_CONFIGURE_INCLUDE_PATH}/liburing)
+file(TOUCH ${LIBURING_CONFIGURE_INCLUDE_PATH}/liburing/config-host.h)
+
+ExternalProject_Add(
+        liburing-install
+        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/liburing
+        PREFIX ${CMAKE_BINARY_DIR}/_deps
+        CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/liburing/configure --prefix=${CMAKE_BINARY_DIR}/_deps/liburing/install
+        INSTALL_COMMAND cmake -E echo "Skipping install step."
+        BUILD_COMMAND cmake -E echo "Skipping build step.")
+
+add_library(
+        uring
+        ${SRC_FILES_LIBURING})
+
+add_dependencies(
+        uring
+        liburing-install)
+
+target_compile_options(
+        uring
+        PRIVATE
+        -include ${LIBURING_CONFIGURE_INCLUDE_PATH}/liburing/config-host.h -Wall -Wextra -Wno-unused-parameter -Wno-sign-compare)
+
+target_include_directories(
+        uring
+        PUBLIC
+        liburing/src/include liburing/src/include/liburing ${LIBURING_CONFIGURE_INCLUDE_PATH} ${LIBURING_BUILD_PATH})
+
+list(APPEND DEPS_LIST_LIBRARIES "uring")
--- tools/cmake/deps/deps.cmake
+++
@@ -1,5 +0,0 @@
-# Add base libraries always available
-list(APPEND DEPS_LIST_LIBRARIES pthread)
-
-include(openssl)
-include(liburing)
