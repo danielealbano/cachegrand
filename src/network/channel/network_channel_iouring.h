6032935c652ea18d407fee3b62f5f1d9e1d4c561
Daniele Salvatore Albano <d.albano@gmail.com>
2020-07-13 21:14:02 +0100

    Ensure that the OP 0 is never used with io_uring to catch possible issues, also the fd is an int and initialize address_str with the right size not a random-but -bing-enough size

--- src/network/channel/network_channel_iouring.h
+++ src/network/channel/network_channel_iouring.h
@@ -7,17 +7,21 @@ extern "C" {
 
 // TODO: this enum and the struct below should most likely become more generic because they should be shared between
 //       the storage io and the network io, will see
+// Never use 0 as OP, helps to spot issues.
 enum {
-    NETWORK_IO_IOURING_OP_NONE = 0,
-    NETWORK_IO_IOURING_OP_TIMEOUT,
+    NETWORK_IO_IOURING_OP_NOP = 1,
+    NETWORK_IO_IOURING_OP_FILES_UPDATE,
     NETWORK_IO_IOURING_OP_ACCEPT,
     NETWORK_IO_IOURING_OP_RECV,
     NETWORK_IO_IOURING_OP_SEND,
+
+    // This goes for last
+    NETWORK_IO_IOURING_OP_LAST,
 };
 
 typedef struct network_channel_iouring_entry_user_data network_channel_iouring_entry_user_data_t;
 struct network_channel_iouring_entry_user_data {
-    uint32_t fd;
+    int32_t fd;
     uint8_t op;
     char* buffer;
     union {
@@ -25,7 +29,7 @@ struct network_channel_iouring_entry_user_data {
         struct sockaddr_in ipv4;
         struct sockaddr_in6 ipv6;
     } new_socket_address;
-    char address_str[50];
+    char address_str[INET6_ADDRSTRLEN + 1];
     socklen_t address_size;
 };
 
