cd15e12dc10b2f267262a84dbb26d2ab992e7ebd
Daniele Salvatore Albano <d.albano@gmail.com>
2020-07-19 17:57:10 +0100

    Refactor the signal handler sigsegv registration and management in a more broad signals management

---
+++ src/signals_support.c
@@ -0,0 +1,88 @@
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+#include <signal.h>
+
+#include "misc.h"
+#include "log.h"
+#include "fatal.h"
+#include "signals_support.h"
+
+LOG_PRODUCER_CREATE_DEFAULT("signal_handler/sigsegv", signal_handler_sigsegv)
+
+char *unknown_signal_name = "UNKNOWN_SIGNAL_NAME";
+
+void signals_support_handler_sigsegv_fatal(int signal_number) {
+    char *signal_name = SIGNALS_SUPPORT_NAME_WRAPPER(signal_number);
+    FATAL(
+            LOG_PRODUCER_DEFAULT,
+            "Recived segmentation fault signal (%s %d):\n",
+            signal_name,
+            signal_number);
+}
+
+void signals_support_register_sigsegv_fatal_handler() {
+#ifndef DEBUG
+    signal(SIGSEGV, signals_support_handler_sigsegv_fatal);
+#endif
+}
+
+char* signals_support_name(
+        int signal_number,
+        char* buffer,
+        size_t buffer_size) {
+    if (signal_number >= NSIG || buffer_size <= 1) {
+        return NULL;
+    }
+
+    char* signal_name = strsignal(signal_number);
+    strncpy(buffer, signal_name, buffer_size - 1);
+    buffer[buffer_size - 1] = 0;
+
+    char* s = buffer;
+    while (*s) {
+        *s = toupper(*s);
+        s++;
+    }
+
+    return buffer;
+}
+
+bool signals_support_register_signal_handler(
+        int signal_number,
+        __sighandler_t signal_handler,
+        struct sigaction* previous_action) {
+    char *signal_name;
+    struct sigaction action;
+    sigemptyset(&action.sa_mask);
+    action.sa_handler = signal_handler;
+    action.sa_flags = 0;
+
+    if (signal_number >= NSIG) {
+        return false;
+    }
+
+    signal_name = SIGNALS_SUPPORT_NAME_WRAPPER(signal_number);
+
+    LOG_D(
+            LOG_PRODUCER_DEFAULT,
+            "Registering signal handler <%s (%d)>",
+            signal_name,
+            signal_number);
+
+    if (sigaction(signal_number, &action, previous_action) < 0) {
+        LOG_E(
+                LOG_PRODUCER_DEFAULT,
+                "Unable to set the handler for <%s (%d)>",
+                signal_name,
+                signal_number);
+        LOG_E_OS_ERROR(LOG_PRODUCER_DEFAULT);
+
+        return false;
+    }
+
+    return true;
+}
---
+++ src/signals_support.h
@@ -0,0 +1,40 @@
+#ifndef CACHEGRAND_SIGNALS_SUPPORT_H
+#define CACHEGRAND_SIGNALS_SUPPORT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SIGNALS_SUPPORT_NAME_BUFFER_SIZE 20
+#define SIGNALS_SUPPORT_NAME_WRAPPER(SIGNAL_NUMBER) \
+    ({ \
+        char *signal_name; \
+        char signal_name_buffer[SIGNALS_SUPPORT_NAME_BUFFER_SIZE] = {0}; \
+        signal_name = signals_support_name(signal_number, signal_name_buffer, sizeof(signal_name_buffer)); \
+        if (!signal_name) { \
+            signal_name = unknown_signal_name; \
+        } \
+        \
+        signal_name; \
+    })
+
+typedef void (*sighandler_t) (int);
+
+extern char *unknown_signal_name;
+
+void signals_support_handler_sigsegv_fatal(int signal_number);
+void signals_support_register_sigsegv_fatal_handler();
+char* signals_support_name(
+        int signal_number,
+        char* buffer,
+        size_t buffer_size);
+bool signals_support_register_signal_handler(
+        int signal_number,
+        sighandler_t signal_handler,
+        struct sigaction* previous_action);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CACHEGRAND_SIGNALS_SUPPORT_H
