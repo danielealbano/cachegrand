ed7baac105e307f18efecf85f1eef8827e6b51a7
Daniele Salvatore Albano <d.albano@gmail.com>
2020-07-19 21:28:34 +0100

    Enable sigsegv catching in program_register_signal_handlers if not debug

--- src/program.c
+++ src/program.c
@@ -75,7 +75,9 @@ void program_register_signal_handlers() {
     sigemptyset(&action.sa_mask);
     action.sa_flags = 0;
 
+#ifndef DEBUG
     signals_support_register_sigsegv_fatal_handler();
+#endif
     signal(SIGCHLD, SIG_IGN);
 
     for(uint8_t i = 0; i < program_signals_count; i++) {
