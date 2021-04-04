c29eb7a3e7cfa3765a58af243b1bfa40ace718f4
Daniele Albano <d.albano@gmail.com>
2021-04-04 21:40:20 +0100

    Add libnuma support to the build system

---
+++ 3rdparty/libnuma.cmake
@@ -0,0 +1,34 @@
+include(CheckIncludeFiles)
+include(CheckCCompilerFlag)
+include(CheckCSourceCompiles)
+include(CheckFunctionExists)
+
+# Can't use pkg-config for libnuma, not all the distros provide numa.pc as part of the numa package
+
+set(OLD_CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES})
+list(APPEND CMAKE_REQUIRED_LIBRARIES "-lnuma")
+
+check_c_source_compiles("
+#include <numa.h>
+int main() {
+    int res = numa_available();
+    return 0;
+}"
+    HOST_HAS_LIBNUMA)
+set(CMAKE_REQUIRED_LIBRARIES ${OLD_CMAKE_REQUIRED_LIBRARIES})
+
+if (NOT HOST_HAS_LIBNUMA)
+    message(FATAL_ERROR "Can't find libnuma")
+endif()
+
+find_library(LIBNUMA_LIBRARY_DIRS NAMES numa NAMES_PER_DIR)
+find_path(LIBNUMA_INCLUDE_DIRS numa.h)
+set(LIBNUMA_LIBRARIES "numa")
+
+message(STATUS ${LIBNUMA_LIBRARY_DIRS})
+message(STATUS ${LIBNUMA_INCLUDE_DIRS})
+message(STATUS ${LIBNUMA_LIBRARIES})
+
+list(APPEND DEPS_LIST_LIBRARIES "${LIBNUMA_LIBRARIES}")
+list(APPEND DEPS_LIST_INCLUDE_DIRS "${LIBNUMA_INCLUDE_DIRS}")
+list(APPEND DEPS_LIST_LIBRARY_DIRS "${LIBNUMA_LIBRARY_DIRS}")
