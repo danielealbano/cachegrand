1fdd7dc28b5e07942d18e660f0865e7c85e5c6b4
Daniele Albano <d.albano@gmail.com>
2021-04-13 23:32:13 +0100

    Fix headers in config.h

--- src/config.c
+++ src/config.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdatomic.h>
+#include <arpa/inet.h>
 
+#include "exttypes.h"
+#include "misc.h"
 #include "xalloc.h"
+#include "protocol/redis/protocol_redis_reader.h"
 #include "network/protocol/network_protocol.h"
+#include "network/io/network_io_common.h"
+#include "network/channel/network_channel.h"
+#include "worker/worker.h"
 
 #include "config.h"
 
