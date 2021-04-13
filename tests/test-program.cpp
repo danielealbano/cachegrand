8d2c5129dd1e6c8c020f49e7896fab7c99c649c3
Daniele Albano <d.albano@gmail.com>
2021-04-13 23:24:07 +0100

    Update the test-program.cpp to pass the config to the workers initialization

--- tests/test-program.cpp
+++ tests/test-program.cpp
@@ -21,6 +21,8 @@
 #include "network/io/network_io_common.h"
 #include "network/channel/network_channel.h"
 #include "worker/worker.h"
+#include "config.h"
+
 #include "program.h"
 
 #pragma GCC diagnostic ignored "-Wwrite-strings"
@@ -41,6 +43,11 @@ void* test_program_wait_loop_terminate(
     program_request_terminate(terminate_event_loop);
 
     return NULL;
+}
+
+void test_program_prepare_mock_config(
+        config_t* config) {
+    memset(config, 0, sizeof(config_t));
 }
 
 TEST_CASE("program.c", "[program]") {
@@ -120,17 +127,23 @@ TEST_CASE("program.c", "[program]") {
     SECTION("program_wait_loop") {
         volatile bool terminate_event_loop = false;
         pthread_t pthread_wait, pthread_terminate;
-        pthread_attr_t attr;
 
         bool *program_terminate_event_loop = program_get_terminate_event_loop();
         *program_terminate_event_loop = false;
 
-        REQUIRE(pthread_attr_init(&attr) == 0);
-        REQUIRE(pthread_create(&pthread_wait, &attr, test_program_wait_loop_wait, (void*)&terminate_event_loop) == 0);
+        REQUIRE(pthread_create(
+                &pthread_wait,
+                NULL,
+                test_program_wait_loop_wait,
+                (void*)&terminate_event_loop) == 0);
 
         sleep(1);
 
-        REQUIRE(pthread_create(&pthread_terminate, &attr, test_program_wait_loop_terminate, (void*)&terminate_event_loop) == 0);
+        REQUIRE(pthread_create(
+                &pthread_terminate,
+                NULL,
+                test_program_wait_loop_terminate,
+                (void*)&terminate_event_loop) == 0);
 
         sleep(3);
 
@@ -142,14 +155,16 @@ TEST_CASE("program.c", "[program]") {
     }
 
     SECTION("program_workers_initialize") {
-        pthread_attr_t attr;
+        config_t config;
         worker_user_data_t* worker_user_data;
         network_channel_address_t addresses[] = { PROGRAM_NETWORK_ADDRESSES };
         volatile bool terminate_event_loop = false;
 
+        test_program_prepare_mock_config(&config);
+
         worker_user_data = program_workers_initialize(
                 &terminate_event_loop,
-                &attr,
+                &config,
                 1);
 
         // TODO: the tests should be able to define on which addresses the program is running, multiple test runs can
@@ -180,14 +195,16 @@ TEST_CASE("program.c", "[program]") {
     }
 
     SECTION("program_workers_cleanup") {
+        config_t config;
         pthread_t worker_pthread;
-        pthread_attr_t attr;
         worker_user_data_t* worker_user_data;
         volatile bool terminate_event_loop = false;
 
+        test_program_prepare_mock_config(&config);
+
         worker_user_data = program_workers_initialize(
                 &terminate_event_loop,
-                &attr,
+                &config,
                 1);
 
         // Terminate running thread
@@ -209,53 +226,55 @@ TEST_CASE("program.c", "[program]") {
 
         REQUIRE(mprobe(worker_user_data) == -MCHECK_FREE);
     }
-//
-//    SECTION("test redis ping/pong") {
-//        pthread_t worker_pthread;
-//        pthread_attr_t attr;
-//        worker_user_data_t* worker_user_data;
-//        struct sockaddr_in address = {0};
-//        size_t buffer_send_data_len;
-//        char buffer_send[64] = {0};
-//        char buffer_recv[64] = {0};
-//        volatile bool terminate_event_loop = false;
-//
-//        worker_user_data = program_workers_initialize(
-//                &terminate_event_loop,
-//                &attr,
-//                1);
-//
-//        // Terminate running thread
-//        worker_pthread = worker_user_data->pthread;
-//
-//        sleep(1);
-//
-//        int clientfd = network_io_common_socket_tcp4_new(0);
-//
-//        address.sin_family = AF_INET;
-//        address.sin_port = htons(12345);
-//        address.sin_addr.s_addr = inet_addr("127.0.0.1");
-//        snprintf(buffer_send, sizeof(buffer_send) - 1, "PING\r\n");
-//        buffer_send_data_len = strlen(buffer_send);
-//
-//        REQUIRE(connect(clientfd, (struct sockaddr*)&address, sizeof(address)) == 0);
-//        REQUIRE(send(clientfd, buffer_send, buffer_send_data_len, 0) == buffer_send_data_len);
-//        REQUIRE(recv(clientfd, buffer_recv, sizeof(buffer_recv), 0) == 7);
-//        REQUIRE(strncmp(buffer_recv, "+PONG\r\n", strlen("+PONG\r\n")) == 0);
-//        REQUIRE(network_io_common_socket_close(clientfd, false));
-//
-//        terminate_event_loop = true;
-//        HASHTABLE_MEMORY_FENCE_STORE();
-//
-//        // Wait for the thread to end
-//        sleep(3);
-//
-//        program_workers_cleanup(
-//                worker_user_data,
-//                1);
-//
-//        REQUIRE(pthread_kill(worker_pthread, 0) == ESRCH);
-//
-//        REQUIRE(mprobe(worker_user_data) == -MCHECK_FREE);
-//    }
+
+    SECTION("test redis ping/pong") {
+        config_t config;
+        pthread_t worker_pthread;
+        worker_user_data_t* worker_user_data;
+        struct sockaddr_in address = {0};
+        size_t buffer_send_data_len;
+        char buffer_send[64] = {0};
+        char buffer_recv[64] = {0};
+        volatile bool terminate_event_loop = false;
+
+        test_program_prepare_mock_config(&config);
+
+        worker_user_data = program_workers_initialize(
+                &terminate_event_loop,
+                &config,
+                1);
+
+        // Terminate running thread
+        worker_pthread = worker_user_data->pthread;
+
+        sleep(1);
+
+        int clientfd = network_io_common_socket_tcp4_new(0);
+
+        address.sin_family = AF_INET;
+        address.sin_port = htons(12345);
+        address.sin_addr.s_addr = inet_addr("127.0.0.1");
+        snprintf(buffer_send, sizeof(buffer_send) - 1, "*1\r\n$4\r\nPING\r\n");
+        buffer_send_data_len = strlen(buffer_send);
+
+        REQUIRE(connect(clientfd, (struct sockaddr*)&address, sizeof(address)) == 0);
+        REQUIRE(send(clientfd, buffer_send, buffer_send_data_len, 0) == buffer_send_data_len);
+        REQUIRE(recv(clientfd, buffer_recv, sizeof(buffer_recv), 0) == 10);
+        REQUIRE(strncmp(buffer_recv, "$4\r\nPONG\r\n", strlen("$4\r\nPONG\r\n")) == 0);
+        REQUIRE(network_io_common_socket_close(clientfd, false));
+
+        terminate_event_loop = true;
+        HASHTABLE_MEMORY_FENCE_STORE();
+
+        // Wait for the thread to end
+        sleep(3);
+
+        program_workers_cleanup(
+                worker_user_data,
+                1);
+
+        REQUIRE(pthread_kill(worker_pthread, 0) == ESRCH);
+
+        REQUIRE(mprobe(worker_user_data) == -MCHECK_FREE);
+    }
 }
