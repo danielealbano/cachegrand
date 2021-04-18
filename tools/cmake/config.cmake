958b7bd93cf1c2a3b8d7f34651a0c948f8edbe10
Daniele Albano <d.albano@gmail.com>
2021-04-18 14:38:39 +0100

    Use config.cmake to pass config settings to cachegrand

--- tools/cmake/config.cmake
+++ tools/cmake/config.cmake
@@ -1,16 +1,2 @@
-# setup the module includes
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/arch/")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/build-types/")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/compiler/")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/options/")
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/code-coverage/")
-
-include(arch)
-include(build-types)
-include(compiler)
-include(options)
-
-include(cmake_config)
-
-include(code-coverage)
+set(CONFIG_FILENAME_DEFAULT "cachegrand.conf")
+set(CONFIG_PATH_DEFAULT "${CMAKE_INSTALL_PREFIX}/etc/${CONFIG_FILENAME_DEFAULT}")
