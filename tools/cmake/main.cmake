a2aef1270e819f2c98f2c484ef2d5fc26638a112
Daniele Albano <d.albano@gmail.com>
2021-04-18 14:38:19 +0100

    Rename tools/cmake/config.cmake in main.cmake, need to use config.cmake for other settings

---
+++ tools/cmake/main.cmake
@@ -0,0 +1,17 @@
+# setup the module includes
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/arch/")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/build-types/")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/compiler/")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/options/")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/code-coverage/")
+
+include(arch)
+include(build-types)
+include(compiler)
+include(options)
+include(config)
+
+include(cmake_config)
+
+include(code-coverage)
