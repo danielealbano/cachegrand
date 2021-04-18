15ed3eb46a10d61a4f5f0731ca9056cff0ae55c5
Daniele Albano <d.albano@gmail.com>
2021-04-18 14:50:33 +0100

    Fix libyaml detection

--- 3rdparty/libyaml.cmake
+++ 3rdparty/libyaml.cmake
@@ -1,39 +1,19 @@
-FIND_PATH(LIBYAML_INCLUDE_DIR NAMES yaml.h)
-FIND_LIBRARY(LIBYAML_LIBRARIES NAMES yaml libyaml)
+include(CheckCSourceCompiles)
 
-message(STATUS "Performing Test HOST_HAS_LIBYAML")
-
-set(libyaml_check_compiles_source "
+list(APPEND CMAKE_REQUIRED_LIBRARIES "-lyaml")
+check_c_source_compiles("
 #include <yaml.h>
 int main() {
     int major, minor, patch;
     yaml_get_version(&major, &minor, &patch);
-    printf(\"%d.%d.%d\", major, minor, patch);
     return 0;
-}")
+}"
+        HOST_HAS_LIBYAML)
+set(CMAKE_REQUIRED_LIBRARIES ${OLD_CMAKE_REQUIRED_LIBRARIES})
 
-# We want to catch the version used to build cachegrand, need to use directly try_run
-file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/libyaml_check_compiles_source.c"
-        "${libyaml_check_compiles_source}\n")
-
-try_run(
-    HOST_HAS_LIBYAML_RUN_EXITCODE
-    HOST_HAS_LIBYAML_COMPILE_RESULT
-    ${CMAKE_CURRENT_BINARY_DIR}/
-    "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/libyaml_check_compiles_source.c"
-    LINK_LIBRARIES "-lyaml"
-    COMPILE_OUTPUT_VARIABLE HOST_HAS_LIBYAML_COMPILE_OUTPUT
-    RUN_OUTPUT_VARIABLE HOST_HAS_LIBYAML_RUN_OUTPUT)
-
-# Did compilation succeed and process return 0 (success)?
-IF("${HOST_HAS_LIBYAML_COMPILE_RESULT}" AND ("${HOST_HAS_LIBYAML_RUN_EXITCODE}" EQUAL 0))
-    set(HOST_HAS_LIBYAML 1)
-    string(STRIP "${HOST_HAS_LIBYAML_RUN_OUTPUT}" HOST_HAS_LIBYAML_VERSION)
-    message(STATUS "Performing Test HOST_HAS_LIBYAML - Success")
-ELSE()
-    message(STATUS "Performing Test HOST_HAS_LIBYAML - Failure")
+if (NOT HOST_HAS_LIBYAML)
     message(FATAL_ERROR "Can't find libyaml")
-ENDIF()
+endif()
 
 find_library(LIBYAML_LIBRARY_DIRS NAMES yaml NAMES_PER_DIR)
 find_path(LIBYAML_INCLUDE_DIRS yaml.h)
