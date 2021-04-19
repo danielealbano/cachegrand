b111611afbc9f7f4bb1376041f3ca59af6798180
Daniele Albano <d.albano@gmail.com>
2021-04-20 00:27:53 +0100

    Fix log ctor and dtor

--- src/log/log.c
+++ src/log/log.c
@@ -8,15 +8,12 @@
 #include <string.h>
 
 #include "misc.h"
-#include "log.h"
 #include "xalloc.h"
 
-// TODO: Currently all the formatting and printing logic is embedded in log_message_internal_printer and a sort of
-//       simple sink wrapper to pass around the FILE* where to print.
-//       The current producers overlay managed via the LOG_PRODUCER_* macros / log_producer_* functions should go
-//       because it doesn't provide anything and it's just a wrapper to the textual TAG so it should just be reverted to
-//       be a simple textual tag.
-//       Everytime a thread tries to submit a message to be logged should check if it has been registered with the
+#include "log.h"
+#include "log/sink/log_sink_console.h"
+
+// TODO: Everytime a thread tries to submit a message to be logged should check if it has been registered with the
 //       logging system and if not it should register, threads are not spawn dynamically so the logging thread will
 //       waste away checks on ended threads, but if it will be necessary a GC can be performed on threads that are not
 //       logging and the logging mechanism will take care of re-registering the thread if it has been de-registered
@@ -30,14 +27,24 @@ thread_local char* log_early_prefix_thread = NULL;
 static log_sink_t log_sinks_registered_list[LOG_SINK_REGISTERED_MAX] = {0};
 static uint8_t log_sinks_registered_count = 0;
 
-static log_sink_t *log_sink_default_console;
+FUNCTION_CTOR(log_sink_console_init, {
+    log_level_t level = LOG_LEVEL_ALL;
+    log_sink_settings_t settings = { 0 };
+    settings.console.use_stdout_for_errors = false;
 
-FUNCTION_CTOR(log_sink_init_console, {
-    log_sink_register(log_sink_console_init(LOG_LEVEL_ALL /*- LOG_LEVEL_DEBUG - LOG_LEVEL_VERBOSE*/));
+#if NDEBUG == 1
+    level -= LOG_LEVEL_DEBUG - LOG_LEVEL_VERBOSE;
+#endif
+
+    log_sink_register(log_sink_console_init(level, &settings));
 })
 
-FUNCTION_DTOR(log_sink_free_console, {
-    log_sink_free(log_sink_default_console);
+FUNCTION_DTOR(log_sink, {
+    for(uint8_t i = 0; i < log_sinks_registered_count && i < LOG_SINK_REGISTERED_MAX; i++) {
+        if (log_sinks_registered_list[i].free_fn) {
+            log_sinks_registered_list[i].free_fn(&log_sinks_registered_list[i].settings);
+        }
+    }
 })
 
 const char* log_level_to_string(log_level_t level) {
