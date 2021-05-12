75a098f571ae6a4a3e828adc7e66c2279c4478ec
Daniele Albano <d.albano@gmail.com>
2021-05-12 22:06:44 +0300

    Fix warning

--- src/config.c
+++ src/config.c
@@ -41,8 +41,6 @@ void config_internal_cyaml_log(
             level = LOG_LEVEL_DEBUG;
             break;
         case CYAML_LOG_NOTICE:
-            level = LOG_LEVEL_WARNING;
-            break;
         case CYAML_LOG_WARNING:
             level = LOG_LEVEL_WARNING;
             break;
