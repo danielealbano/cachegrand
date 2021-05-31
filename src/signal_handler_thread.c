7fca8abf0de56738c3b8eeae656528e338a68ef9
Daniele Albano <d.albano@gmail.com>
2021-05-31 23:10:27 +0300

    Add header and missing include to signal_handler_thread.c

--- src/signal_handler_thread.c
+++ src/signal_handler_thread.c
@@ -1,4 +1,13 @@
+/**
+ * Copyright (C) 2020-2021 Daniele Salvatore Albano
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ **/
+
 #include <stdint.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <signal.h>
 #include <stdatomic.h>
