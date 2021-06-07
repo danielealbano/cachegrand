af4c68ac18446b6c9596685ee597f6ab9a35116f
Daniele Albano <d.albano@gmail.com>
2021-06-07 19:05:34 +0300

    Add volatile bool and fix style in exttypes.h

--- src/exttypes.h
+++ src/exttypes.h
@@ -12,11 +12,12 @@ typedef unsigned __int128 uint128_t;
 // Defines the wrapper for the volatile type and creates a set of types based off the standard uint* using it
 #define _Volatile(T)    volatile T
 
-typedef _Volatile(uint8_t)  uint8_volatile_t;
-typedef _Volatile(uint16_t)  uint16_volatile_t;
-typedef _Volatile(uint32_t)  uint32_volatile_t;
-typedef _Volatile(uint64_t)  uint64_volatile_t;
-typedef _Volatile(uint128_t)  uint128_volatile_t;
+typedef _Volatile(uint8_t) uint8_volatile_t;
+typedef _Volatile(uint16_t) uint16_volatile_t;
+typedef _Volatile(uint32_t) uint32_volatile_t;
+typedef _Volatile(uint64_t) uint64_volatile_t;
+typedef _Volatile(uint128_t) uint128_volatile_t;
+typedef _Volatile(bool) bool_volatile_t;
 
 #ifdef __cplusplus
 }
