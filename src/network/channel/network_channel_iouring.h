c17c272febd907f53cbeb4a3757f1f615fd524ca
Daniele Albano <d.albano@gmail.com>
2021-06-08 10:16:24 +0300

    Remove commented out old headers

--- src/network/channel/network_channel_iouring.h
+++ src/network/channel/network_channel_iouring.h
@@ -22,24 +22,6 @@ network_channel_iouring_t* network_channel_iouring_new_multi(
 void network_channel_iouring_free(
         network_channel_iouring_t* network_channel);
 
-    //typedef struct network_channel_iouring_entry_user_data network_channel_iouring_entry_user_data_t;
-//struct network_channel_iouring_entry_user_data {
-//    network_io_iouring_op_t op;
-//    int mapped_fd;
-//    network_channel_t *channel;
-//    network_channel_socket_address_t listener_new_socket_address;
-//};
-//
-//network_channel_iouring_entry_user_data_t* network_channel_iouring_entry_user_data_new(
-//        network_io_iouring_op_t op);
-//
-//network_channel_iouring_entry_user_data_t* network_channel_iouring_entry_user_data_new_with_mapped_fd(
-//        network_io_iouring_op_t op,
-//        int mapped_fd);
-//
-//void network_channel_iouring_entry_user_data_free(
-//        network_channel_iouring_entry_user_data_t* iouring_user_data);
-
 #ifdef __cplusplus
 }
 #endif
