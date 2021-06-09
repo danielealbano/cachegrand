2b2af37de0bf61d296f01e23c58caeaec3311f16
Daniele Albano <d.albano@gmail.com>
2021-06-09 22:02:08 +0300

    Fix a warning caused by a missing cast to log_level_t

--- src/log/log.h
+++ src/log/log.h
@@ -42,7 +42,7 @@ enum log_level {
 };
 typedef enum log_level log_level_t;
 
-#define LOG_LEVEL_ALL (((LOG_LEVEL_MAX - 1u) * 2u) - 1u)
+#define LOG_LEVEL_ALL ((log_level_t)(((LOG_LEVEL_MAX - 1) * 2) - 1))
 
 void log_set_early_prefix_thread(
         char* prefix);
