c90cae652332cd36a3d4b05cec49a2c4c2fc7a0d
Daniele Albano <d.albano@gmail.com>
2021-06-12 09:21:52 +0300

    Refactor the signal_handler_thread to make it more testable

--- src/signal_handler_thread.c
+++ src/signal_handler_thread.c
@@ -35,16 +35,6 @@ static signal_handler_thread_context_t *int_context;
 int program_signals[] = { SIGUSR1, SIGINT, SIGHUP, SIGTERM, SIGQUIT };
 uint8_t program_signals_count = sizeof(program_signals) / sizeof(int);
 
-bool signal_handler_thread_running_under_debugger() {
-    bool debugger_attached = false;
-    if (ptrace(PTRACE_TRACEME, 0, 1, 0) < 0) {
-        debugger_attached = true;
-        ptrace(PTRACE_DETACH, 0, 1, 0);
-    }
-
-    return debugger_attached;
-}
-
 void signal_handler_thread_handle_signal(
         int signal_number) {
     char *signal_name = SIGNALS_SUPPORT_NAME_WRAPPER(signal_number);
@@ -89,8 +79,6 @@ void signal_handler_thread_register_signal_handlers(
     sigemptyset(&action.sa_mask);
     action.sa_flags = 0;
 
-    bool debugger_attached = signal_handler_thread_running_under_debugger();
-
     signal(SIGCHLD, SIG_IGN);
 
     for(uint8_t i = 0; i < program_signals_count; i++) {
@@ -119,44 +107,72 @@ char* signal_handler_thread_log_producer_set_early_prefix_thread(
     return prefix;
 }
 
-void* signal_handler_thread_func(
-        void* user_data) {
-    sigset_t waitset = { 0 };
+void signal_handler_thread_main_loop(
+        sigset_t *waitset,
+        struct timespec *timeout) {
     siginfo_t info = { 0 };
-    struct timespec timeout;
-
-    int_context = (signal_handler_thread_context_t*)user_data;
-
-    char* log_producer_early_prefix_thread =
-            signal_handler_thread_log_producer_set_early_prefix_thread(int_context);
-
-    if (pthread_setname_np(
-           pthread_self(),
-            "signal_handler") != 0) {
-        LOG_E(TAG, "Unable to set name of the signal handler thread");
-        LOG_E_OS_ERROR(TAG);
-    }
-
-    signal_handler_thread_register_signal_handlers(&waitset);
-    sigprocmask(SIG_BLOCK, &waitset, NULL);
-
-    // Use a timeout to ensure that the thread exit if it should terminate
-    timeout.tv_sec = 0;
-    timeout.tv_nsec = SIGNAL_HANDLER_THREAD_LOOP_MAX_WAIT_TIME_MS * 1000000;
 
     LOG_V(TAG, "Starting events loop");
 
     do {
-        int res = sigtimedwait(&waitset, &info, &timeout);
+        int res = sigtimedwait(waitset, &info, timeout);
 
         if (res == 0 && errno != EAGAIN) {
             FATAL(TAG, "Error while waiting for a signal");
         }
     } while(!signal_handler_thread_should_terminate(int_context));
 
-    LOG_V(TAG, "Tearing signal handler thread");
+    LOG_V(TAG, "Events loop ended");
+}
+
+void signal_handler_thread_setup_timeout(
+        struct timespec *timeout) {
+
+    // Use a timeout to ensure that the thread exit if it should terminate
+    timeout->tv_sec = 0;
+    timeout->tv_nsec = SIGNAL_HANDLER_THREAD_LOOP_MAX_WAIT_TIME_MS * 1000000;
+}
+
+void signal_handler_thread_set_thread_name() {
+    if (pthread_setname_np(
+            pthread_self(),
+            "signal_handler") != 0) {
+        LOG_E(TAG, "Unable to set name of the signal handler thread");
+        LOG_E_OS_ERROR(TAG);
+    }
+}
+
+void signal_handler_thread_mask_signals(
+        sigset_t *waitset) {
+    sigprocmask(SIG_BLOCK, waitset, NULL);
+}
+
+void signal_handler_thread_teardown(
+        char *log_producer_early_prefix_thread) {
+    // It's not really necessary to restore the signal mask or do anything else as the only
+    // reason for the thread to terminate is the the entire application is exiting.
+
+    LOG_V(TAG, "Tearing down signal handler thread");
 
     xalloc_free(log_producer_early_prefix_thread);
+}
+
+void* signal_handler_thread_func(
+        void* user_data) {
+    struct timespec timeout;
+    sigset_t waitset = { 0 };
+
+    int_context = (signal_handler_thread_context_t*)user_data;
+
+    char* log_producer_early_prefix_thread =
+            signal_handler_thread_log_producer_set_early_prefix_thread(int_context);
+
+    signal_handler_thread_set_thread_name();
+    signal_handler_thread_register_signal_handlers(&waitset);
+    signal_handler_thread_mask_signals(&waitset);
+    signal_handler_thread_setup_timeout(&timeout);
+    signal_handler_thread_main_loop(&waitset, &timeout);
+    signal_handler_thread_teardown(log_producer_early_prefix_thread);
 
     return NULL;
 }
--- src/signal_handler_thread.h
+++ src/signal_handler_thread.h
@@ -8,12 +8,38 @@ extern "C" {
 #define SIGNAL_HANDLER_THREAD_LOOP_MAX_WAIT_TIME_MS 500
 #define SIGNAL_HANDLER_THREAD_LOG_PRODUCER_PREFIX_FORMAT_STRING "[signal handler thread]"
 
-
 typedef struct signal_handler_thread_context signal_handler_thread_context_t;
 struct signal_handler_thread_context {
     pthread_t pthread;
     volatile bool *terminate_event_loop;
 };
+
+void signal_handler_thread_handle_signal(
+        int signal_number);
+
+bool signal_handler_thread_should_terminate(
+        signal_handler_thread_context_t *context);
+
+void signal_handler_thread_register_signal_handlers(
+        sigset_t *waitset);
+
+char* signal_handler_thread_log_producer_set_early_prefix_thread(
+        signal_handler_thread_context_t *context);
+
+void signal_handler_thread_main_loop(
+        sigset_t *waitset,
+        struct timespec *timeout);
+
+void signal_handler_thread_setup_timeout(
+        struct timespec *timeout);
+
+void signal_handler_thread_set_thread_name();
+
+void signal_handler_thread_mask_signals(
+        sigset_t *waitset);
+
+void signal_handler_thread_teardown(
+        char *log_producer_early_prefix_thread);
 
 void* signal_handler_thread_func(
         void* user_data);
