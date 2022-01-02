5df6592614f4188a7b09b9f2aa8125e9962dcdb7
Daniele Salvatore Albano <d.albano@gmail.com>
2022-01-02 18:17:47 +0300

    Introduce the network_op_result enum to let network op functions to indicate if the channel has to be closed, if the op was successful or if an error happened

--- src/worker/network/worker_network.h
+++ src/worker/network/worker_network.h
@@ -5,21 +5,28 @@
 extern "C" {
 #endif
 
+enum network_op_result {
+    NETWORK_OP_RESULT_OK,
+    NETWORK_OP_RESULT_CLOSE_SOCKET,
+    NETWORK_OP_RESULT_ERROR,
+};
+typedef enum network_op_result network_op_result_t;
+
 void worker_network_listeners_initialize(
         worker_context_t *worker_context);
 
 void worker_network_listeners_listen(
         worker_context_t *worker_context);
 
-bool worker_network_receive(
+network_op_result_t worker_network_receive(
         network_channel_t *channel);
 
-bool worker_network_send(
+network_op_result_t worker_network_send(
         network_channel_t *channel,
         network_channel_buffer_data_t *buffer,
         size_t buffer_length);
 
-bool worker_network_close(
+network_op_result_t worker_network_close(
         network_channel_t *channel);
 
 bool worker_network_protocol_process_events(
