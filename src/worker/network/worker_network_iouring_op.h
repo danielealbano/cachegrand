b8952c32804893af13ceaeba466d5f6d7521475c
Daniele Salvatore Albano <d.albano@gmail.com>
2022-01-05 15:24:58 +0300

    Expose in worker_network_iouring_op.h all the functions defined in the c file

--- src/worker/network/worker_network_iouring_op.h
+++ src/worker/network/worker_network_iouring_op.h
@@ -5,10 +5,31 @@
 extern "C" {
 #endif
 
+void worker_network_iouring_op_network_post_close(
+        network_channel_iouring_t *channel);
+
+network_channel_t* worker_network_iouring_op_network_accept_setup_new_channel(
+        worker_iouring_context_t *context,
+        network_channel_iouring_t *listener_channel,
+        network_channel_iouring_t *new_channel,
+        io_uring_cqe_t *cqe);
+
 network_channel_t* worker_network_iouring_op_network_accept(
         network_channel_t *listener_channel);
 
-void worker_network_iouring_op_register();
+bool worker_network_iouring_op_network_close(
+        network_channel_t *channel,
+        bool shutdown_may_fail);
+
+size_t worker_network_iouring_op_network_receive(
+        network_channel_t *channel,
+        char* buffer,
+        size_t buffer_length);
+
+size_t worker_network_iouring_op_network_send(
+        network_channel_t *channel,
+        char* buffer,
+        size_t buffer_length);
 
 bool worker_network_iouring_initialize(
         worker_context_t *worker_context);
