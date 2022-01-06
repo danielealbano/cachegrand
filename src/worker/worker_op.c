39f1173432f835c63477a0ae34de4a0c5dad139c
Daniele Salvatore Albano <d.albano@gmail.com>
2022-01-06 08:54:37 +0300

    Implement the worker storage operation

--- src/worker/worker_op.c
+++ src/worker/worker_op.c
@@ -20,13 +20,17 @@
 #include "network/protocol/network_protocol.h"
 #include "network/io/network_io_common.h"
 #include "network/channel/network_channel.h"
+#include "storage/io/storage_io_common.h"
+#include "storage/channel/storage_channel.h"
 #include "worker/worker_common.h"
-#include "worker/worker_op.h"
 #include "fiber_scheduler.h"
+#include "worker/worker_op.h"
 
 #define TAG "worker_op"
 
 worker_op_timer_fp_t* worker_op_timer;
+
+// Network operations
 worker_op_network_channel_new_fp_t* worker_op_network_channel_new;
 worker_op_network_channel_multi_new_fp_t* worker_op_network_channel_multi_new;
 worker_op_network_channel_multi_get_fp_t* worker_op_network_channel_multi_get;
@@ -36,6 +40,13 @@ worker_op_network_accept_fp_t* worker_op_network_accept;
 worker_op_network_receive_fp_t* worker_op_network_receive;
 worker_op_network_send_fp_t* worker_op_network_send;
 worker_op_network_close_fp_t* worker_op_network_close;
+
+// File operations
+worker_op_storage_open_fp_t* worker_op_storage_open;
+worker_op_storage_read_fp_t* worker_op_storage_read;
+worker_op_storage_write_fp_t* worker_op_storage_write;
+worker_op_storage_flush_fp_t* worker_op_storage_flush;
+worker_op_storage_close_fp_t* worker_op_storage_close;
 
 void worker_timer_fiber_entrypoint(
         void *user_data) {
