ac4e3b1a448704f305fbe911f597f59421e41d05
Daniele Salvatore Albano <d.albano@gmail.com>
2022-01-07 09:48:12 +0300

    Implement tests for the storage channel io_uring

---
+++ tests/storage/channel/test-storage-channel-iouring.cpp
@@ -0,0 +1,17 @@
+#include <catch2/catch.hpp>
+
+#include <arpa/inet.h>
+
+#include "storage/io/storage_io_common.h"
+#include "storage/channel/storage_channel.h"
+#include "storage/channel/storage_channel_iouring.h"
+
+TEST_CASE("storage/channel/storage_channel_iouring.c", "[storage][channel][storage_channel_iouring]") {
+    SECTION("storage_channel_iouring_new") {
+        storage_channel_iouring_t* storage_channel_iouring = storage_channel_iouring_new();
+
+        REQUIRE(storage_channel_iouring != NULL);
+
+        storage_channel_iouring_free(storage_channel_iouring);
+    }
+}
