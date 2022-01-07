b4d91eaeba384313d198a0a0ca2832269fbdb29a
Daniele Salvatore Albano <d.albano@gmail.com>
2022-01-07 09:48:24 +0300

    Fix a test name in network channel io_uring

--- tests/network/channel/test-network-channel-iouring.cpp
+++ tests/network/channel/test-network-channel-iouring.cpp
@@ -18,7 +18,7 @@ TEST_CASE("network/channel/network_channel_iouring.c", "[network][channel][netwo
         network_channel_iouring_free(network_channel_iouring);
     }
 
-    SECTION("network_channel_iouring_new_new") {
+    SECTION("network_channel_iouring_multi_new") {
         network_channel_iouring_t* network_channel_iouring = network_channel_iouring_multi_new(3);
 
         REQUIRE(network_channel_iouring != NULL);
