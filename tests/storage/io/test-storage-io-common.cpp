9be8e620d9e707dd6e07d38a624a3a478d19ebb3
Daniele Salvatore Albano <d.albano@gmail.com>
2022-01-07 09:53:14 +0300

    Implement tests for storage-io-common

---
+++ tests/storage/io/test-storage-io-common.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch.hpp>
+#include <unistd.h>
+
+#include "storage/io/storage_io_common.h"
+
+TEST_CASE("storage/io/storage_io_common.c", "[storage][storage_io][storage_io_common]") {
+    SECTION("storage_io_common_close") {
+        char fixture_temp_path[] = "/tmp/cachegrand-tests-XXXXXX.tmp";
+        int fixture_temp_path_suffix_len = 4;
+
+        SECTION("invalid fd") {
+            int fd = -1;
+            REQUIRE(storage_io_common_close(fd) == false);
+        }
+
+        SECTION("valid fd") {
+            int fd = mkstemps(fixture_temp_path, fixture_temp_path_suffix_len);
+            REQUIRE(storage_io_common_close(fd));
+            unlink(fixture_temp_path);
+        }
+    }
+}
