ec857f888f97145c2611fb4ab0483a4cf5de4b3a
Daniele Salvatore Albano <d.albano@gmail.com>
2022-01-16 18:25:24 +0300

    Add testing for storage_channel new functions

--- tests/storage/channel/test-storage-channel.cpp
+++ tests/storage/channel/test-storage-channel.cpp
@@ -9,4 +9,23 @@ TEST_CASE("storage/channel/storage_channel.c", "[storage][storage_channel][stora
 
         REQUIRE(storage_channel_init(&storage_channel));
     }
+
+    SECTION("storage_channel_new") {
+        storage_channel_t* storage_channel = storage_channel_new();
+
+        REQUIRE(storage_channel != NULL);
+
+        storage_channel_free(storage_channel);
+    }
+
+    SECTION("storage_channel_multi_new") {
+        storage_channel_t* storage_channel = storage_channel_multi_new(3);
+
+        REQUIRE(storage_channel != NULL);
+        for(int i = 0; i < 3; i++) {
+            REQUIRE(storage_channel[i].fd == 0);
+        }
+
+        storage_channel_free(storage_channel);
+    }
 }
