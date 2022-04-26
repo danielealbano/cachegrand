0a746aac1df1b3d6f0feb584c3124db0d042c058
Daniele Salvatore Albano <d.albano@gmail.com>
2022-04-27 00:14:02 +0200

    Update the redis get and set commands to support operations over the storage_db

--- src/network/protocol/redis/command/network_protocol_redis_command_get.c
+++ src/network/protocol/redis/command/network_protocol_redis_command_get.c
@@ -41,9 +41,16 @@
 NETWORK_PROTOCOL_REDIS_COMMAND_FUNCPTR_END(get) {
     char *send_buffer, *send_buffer_start, *send_buffer_end;
     size_t send_buffer_length;
-    // TODO: just an hack, the storage system is missing
-    size_t *value_length;
     uintptr_t memptr = 0;
+    storage_db_entry_index_t *entry_index = NULL;
+    storage_db_chunk_info_t *chunk_info = NULL;
+
+    send_buffer = slab_allocator_mem_alloc(STORAGE_DB_CHUNK_MAX_SIZE);
+    send_buffer_length = STORAGE_DB_CHUNK_MAX_SIZE;
+    send_buffer_start = send_buffer;
+    send_buffer_end = send_buffer_start + send_buffer_length;
+
+    storage_db_t *db = worker_context_get()->db;
 
     bool res = hashtable_mcmp_op_get(
             hashtable,
@@ -52,20 +59,92 @@ NETWORK_PROTOCOL_REDIS_COMMAND_FUNCPTR_END(get) {
             &memptr);
 
     if (res) {
-        value_length = (size_t*)memptr;
-        memptr += sizeof(size_t);
-    }
+        entry_index = (storage_db_entry_index_t*)memptr;
 
-    send_buffer_length = (res ? *value_length : 0) + 256;
-    send_buffer = send_buffer_start = slab_allocator_mem_alloc(send_buffer_length);
-    send_buffer_end = send_buffer_start + send_buffer_length;
-
-    if (res) {
-        send_buffer_start = protocol_redis_writer_write_blob_string(
+        // Prepend the blog start in the buffer which is never sent at the very beginning because there will always be
+        // a chunk big enough to hold always at least the necessary protocol data and 1 database chunk.
+        send_buffer_start = protocol_redis_writer_write_argument_blob_start(
                 send_buffer_start,
-                send_buffer_end - send_buffer_start,
-                (char*)memptr,
-                (int)(*value_length));
+                send_buffer_length,
+                false,
+                (int)entry_index->value_length);
+
+        chunk_info = storage_db_entry_value_chunk_get(entry_index, 0);
+
+        // Check if the first chunk fits into the buffer, will always be fails if there is more than one chunk
+        if (chunk_info->chunk_length + (send_buffer_start - send_buffer) > send_buffer_length) {
+            if (network_send(
+                    channel,
+                    send_buffer,
+                    send_buffer_start - send_buffer) != NETWORK_OP_RESULT_OK) {
+                LOG_E(TAG, "[REDIS][GET] Critical error, unable to send argument blob start");
+
+                goto fail;
+            }
+
+            send_buffer_start = send_buffer;
+        }
+
+        // Build the chunks for the value
+        for(storage_db_chunk_index_t chunk_index = 0; chunk_index < entry_index->key_chunks_count; chunk_index++) {
+            chunk_info = storage_db_entry_value_chunk_get(entry_index, chunk_index);
+            res = storage_db_entry_chunk_read(
+                    db,
+                    chunk_info,
+                    send_buffer_start);
+
+            if (!res) {
+                LOG_E(
+                        TAG,
+                        "[REDIS][GET] Critical error, unable to read chunk <%u> at offset <%u> long <%u> bytes from shard <%u>",
+                        chunk_index,
+                        chunk_info->chunk_offset,
+                        chunk_info->chunk_length,
+                        chunk_info->shard_index);
+
+                goto fail;
+            }
+
+            send_buffer_start += chunk_info->chunk_length;
+
+            // Check if it's the last chunk and if the the argument blob terminator (a \r\n) will fit in the current
+            // buffer or not, if it's the case will skip the network send to avoid a fiber switch
+
+            if (!(entry_index->value_chunks_count == chunk_index + 1 &&
+                chunk_info->chunk_length + (send_buffer_start - send_buffer) + 2 > send_buffer_length)) {
+                if (network_send(
+                        channel,
+                        send_buffer,
+                        send_buffer_start - send_buffer) != NETWORK_OP_RESULT_OK) {
+                    LOG_E(
+                            TAG,
+                            "[REDIS][GET] Critical error, unable to send chunk <%u> at offset <%u> long <%u> bytes from shard <%u>",
+                            chunk_index,
+                            chunk_info->chunk_offset,
+                            chunk_info->chunk_length,
+                            chunk_info->shard_index);
+
+                    goto fail;
+                }
+
+                send_buffer_start = send_buffer;
+            }
+        }
+
+        send_buffer_start = protocol_redis_writer_write_argument_blob_end(
+                send_buffer_start,
+                send_buffer_end - send_buffer_start);
+
+        if (network_send(
+                channel,
+                send_buffer,
+                send_buffer_start - send_buffer) != NETWORK_OP_RESULT_OK) {
+            LOG_E(
+                    TAG,
+                    "[REDIS][GET] Critical error, unable to send blob argument terminator");
+
+            goto fail;
+        }
     } else {
         if (protocol_context->resp_version == PROTOCOL_REDIS_RESP_VERSION_2) {
             send_buffer_start = protocol_redis_writer_write_blob_string_null(
@@ -76,20 +155,23 @@ NETWORK_PROTOCOL_REDIS_COMMAND_FUNCPTR_END(get) {
                     send_buffer_start,
                     send_buffer_end - send_buffer_start);
         }
+
+        if (network_send(
+                channel,
+                send_buffer,
+                send_buffer_start - send_buffer) != NETWORK_OP_RESULT_OK) {
+            LOG_E(
+                    TAG,
+                    "[REDIS][GET] Critical error, unable to send blob argument");
+
+            goto fail;
+        }
     }
 
-    if (send_buffer_start == NULL) {
-        LOG_E(TAG, "buffer length incorrectly calculated, not enough space!");
-        slab_allocator_mem_free(send_buffer);
-        return false;
-    }
-
-    if (network_send(
-            channel,
-            send_buffer,
-            send_buffer_start - send_buffer) != NETWORK_OP_RESULT_OK) {
-        return false;
-    }
-
+    slab_allocator_mem_free(send_buffer);
     return true;
+
+fail:
+    slab_allocator_mem_free(send_buffer);
+    return false;
 }
