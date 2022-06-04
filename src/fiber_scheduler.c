6f050bca71a4115af6682fb786ca6924a6837098
Daniele Salvatore Albano <d.albano@gmail.com>
2022-06-04 21:25:36 +0200

    Drop the code related to updating the fiber execution costs related counters

--- src/fiber_scheduler.c
+++ src/fiber_scheduler.c
@@ -171,27 +171,14 @@ void fiber_scheduler_switch_to(
     unsigned stack_id = VALGRIND_STACK_REGISTER(fiber->stack_base, fiber->stack_base + fiber->stack_size);
 #endif
 
-    uint32_t aux = 0;
-    uint64_t cost_cycles_start = intrinsic_rdtscp(&aux);
-
     // Switch to the new fiber
     fiber_context_swap(
             previous_fiber,
             fiber);
 
-    uint64_t cost_cycles_end = intrinsic_rdtscp(&aux);
-
 #if defined(HAS_VALGRIND)
     VALGRIND_STACK_DEREGISTER(stack_id);
 #endif
-
-    if (likely(cost_cycles_end > cost_cycles_start)) {
-        fiber->cost.cycles = cost_cycles_end - cost_cycles_start;
-    } else if (cost_cycles_end < cost_cycles_start) {
-        fiber->cost.cycles = cost_cycles_start - cost_cycles_end;
-    } else {
-        fiber->cost.cycles = UINT64_MAX;
-    }
 
     LOG_D(TAG, "Switching back from fiber <%s>, file <%s:%d>, to fiber <%s>, file <%s:%d>",
           fiber->name,
