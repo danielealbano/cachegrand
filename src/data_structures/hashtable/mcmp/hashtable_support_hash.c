04a5650101b45a73a0591e2edef3eeb3d744bb77
Daniele Salvatore Albano <d.albano@gmail.com>
2022-06-05 17:53:48 +0200

    Add support to use xxh3 if selected

--- src/data_structures/hashtable/mcmp/hashtable_support_hash.c
+++ src/data_structures/hashtable/mcmp/hashtable_support_hash.c
@@ -12,6 +12,8 @@
 
 #if CACHEGRAND_CMAKE_CONFIG_USE_HASH_ALGORITHM_T1HA2 == 1
 #include "t1ha.h"
+#elif CACHEGRAND_CMAKE_CONFIG_USE_HASH_ALGORITHM_XXH3 == 1
+#include "xxhash.h"
 #elif CACHEGRAND_CMAKE_CONFIG_USE_HASH_ALGORITHM_CRC32C == 1
 #include "hash/hash_crc32c.h"
 #else
@@ -27,6 +29,8 @@
 hashtable_hash_t hashtable_mcmp_support_hash_calculate(hashtable_key_data_t *key, hashtable_key_size_t key_size) {
 #if CACHEGRAND_CMAKE_CONFIG_USE_HASH_ALGORITHM_T1HA2 == 1
     return (hashtable_hash_t)t1ha2_atonce(key, key_size, HASHTABLE_SUPPORT_HASH_SEED);
+#elif CACHEGRAND_CMAKE_CONFIG_USE_HASH_ALGORITHM_XXH3 == 1
+    return (hashtable_hash_t)XXH3_64bits_withSeed(key, key_size, HASHTABLE_SUPPORT_HASH_SEED);
 #elif CACHEGRAND_CMAKE_CONFIG_USE_HASH_ALGORITHM_CRC32C == 1
     uint32_t crc32 = hash_crc32c(key, key_size, HASHTABLE_SUPPORT_HASH_SEED);
     hashtable_hash_t hash = ((uint64_t)hash_crc32c(key, key_size, crc32) << 32u) | crc32;
