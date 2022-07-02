df7c589ea4a0f6db07b6c082379a75ceff7e91ea
Daniele Salvatore Albano <d.albano@gmail.com>
2022-07-02 18:36:16 +0200

    Fix includes, now network_channel is dependant on config

--- src/network/channel/network_channel_iouring.c
+++ src/network/channel/network_channel_iouring.c
@@ -17,6 +17,7 @@
 #include "data_structures/hashtable/mcmp/hashtable.h"
 #include "slab_allocator.h"
 #include "xalloc.h"
+#include "config.h"
 #include "network/protocol/network_protocol.h"
 #include "network/io/network_io_common.h"
 #include "network/channel/network_channel.h"
