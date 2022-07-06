46cfb9f66dc3b784f5ac1d9e0844164067a90292
Vito Castellano <vito.castellano@facile.it>
2022-07-06 10:51:58 +0200

    Restore correct file of libcyaml

--- 3rdparty/libcyaml.cmake
+++ 3rdparty/libcyaml.cmake
@@ -17,8 +17,8 @@ ExternalProject_Add(
         PREFIX ${CMAKE_BINARY_DIR}/_deps
         CONFIGURE_COMMAND ""
         BUILD_COMMAND
-            sed -e "s/VERSION_DEVEL = 1/VERSION_DEVEL = 0/" -i ${LIBCYAML_SRC_PATH}/Makefile &&
-            make -C ${LIBCYAML_SRC_PATH} ${LIBCYAML_BUILD_PATH_REL}/libcyaml.a ${LIBCYAML_BUILD_PATH_REL}/libcyaml.so.1
+        sed -e "s/VERSION_DEVEL = 1/VERSION_DEVEL = 0/" -i ${LIBCYAML_SRC_PATH}/Makefile &&
+        CFLAGS="-fPIC" make -C ${LIBCYAML_SRC_PATH} ${LIBCYAML_BUILD_PATH_REL}/libcyaml.a ${LIBCYAML_BUILD_PATH_REL}/libcyaml.so.1
         INSTALL_COMMAND "")
 
 find_library(LIBCYAML_LIBRARY_DIRS NAMES yaml NAMES_PER_DIR)
