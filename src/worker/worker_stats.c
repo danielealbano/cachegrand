3a68a4f596517d289eaf993aee7a1c3e5468fe60
Daniele Salvatore Albano <d.albano@gmail.com>
2022-07-07 19:38:06 +0200

    Add a function to aggregate the statistics of all the workers

--- src/worker/worker_stats.c
+++ src/worker/worker_stats.c
@@ -21,6 +21,7 @@
 #include "fiber.h"
 #include "data_structures/small_circular_queue/small_circular_queue.h"
 #include "data_structures/double_linked_list/double_linked_list.h"
+#include "slab_allocator.h"
 #include "data_structures/hashtable/mcmp/hashtable.h"
 #include "network/protocol/network_protocol.h"
 #include "network/io/network_io_common.h"
@@ -28,6 +29,10 @@
 #include "storage/io/storage_io_common.h"
 #include "storage/channel/storage_channel.h"
 #include "storage/db/storage_db.h"
+#include "worker/worker_stats.h"
+#include "worker/worker_context.h"
+#include "signal_handler_thread.h"
+#include "program.h"
 
 // Needed to be defined there as there is a recursive dependency between worker_context and worker_stats
 typedef struct worker_context worker_context_t;
@@ -64,4 +69,66 @@ bool worker_stats_should_publish(
 worker_stats_t *worker_stats_get() {
     worker_context_t *context = worker_context_get();
     return &context->stats.internal;
+}
+
+worker_stats_t *worker_stats_aggregate(
+        worker_stats_t *aggregated_stats) {
+    program_context_t *program_context = program_get_context();
+
+    // TODO: The access should be synced (e.g. with a spinlock) as the workers might decide to update the stats while
+    //       they are being read providing potentially messed up data
+    for(uint32_t index = 0; index < program_context->workers_count; index++) {
+        worker_stats_volatile_t *worker_stats_shared = &program_context->workers_context[index].stats.shared;
+
+        aggregated_stats->network.total.received_packets +=
+                worker_stats_shared->network.total.received_packets;
+        aggregated_stats->network.total.received_data +=
+                worker_stats_shared->network.total.received_data;
+        aggregated_stats->network.total.sent_packets +=
+                worker_stats_shared->network.total.sent_packets;
+        aggregated_stats->network.total.sent_data +=
+                worker_stats_shared->network.total.sent_data;
+        aggregated_stats->network.total.accepted_connections +=
+                worker_stats_shared->network.total.accepted_connections;
+        aggregated_stats->network.total.active_connections +=
+                worker_stats_shared->network.total.active_connections;
+        aggregated_stats->network.per_minute.received_packets +=
+                worker_stats_shared->network.per_minute.received_packets;
+        aggregated_stats->network.per_minute.received_data +=
+                worker_stats_shared->network.per_minute.received_data;
+        aggregated_stats->network.per_minute.sent_data +=
+                worker_stats_shared->network.per_minute.sent_data;
+        aggregated_stats->network.per_minute.sent_packets +=
+                worker_stats_shared->network.per_minute.sent_packets;
+        aggregated_stats->network.per_minute.accepted_connections +=
+                worker_stats_shared->network.per_minute.accepted_connections;
+        aggregated_stats->storage.total.written_data +=
+                worker_stats_shared->storage.total.written_data;
+        aggregated_stats->storage.total.write_iops +=
+                worker_stats_shared->storage.total.write_iops;
+        aggregated_stats->storage.total.read_data +=
+                worker_stats_shared->storage.total.read_data;
+        aggregated_stats->storage.total.read_iops +=
+                worker_stats_shared->storage.total.read_iops;
+        aggregated_stats->storage.total.open_files +=
+                worker_stats_shared->storage.total.open_files;
+        aggregated_stats->storage.per_minute.written_data +=
+                worker_stats_shared->storage.per_minute.written_data;
+        aggregated_stats->storage.per_minute.write_iops +=
+                worker_stats_shared->storage.per_minute.write_iops;
+        aggregated_stats->storage.per_minute.read_data +=
+                worker_stats_shared->storage.per_minute.read_data;
+        aggregated_stats->storage.per_minute.read_iops +=
+                worker_stats_shared->storage.per_minute.read_iops;
+
+        if (worker_stats_shared->last_update_timestamp.tv_sec > aggregated_stats->last_update_timestamp.tv_sec) {
+            aggregated_stats->last_update_timestamp.tv_sec =
+                    worker_stats_shared->last_update_timestamp.tv_sec;
+        }
+    }
+
+    aggregated_stats->started_on_timestamp.tv_sec =
+            program_context->workers_context[0].stats.shared.started_on_timestamp.tv_sec;
+
+    return aggregated_stats;
 }
