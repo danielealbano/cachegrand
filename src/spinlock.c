a88a1c605a942ff1e4f44b757420e1e06b9673d5
Daniele Salvatore Albano <d.albano@gmail.com>
2022-07-13 22:33:34 +0200

    Drop the flags from the spinlocks, as they were used only in tests, and improve incorrect unlocking detection

--- src/spinlock.c
+++ src/spinlock.c
@@ -31,18 +31,16 @@
 void spinlock_init(
         spinlock_lock_volatile_t* spinlock) {
     spinlock->lock = SPINLOCK_UNLOCKED;
-    spinlock->predicted_spins = 0;
 }
 
 void spinlock_unlock(
         spinlock_lock_volatile_t* spinlock) {
 #if DEBUG == 1
     long thread_id = syscall(__NR_gettid);
-    uint8_t expected_lock = (uint8_t)thread_id;
+    uint32_t expected_lock = (uint32_t)thread_id;
     assert(spinlock->lock == expected_lock);
 #endif
 
-    spinlock->flags = 0;
     spinlock->lock = SPINLOCK_UNLOCKED;
     MEMORY_FENCE_STORE();
 }
@@ -57,43 +55,14 @@ bool spinlock_try_lock(
         spinlock_lock_volatile_t *spinlock) {
 #if DEBUG == 1
     long thread_id = syscall(__NR_gettid);
-    uint8_t new_value = thread_id;
-    uint8_t expected_value = SPINLOCK_UNLOCKED;
+    uint32_t new_value = thread_id;
+    uint32_t expected_value = SPINLOCK_UNLOCKED;
 #else
-    uint8_t new_value = SPINLOCK_LOCKED;
-    uint8_t expected_value = SPINLOCK_UNLOCKED;
+    uint32_t new_value = SPINLOCK_LOCKED;
+    uint32_t expected_value = SPINLOCK_UNLOCKED;
 #endif
 
     return __sync_bool_compare_and_swap(&spinlock->lock, expected_value, new_value);
-}
-
-void spinlock_set_flag(
-        spinlock_lock_volatile_t *spinlock,
-        spinlock_flag_t flag) {
-    spinlock_flag_t old_flags, new_flags;
-    old_flags = spinlock->flags;
-    do {
-        new_flags = old_flags | flag;
-    } while(__sync_val_compare_and_swap(&spinlock->flags, old_flags, new_flags) != old_flags);
-}
-
-void spinlock_unset_flag(
-        spinlock_lock_volatile_t *spinlock,
-        spinlock_flag_t flag) {
-    spinlock_flag_t old_flags, new_flags;
-    old_flags = spinlock->flags;
-    do {
-        new_flags = old_flags & ~flag;
-    } while(__sync_val_compare_and_swap(&spinlock->flags, old_flags, new_flags) != old_flags);
-}
-
-bool spinlock_has_flag(
-        spinlock_lock_volatile_t *spinlock,
-        spinlock_flag_t flag) {
-    MEMORY_FENCE_LOAD();
-    spinlock_flag_t flags = spinlock->flags;
-
-    return (flags & flag) == flag;
 }
 
 bool spinlock_lock_internal(
@@ -111,11 +80,7 @@ bool spinlock_lock_internal(
             spins++;
         }
 
-        // TODO: implement spinlock auto balancing using the predicted_spins property of the lock struct
-
         if (spins == max_spins_before_probably_stuck) {
-            spinlock_set_flag(spinlock, SPINLOCK_FLAG_POTENTIALLY_STUCK);
-
             LOG_E(TAG, "Possible stuck spinlock detected for thread %lu in %s at %s:%u",
                     pthread_self(), src_func, src_path, src_line);
         }
