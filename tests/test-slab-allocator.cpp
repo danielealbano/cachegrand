4a287bdaa77ec59f17e95368c05247e39bec8627
Daniele Salvatore Albano <d.albano@gmail.com>
2022-07-17 18:00:09 +0200

    Implement multi threaded fuzzy testing for the slab allocator

--- tests/test-slab-allocator.cpp
+++ tests/test-slab-allocator.cpp
@@ -9,11 +9,10 @@
 #include <catch2/catch.hpp>
 
 #include <cstring>
-#include <ctime>
-#include <unistd.h>
 
 #include "exttypes.h"
 #include "spinlock.h"
+#include "memory_fences.h"
 #include "data_structures/double_linked_list/double_linked_list.h"
 #include "data_structures/queue_mpmc/queue_mpmc.h"
 #include "xalloc.h"
@@ -22,11 +21,29 @@
 #include "hugepages.h"
 #include "hugepage_cache.h"
 #include "thread.h"
+#include "utils_cpu.h"
+#include "log/log.h"
+#include "fatal.h"
 
 #include "slab_allocator.h"
 
-typedef struct mem_alloc_test_data mem_alloc_test_data_t;
-struct mem_alloc_test_data {
+typedef struct test_slab_allocator_fuzzy_test_thread_info test_slab_allocator_fuzzy_test_thread_info_t;
+struct test_slab_allocator_fuzzy_test_thread_info {
+    pthread_t thread;
+    uint32_t cpu_index;
+    bool_volatile_t *start;
+    bool_volatile_t *stop;
+    bool_volatile_t stopped;
+    uint32_t min_used_slots;
+    uint32_t max_used_slots;
+    uint32_t object_size;
+    bool can_place_signature_at_end;
+    uint32_volatile_t *ops_counter_total;
+    uint32_volatile_t *ops_counter_mem_alloc;
+};
+
+typedef struct test_slab_allocator_fuzzy_test_data test_slab_allocator_fuzzy_test_data_t;
+struct test_slab_allocator_fuzzy_test_data {
     uint32_t ops_counter_total;
     uint32_t ops_counter_mem_alloc;
     uint64_t hash_data_x;
@@ -52,19 +69,188 @@ uint64_t test_slab_allocator_calc_hash_y(
     return y;
 }
 
+void *test_slab_allocator_fuzzy_multi_thread_single_size_thread_func(
+        void *user_data) {
+    test_slab_allocator_fuzzy_test_thread_info_t *ti = (test_slab_allocator_fuzzy_test_thread_info_t *)user_data;
+
+    uint32_t min_used_slots = ti->min_used_slots;
+    uint32_t max_used_slots = ti->max_used_slots;
+    uint32_t object_size = ti->object_size;
+    bool can_place_signature_at_end = ti->can_place_signature_at_end;
+
+    thread_current_set_affinity(ti->cpu_index);
+
+    double_linked_list_t *list = double_linked_list_init();
+
+    do {
+        MEMORY_FENCE_LOAD();
+    } while (!*ti->start);
+
+    while (!*ti->stop) {
+        MEMORY_FENCE_LOAD();
+        uint32_t ops_counter_total = __atomic_fetch_add(ti->ops_counter_total, 1, __ATOMIC_RELAXED);
+
+        if (list->count < min_used_slots ||
+            (random_generate() % 1000 > 500 && list->count < max_used_slots)) {
+            // allocate memory
+            uint32_t ops_counter_mem_alloc = __atomic_fetch_add(ti->ops_counter_mem_alloc, 1, __ATOMIC_RELAXED);
+
+            void* memptr = slab_allocator_mem_alloc_zero(object_size);
+
+            test_slab_allocator_fuzzy_test_data_t *data = (test_slab_allocator_fuzzy_test_data_t*)memptr;
+            data->ops_counter_total = ops_counter_total;
+            data->ops_counter_mem_alloc = ops_counter_mem_alloc;
+            data->hash_data_x = test_slab_allocator_calc_hash_x(ops_counter_total);
+            data->hash_data_y = test_slab_allocator_calc_hash_y(ops_counter_mem_alloc);
+            data->memptr = memptr;
+
+            if (can_place_signature_at_end) {
+                memcpy(
+                        (void*)((uintptr_t)memptr + object_size - sizeof(test_slab_allocator_fuzzy_test_data_t)),
+                        memptr,
+                        sizeof(test_slab_allocator_fuzzy_test_data_t));
+            }
+
+            double_linked_list_item_t *item = double_linked_list_item_init();
+            item->data = memptr;
+            double_linked_list_push_item(list, item);
+        } else {
+            double_linked_list_item_t *item = double_linked_list_shift_item(list);
+
+            test_slab_allocator_fuzzy_test_data_t *data = (test_slab_allocator_fuzzy_test_data_t*)item->data;
+
+            uint64_t hash_data_x = test_slab_allocator_calc_hash_x(data->ops_counter_total);
+            uint64_t hash_data_y = test_slab_allocator_calc_hash_y(data->ops_counter_mem_alloc);
+
+            if (data->hash_data_x != hash_data_x) {
+                // Can't use require as this code runs inside a thread, not allowed by Catch2
+                FATAL("test-slab-allocator", "Incorrect hash x");
+            }
+
+            if (data->hash_data_y != hash_data_y) {
+                // Can't use require as this code runs inside a thread, not allowed by Catch2
+                FATAL("test-slab-allocator", "Incorrect hash y");
+            }
+
+            if (data->memptr != item->data) {
+                // Can't use require as this code runs inside a thread, not allowed by Catch2
+                FATAL("test-slab-allocator", "Incorrect memory pointer");
+            }
+
+            if (can_place_signature_at_end) {
+                int res = memcmp(
+                        (void*)((uintptr_t)data->memptr + object_size - sizeof(test_slab_allocator_fuzzy_test_data_t)),
+                        data->memptr,
+                        sizeof(test_slab_allocator_fuzzy_test_data_t));
+                if (res != 0) {
+                    // Can't use require as this code runs inside a thread, not allowed by Catch2
+                    FATAL("test-slab-allocator", "Incorrect signature");
+                }
+            }
+
+            slab_allocator_mem_free(item->data);
+            double_linked_list_item_free(item);
+        }
+    }
+
+    ti->stopped = true;
+    MEMORY_FENCE_STORE();
+
+    return nullptr;
+}
+
+void test_slab_allocator_fuzzy_multi_thread_single_size(
+        uint32_t duration,
+        uint32_t object_size,
+        uint32_t min_used_slots,
+        uint32_t use_max_hugepages) {
+    uint32_t ops_counter_total = 0, ops_counter_mem_alloc = 0;
+    timespec_t start_time, current_time, diff_time;
+    queue_mpmc_t *queue_mpmc = queue_mpmc_init();
+    bool start = false;
+    bool stop = false;
+    int n_cpus = utils_cpu_count();
+
+    assert(object_size >= sizeof(test_slab_allocator_fuzzy_test_data_t));
+
+    uint32_t max_used_slots =
+            (use_max_hugepages * HUGEPAGE_SIZE_2MB) /
+            (object_size + sizeof(slab_slot_t));
+
+    bool can_place_signature_at_end = object_size > (sizeof(test_slab_allocator_fuzzy_test_data_t) * 2);
+
+    test_slab_allocator_fuzzy_test_thread_info_t *ti_list =
+            (test_slab_allocator_fuzzy_test_thread_info_t*)malloc(sizeof(test_slab_allocator_fuzzy_test_thread_info_t) * n_cpus);
+
+    for(int i = 0; i < n_cpus; i++) {
+        test_slab_allocator_fuzzy_test_thread_info_t *ti = &ti_list[i];
+
+        ti->cpu_index = i;
+        ti->start = &start;
+        ti->stop = &stop;
+        ti->stopped = false;
+        ti->min_used_slots = min_used_slots;
+        ti->max_used_slots = max_used_slots;
+        ti->object_size = object_size;
+        ti->can_place_signature_at_end = can_place_signature_at_end;
+        ti->ops_counter_total = &ops_counter_total;
+        ti->ops_counter_mem_alloc = &ops_counter_mem_alloc;
+
+        if (pthread_create(
+                &ti->thread,
+                nullptr,
+                test_slab_allocator_fuzzy_multi_thread_single_size_thread_func,
+                ti) != 0) {
+            REQUIRE(false);
+        }
+    }
+
+    start = true;
+    MEMORY_FENCE_STORE();
+
+    clock_monotonic(&start_time);
+
+    do {
+        clock_monotonic(&current_time);
+        sched_yield();
+
+        clock_diff(&diff_time, &current_time, &start_time);
+    } while(diff_time.tv_sec < duration);
+
+    stop = true;
+    MEMORY_FENCE_STORE();
+
+    bool stopped;
+    do {
+        stopped = true;
+        sched_yield();
+
+        // wait for all the threads to stop
+        for(int i = 0; i < n_cpus && stopped; i++) {
+            MEMORY_FENCE_LOAD();
+            if (!ti_list[i].stopped) {
+                stopped = false;
+                continue;
+            }
+        }
+    } while(!stopped);
+
+    queue_mpmc_free(queue_mpmc);
+    free(ti_list);
+}
+
 void test_slab_allocator_fuzzy_single_thread_single_size(
         uint32_t duration,
-        size_t object_size,
+        uint32_t object_size,
         uint32_t min_used_slots,
         uint32_t use_max_hugepages) {
     timespec_t start_time, current_time, diff_time;
     uint32_t ops_counter_total = 0,
-            ops_counter_mem_alloc = 0,
-            ops_counter_mem_free = 0;
+            ops_counter_mem_alloc = 0;
 
     // The object size must be large enough to hold the test data, 32 bytes, so it's not
     // possible to test the 16 bytes slab allocator
-    assert(object_size >= sizeof(mem_alloc_test_data_t));
+    assert(object_size >= sizeof(test_slab_allocator_fuzzy_test_data_t));
 
     // The calculation for the max slots to use is not 100% correct as it doesn't take into
     // account the slab header (64 bytes) and the padding placed before the actual data to
@@ -77,7 +263,7 @@ void test_slab_allocator_fuzzy_single_thread_single_size(
     // If there is enough space to place the signature of the object ALSO at the end set
     // this flag to true and the code will take care of copying the signature from the
     // beginning of the allocated memory to the end as well for validation
-    bool can_place_signature_at_end = object_size > (sizeof(mem_alloc_test_data_t) * 2);
+    bool can_place_signature_at_end = object_size > (sizeof(test_slab_allocator_fuzzy_test_data_t) * 2);
 
     double_linked_list_t *list = double_linked_list_init();
 
@@ -88,11 +274,6 @@ void test_slab_allocator_fuzzy_single_thread_single_size(
 
         ops_counter_total++;
 
-        // Catch the overflow, no reason to perform more than 4 billion of tests
-        if (ops_counter_total == 0) {
-            break;
-        }
-
         if (list->count < min_used_slots ||
             (random_generate() % 1000 > 500 && list->count < max_used_slots)) {
             // allocate memory
@@ -100,7 +281,7 @@ void test_slab_allocator_fuzzy_single_thread_single_size(
 
             void* memptr = slab_allocator_mem_alloc_zero(object_size);
 
-            mem_alloc_test_data_t *data = (mem_alloc_test_data_t*)memptr;
+            test_slab_allocator_fuzzy_test_data_t *data = (test_slab_allocator_fuzzy_test_data_t*)memptr;
             data->ops_counter_total = ops_counter_total;
             data->ops_counter_mem_alloc = ops_counter_mem_alloc;
             data->hash_data_x = test_slab_allocator_calc_hash_x(ops_counter_total);
@@ -109,20 +290,18 @@ void test_slab_allocator_fuzzy_single_thread_single_size(
 
             if (can_place_signature_at_end) {
                 memcpy(
-                        (void*)((uintptr_t)memptr + object_size - sizeof(mem_alloc_test_data_t)),
+                        (void*)((uintptr_t)memptr + object_size - sizeof(test_slab_allocator_fuzzy_test_data_t)),
                         memptr,
-                        sizeof(mem_alloc_test_data_t));
+                        sizeof(test_slab_allocator_fuzzy_test_data_t));
             }
 
             double_linked_list_item_t *item = double_linked_list_item_init();
             item->data = memptr;
             double_linked_list_push_item(list, item);
         } else {
-            ops_counter_mem_free++;
-
             double_linked_list_item_t *item = double_linked_list_shift_item(list);
 
-            mem_alloc_test_data_t *data = (mem_alloc_test_data_t*)item->data;
+            test_slab_allocator_fuzzy_test_data_t *data = (test_slab_allocator_fuzzy_test_data_t*)item->data;
 
             uint64_t hash_data_x = test_slab_allocator_calc_hash_x(data->ops_counter_total);
             uint64_t hash_data_y = test_slab_allocator_calc_hash_y(data->ops_counter_mem_alloc);
@@ -133,9 +312,9 @@ void test_slab_allocator_fuzzy_single_thread_single_size(
 
             if (can_place_signature_at_end) {
                 int res = memcmp(
-                        (void*)((uintptr_t)data->memptr + object_size - sizeof(mem_alloc_test_data_t)),
+                        (void*)((uintptr_t)data->memptr + object_size - sizeof(test_slab_allocator_fuzzy_test_data_t)),
                         data->memptr,
-                        sizeof(mem_alloc_test_data_t));
+                        sizeof(test_slab_allocator_fuzzy_test_data_t));
                 REQUIRE(res == 0);
             }
 
@@ -263,7 +442,7 @@ TEST_CASE("slab_allocator.c", "[slab_allocator]") {
 
         SECTION("slab_allocator_slice_calculate_data_offset") {
             size_t usable_hugepage_size = 4096 * 4;
-            size_t object_size = 32;
+            uint32_t object_size = 32;
             uint32_t slots_count = (int)(usable_hugepage_size / (object_size + sizeof(slab_slot_t)));
             size_t data_offset = sizeof(slab_slice_t) + (slots_count * sizeof(slab_slot_t));
             data_offset += xalloc_get_page_size() - (data_offset % xalloc_get_page_size());
@@ -653,7 +832,7 @@ TEST_CASE("slab_allocator.c", "[slab_allocator]") {
             slab_allocator_enable(false);
         }
 
-        SECTION("slab_allocator alloc and free - fuzzy") {
+        SECTION("slab_allocator alloc and free - fuzzy - single thread") {
             uint32_t min_used_slots = 2500;
             uint32_t use_max_hugepages = 125;
             uint32_t max_duration = 1;
@@ -759,6 +938,123 @@ TEST_CASE("slab_allocator.c", "[slab_allocator]") {
 
             SECTION("single thread / one size - size 64k") {
                 test_slab_allocator_fuzzy_single_thread_single_size(
+                        max_duration,
+                        65536,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            slab_allocator_thread_cache_free(slab_allocator_thread_cache_get());
+            slab_allocator_thread_cache_set(nullptr);
+            slab_allocator_enable(false);
+        }
+
+        SECTION("slab_allocator alloc and free - fuzzy - multi thread") {
+            uint32_t min_used_slots = 2500;
+            uint32_t use_max_hugepages = 125;
+            uint32_t max_duration = 1;
+
+            slab_allocator_enable(true);
+            slab_allocator_thread_cache_set(slab_allocator_thread_cache_init());
+
+            SECTION("multi thread / one size - size 32") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        32,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 64") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        64,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 128") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        128,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 256") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        256,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 512") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        512,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 1024") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        1024,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 1k") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        1024,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 2k") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        2048,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 4k") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        4096,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 8k") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        8192,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 16k") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        16384,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 32k") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
+                        max_duration,
+                        32768,
+                        min_used_slots,
+                        use_max_hugepages);
+            }
+
+            SECTION("multi thread / one size - size 64k") {
+                test_slab_allocator_fuzzy_multi_thread_single_size(
                         max_duration,
                         65536,
                         min_used_slots,
