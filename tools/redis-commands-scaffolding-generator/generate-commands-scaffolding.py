050b1bf6416ee120bf960d01907c04a2d65489b7
Daniele Salvatore Albano <d.albano@gmail.com>
2022-08-12 22:40:33 +0200

    Add a required include to the c files generated by the scaffolding generator

--- tools/redis-commands-scaffolding-generator/generate-commands-scaffolding.py
+++ tools/redis-commands-scaffolding-generator/generate-commands-scaffolding.py
@@ -467,6 +467,7 @@ class Program:
         fp.writelines("\n".join([
             "#include <stdint.h>",
             "#include <stdbool.h>",
+            "#include <stddef.h>",
             "#include <string.h>",
             "#include <strings.h>",
             "#include <arpa/inet.h>",
