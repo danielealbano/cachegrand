e090027d9cdf1b78d0eec402b744a1eb59e279be
Daniele Salvatore Albano <d.albano@gmail.com>
2022-08-16 08:17:55 +0200

    Add support for expiration to set

--- src/module/redis/command/module_redis_command_set.c
+++ src/module/redis/command/module_redis_command_set.c
@@ -49,12 +49,37 @@
 MODULE_REDIS_COMMAND_FUNCPTR_COMMAND_END(set) {
     module_redis_command_set_context_t *context = connection_context->command.context;
 
+    storage_db_expiry_time_ms_t expiry_time_ms = STORAGE_DB_ENTRY_NO_EXPIRY;
+
+    if (context->expiration.value.pxat_unix_time_milliseconds.has_token) {
+        expiry_time_ms = (int64_t)context->expiration.value.pxat_unix_time_milliseconds.value;
+    } else if (context->expiration.value.exat_unix_time_seconds.has_token) {
+        expiry_time_ms = (int64_t)context->expiration.value.exat_unix_time_seconds.value * 1000;
+    } else if (context->expiration.value.px_milliseconds.has_token) {
+        expiry_time_ms = clock_realtime_coarse_int64_ms() + (int64_t)context->expiration.value.px_milliseconds.value;
+    } else if (context->expiration.value.ex_seconds.has_token) {
+        expiry_time_ms = clock_realtime_coarse_int64_ms() + ((int64_t)context->expiration.value.ex_seconds.value * 1000);
+    } else if (context->expiration.value.keepttl_keepttl.has_token) {
+        // TODO: not implemented, require a RMW operation
+    }
+
+    if (context->condition.value.nx_nx.has_token) {
+        // TODO: not implemented, require a RMW operation
+    } else if (context->condition.value.xx_xx.has_token) {
+        // TODO: not implemented, require a RMW operation
+    }
+
+    if (context->get_get.has_token) {
+        // TODO: check if the value is a string
+        // TODO: not implemented, require a RMW operation
+    }
+
     if (!storage_db_add_new_entry_index(
             connection_context->db,
             context->key.value.key,
             context->key.value.length,
             context->value.value.chunk_sequence,
-            STORAGE_DB_ENTRY_NO_EXPIRY)) {
+            expiry_time_ms)) {
         module_redis_connection_error_message_printf_noncritical(connection_context, "ERR set failed");
         return true;
     }
