df73931c744139888e42b114fe0ac349df9de9b1
Daniele Salvatore Albano <d.albano@gmail.com>
2022-08-30 00:39:39 +0200

    Move the glob pattern matching onto utils_string

--- src/utils_string.c
+++ src/utils_string.c
@@ -10,6 +10,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#include "misc.h"
+
 #include "utils_string.h"
 
 IFUNC_WRAPPER_RESOLVE(UTILS_STRING_NAME_IFUNC(cmp_eq_32)) {
@@ -42,7 +44,10 @@ bool IFUNC_WRAPPER(UTILS_STRING_NAME_IFUNC(casecmp_eq_32),
                    (const char *a, size_t a_len, const char *b, size_t b_len));
 
 // Derived from https://www.codeproject.com/Articles/5163931/Fast-String-Matching-with-Wildcards-Globs-and-Giti
-uint32_t utils_string_utf8_decode_char(char *string, size_t string_length, size_t *char_length) {
+uint32_t utils_string_utf8_decode_char(
+        char *string,
+        size_t string_length,
+        size_t *char_length) {
     *char_length = 0;
     int c1, c2, c3, c = (unsigned char)*string;
 
@@ -101,4 +106,154 @@ uint32_t utils_string_utf8_decode_char(char *string, size_t string_length, size_
     }
 
     return (((c & 0x07) << 18) | (c1 << 12) | (c2 << 6) | (c3 & 0x3F));
+}
+
+// Derived from https://www.codeproject.com/Articles/5163931/Fast-String-Matching-with-Wildcards-Globs-and-Giti
+// TODO: should handle UTF-8? Redis code seems doesn't do it;
+bool utils_string_glob_match(
+        char *string,
+        size_t string_length,
+        char *pattern,
+        size_t pattern_length) {
+    int last_char;
+    bool matched, reverse;
+    char *string_backup = NULL, *pattern_backup = NULL;
+    size_t string_length_backup = 0, pattern_length_backup = 0;
+
+    if (unlikely(pattern_length == 0 || string_length == 0)) {
+        return false;
+    }
+
+    if (unlikely(pattern_length == 1 && (*pattern == '*' || *pattern == '?'))) {
+        return true;
+    }
+
+    while (string_length > 0) {
+        switch (*pattern) {
+            case '*':
+                string_backup = string;
+                pattern_backup = ++pattern;
+                string_length_backup = string_length;
+                pattern_length_backup = --pattern_length;
+                continue;
+
+            case '?':
+                string++;
+                pattern++;
+                string_length--;
+                pattern_length--;
+                continue;
+
+            case '[':
+                if (pattern_length <= 1) {
+                    break;
+                }
+
+                pattern++;
+                pattern_length--;
+                reverse = false;
+
+                if (*pattern == '^') {
+                    reverse = true;
+                    pattern++;
+                    pattern_length--;
+                }
+
+                matched = false;
+                int prev_char = -1;
+                while(pattern_length > 0 && *pattern != ']') {
+                    switch(*pattern) {
+                        case '-':
+                            if (prev_char == -1) {
+                                continue;
+                            }
+
+                            pattern++;
+                            pattern_length--;
+                            if (pattern_length == 0) {
+                                continue;
+                            }
+
+                            if (!matched) {
+                                if (*string >= prev_char && *string <= *pattern) {
+                                    matched = true;
+                                }
+                            }
+                            break;
+
+                        case '\\':
+                            pattern++;
+                            pattern_length--;
+                            if (pattern_length == 0) {
+                                continue;
+                            }
+
+                        default:
+                            if (!matched) {
+                                if (*string == *pattern) {
+                                    matched = true;
+                                }
+                            }
+
+                            prev_char = *pattern;
+                            pattern++;
+                            pattern_length--;
+                            break;
+                    }
+                }
+
+                if (*pattern != ']') {
+                    break;
+                }
+
+                if (reverse) {
+                    matched = matched ? false : true;
+                }
+
+                if (!matched) {
+                    break;
+                }
+
+                pattern++;
+                pattern_length--;
+                string++;
+                string_length--;
+                continue;
+
+            case '\\':
+                pattern++;
+                pattern_length--;
+
+                if (pattern_length == 0) {
+                    break;
+                }
+
+            default:
+                if (*pattern != *string) {
+                    break;
+                }
+
+                string++;
+                pattern++;
+                string_length--;
+                pattern_length--;
+                continue;
+        }
+
+        if (pattern_backup == NULL) {
+            return false;
+        }
+
+        string = ++string_backup;
+        pattern = pattern_backup;
+        string_length = --string_length_backup;
+        pattern_length = pattern_length_backup;
+    }
+
+    while (pattern_length > 0 && *pattern == '*') {
+        pattern++;
+        pattern_length--;
+    }
+
+    return pattern_length == 0 && string_length == 0 ? true : false;
 }
