0d42f026f8ef59651f708374fcc734f205f105d2
Vito Castellano <castellano.vito@gmail.com>
2022-08-31 10:23:08 +0200

    Implemented SETENX command

---
+++ tests/unit_tests/modules/redis/command/test-modules-redis-command-setnx.cpp
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2018-2022 Vito Castellano
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ **/
+
+#include <catch2/catch.hpp>
+
+#include <cstdbool>
+#include <cstring>
+#include <memory>
+#include <string>
+
+#include <unistd.h>
+#include <netinet/in.h>
+
+#include "clock.h"
+#include "exttypes.h"
+#include "spinlock.h"
+#include "data_structures/small_circular_queue/small_circular_queue.h"
+#include "data_structures/double_linked_list/double_linked_list.h"
+#include "data_structures/hashtable/mcmp/hashtable.h"
+#include "config.h"
+#include "fiber.h"
+#include "worker/worker_stats.h"
+#include "worker/worker_context.h"
+#include "signal_handler_thread.h"
+#include "storage/io/storage_io_common.h"
+#include "storage/channel/storage_channel.h"
+#include "storage/db/storage_db.h"
+
+#include "program.h"
+
+#include "test-modules-redis-command-fixture.hpp"
+
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+
+TEST_CASE_METHOD(TestModulesRedisCommandFixture, "Redis - command - SETNX", "[redis][command][SETNX]") {
+//    SECTION("Missing parameters - key and value") {
+//        REQUIRE(send_recv_resp_command_text(
+//                client_fd,
+//                std::vector<std::string>{"SETNX"},
+//                "-ERR wrong number of arguments for 'SETNX' command\r\n"));
+//    }
+//
+//    SECTION("Missing parameters - value") {
+//        REQUIRE(send_recv_resp_command_text(
+//                client_fd,
+//                std::vector<std::string>{"SETNX", "a_key"},
+//                "-ERR wrong number of arguments for 'SETNX' command\r\n"));
+//    }
+//
+//    SECTION("Too many parameters - one extra parameter") {
+//        REQUIRE(send_recv_resp_command_text(
+//                client_fd,
+//                std::vector<std::string>{"SETNX", "a_key", "b_value", "extra parameter"},
+//                "-ERR syntax error\r\n"));
+//    }
+
+    SECTION("New key - NX") {
+        SECTION("Key not existing") {
+            REQUIRE(send_recv_resp_command_text(
+                    client_fd,
+                    std::vector<std::string>{"SETNX", "a_key", "b_value"},
+                    "+OK\r\n"));
+
+            REQUIRE(send_recv_resp_command_text(
+                    client_fd,
+                    std::vector<std::string>{"GET", "a_key"},
+                    "$7\r\nb_value\r\n"));
+        }
+
+        SECTION("Key existing") {
+            REQUIRE(send_recv_resp_command_text(
+                    client_fd,
+                    std::vector<std::string>{"SETNX", "a_key", "b_value"},
+                    "+OK\r\n"));
+
+            REQUIRE(send_recv_resp_command_text(
+                    client_fd,
+                    std::vector<std::string>{"SETNX", "a_key", "c_value"},
+                    "$-1\r\n"));
+
+            REQUIRE(send_recv_resp_command_text(
+                    client_fd,
+                    std::vector<std::string>{"GET", "a_key"},
+                    "$7\r\nb_value\r\n"));
+        }
+    }
+}
