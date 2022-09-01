cc9fa4a3cdcd738cc8f44ac5d9d8e0e0d9a45229
Daniele Salvatore Albano <d.albano@gmail.com>
2022-09-01 23:46:18 +0200

    Implement the tests for the RENAME and RENAMENX commands

---
+++ tests/unit_tests/modules/redis/command/test-modules-redis-command-renamenx.cpp
@@ -0,0 +1,113 @@
+/**
+ * Copyright (C) 2018-2022 Vito Castellano
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ **/
+
+#include <catch2/catch.hpp>
+
+#include <cstdbool>
+#include <cstring>
+#include <memory>
+#include <string>
+
+#include <unistd.h>
+#include <netinet/in.h>
+
+#include "clock.h"
+#include "exttypes.h"
+#include "spinlock.h"
+#include "data_structures/small_circular_queue/small_circular_queue.h"
+#include "data_structures/double_linked_list/double_linked_list.h"
+#include "data_structures/hashtable/mcmp/hashtable.h"
+#include "config.h"
+#include "fiber.h"
+#include "worker/worker_stats.h"
+#include "worker/worker_context.h"
+#include "signal_handler_thread.h"
+#include "storage/io/storage_io_common.h"
+#include "storage/channel/storage_channel.h"
+#include "storage/db/storage_db.h"
+
+#include "program.h"
+
+#include "test-modules-redis-command-fixture.hpp"
+
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+
+TEST_CASE_METHOD(TestModulesRedisCommandFixture, "Redis - command - RENAMENX", "[redis][command][RENAMENX]") {
+    SECTION("Key not existing") {
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"RENAMENX", "a_key", "b_value"},
+                "-ERR no such key\r\n"));
+    }
+
+    SECTION("Key existing") {
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"SET", "a_key", "b_value"},
+                "+OK\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"RENAMENX", "a_key", "b_key"},
+                ":1\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"GET", "b_key"},
+                "$7\r\nb_value\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"GET", "a_key"},
+                "$-1\r\n"));
+    }
+
+    SECTION("Overwrite same key") {
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"SET", "a_key", "b_value"},
+                "+OK\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"RENAMENX", "a_key", "a_key"},
+                ":0\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"GET", "a_key"},
+                "$7\r\nb_value\r\n"));
+    }
+
+    SECTION("Overwrite key") {
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"SET", "a_key", "b_value"},
+                "+OK\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"SET", "b_key", "c_value"},
+                "+OK\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"RENAMENX", "a_key", "b_key"},
+                ":0\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"GET", "b_key"},
+                "$7\r\nc_value\r\n"));
+
+        REQUIRE(send_recv_resp_command_text(
+                client_fd,
+                std::vector<std::string>{"GET", "a_key"},
+                "$7\r\nb_value\r\n"));
+    }
+}
