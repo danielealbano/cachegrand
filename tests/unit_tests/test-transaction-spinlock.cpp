7c6340e134012c13c915402cdf20b335090e98a8
Daniele Salvatore Albano <d.albano@gmail.com>
2022-09-02 22:46:13 +0200

    Add tests for the newly implemented components

---
+++ tests/unit_tests/test-transaction-spinlock.cpp
@@ -0,0 +1,307 @@
+/**
+ * Copyright (C) 2018-2022 Daniele Salvatore Albano
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ **/
+
+#include <catch2/catch.hpp>
+#include <unistd.h>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <atomic>
+using namespace std;
+
+#include "storage/io/storage_io_common.h"
+#include "storage/channel/storage_channel.h"
+
+#include "misc.h"
+#include "exttypes.h"
+#include "memory_fences.h"
+#include "utils_cpu.h"
+#include "thread.h"
+#include "xalloc.h"
+#include "spinlock.h"
+#include "transaction.h"
+#include "transaction_spinlock.h"
+#include "fiber.h"
+#include "fiber_scheduler.h"
+#include "clock.h"
+#include "config.h"
+#include "data_structures/hashtable/mcmp/hashtable.h"
+#include "worker/worker_stats.h"
+#include "worker/worker_context.h"
+#include "worker/worker.h"
+
+// Returns 1 if it can do the initial lock, 2 instead if it's able to reach the point in which has
+// to wait for the spinlock to become free
+void* test_transaction_spinlock_lock_lock_retry_try_lock_thread_func(void* rawdata) {
+    transaction_t transaction = { .transaction_id = { }, .locks = { .count = 0 , .size = 0, .list = nullptr, } };
+    transaction_id_t *transaction_id = &transaction.transaction_id;
+    transaction_id->worker_index = 0; transaction_id->transaction_index = 0;
+    transaction_spinlock_lock_t* lock = (transaction_spinlock_lock_t*)rawdata;
+
+    transaction_acquire(&transaction);
+    if (transaction_spinlock_try_lock(lock, &transaction)) {
+        transaction_release(&transaction);
+        return (void*)1;
+    }
+
+    REQUIRE(transaction_spinlock_lock(lock, &transaction));
+
+    if (transaction_spinlock_is_locked(lock) == 1) {
+        transaction_release(&transaction);
+        return (void*)2;
+    } else {
+        transaction_release(&transaction);
+        return (void*)1;
+    }
+}
+
+void test_transaction_spinlock_lock_thread_wait_on_flag(
+        volatile bool *flag,
+        bool expecting_value) {
+    do {
+        MEMORY_FENCE_LOAD();
+    } while (*flag != expecting_value);
+}
+
+// Increments a number of times using the spinlock for each increment
+struct test_transaction_spinlock_lock_counter_thread_func_data {
+    bool* start_flag;
+    uint32_t thread_num;
+    pthread_t thread_id;
+    transaction_spinlock_lock_volatile_t* lock;
+    uint64_t increments;
+    uint64_t* counter;
+};
+void* test_transaction_spinlock_lock_counter_thread_func(void* rawdata) {
+    struct test_transaction_spinlock_lock_counter_thread_func_data* data =
+            (struct test_transaction_spinlock_lock_counter_thread_func_data*)rawdata;
+
+    worker_context_t worker_context = { 0 };
+    worker_context.worker_index = data->thread_num;
+    worker_context_set(&worker_context);
+
+    thread_current_set_affinity(data->thread_num);
+
+    test_transaction_spinlock_lock_thread_wait_on_flag(data->start_flag, true);
+
+    for(uint64_t i = 0; i < data->increments; i++) {
+        transaction_t transaction = { .transaction_id = { }, .locks = { .count = 0 , .size = 0, .list = nullptr, } };
+        transaction_id_t *transaction_id = &transaction.transaction_id;
+        transaction_id->worker_index = 0; transaction_id->transaction_index = 0;
+
+        transaction_acquire(&transaction);
+
+        REQUIRE(transaction_spinlock_lock(data->lock, &transaction));
+        (*data->counter)++;
+
+        transaction_release(&transaction);
+    }
+
+    return nullptr;
+}
+
+TEST_CASE("transaction_spinlock.c", "[transaction_spinlock]") {
+    worker_context_t worker_context = { 0 };
+    worker_context.worker_index = UINT16_MAX;
+    worker_context_set(&worker_context);
+    transaction_set_worker_index(worker_context.worker_index);
+
+    SECTION("sizeof(transaction_spinlock_lock_t) == 4") {
+        REQUIRE(sizeof(transaction_spinlock_lock_t) == 4);
+    }
+
+    SECTION("transaction_spinlock_init") {
+        transaction_spinlock_lock_t lock = {0};
+        transaction_spinlock_init(&lock);
+
+        REQUIRE(lock.transaction_id == TRANSACTION_SPINLOCK_UNLOCKED);
+    }
+
+    SECTION("transaction_spinlock_is_locked") {
+        transaction_spinlock_lock_t lock = {0};
+        transaction_spinlock_init(&lock);
+
+        lock.transaction_id = 1;
+
+        REQUIRE(transaction_spinlock_is_locked(&lock));
+    }
+
+    SECTION("transaction_spinlock_try_lock") {
+        SECTION("lock") {
+            transaction_t transaction = { .transaction_id = { }, .locks = { .count = 0 , .size = 0, .list = nullptr, } };
+            transaction_id_t *transaction_id = &transaction.transaction_id;
+            transaction_id->worker_index = 0; transaction_id->transaction_index = 0;
+            transaction_spinlock_lock_t lock = {0};
+            transaction_spinlock_init(&lock);
+
+            transaction_acquire(&transaction);
+
+            REQUIRE(transaction_spinlock_try_lock(&lock, &transaction));
+
+            REQUIRE(lock.transaction_id != TRANSACTION_SPINLOCK_UNLOCKED);
+            REQUIRE(transaction.locks.size == 8);
+            REQUIRE(transaction.locks.count == 1);
+
+            transaction_release(&transaction);
+        }
+
+        SECTION("lock - fail already locked") {
+            transaction_spinlock_lock_t lock = {0};
+            transaction_spinlock_init(&lock);
+
+            lock.transaction_id = 1;
+
+            REQUIRE(transaction_spinlock_is_locked(&lock));
+
+            REQUIRE(lock.transaction_id != TRANSACTION_SPINLOCK_UNLOCKED);
+        }
+    }
+
+    SECTION("transaction_spinlock_unlock") {
+        SECTION("unlock") {
+            transaction_t transaction = { .transaction_id = { }, .locks = { .count = 0 , .size = 0, .list = nullptr, } };
+            transaction_id_t *transaction_id = &transaction.transaction_id;
+            transaction_id->worker_index = 0; transaction_id->transaction_index = 1;
+            transaction_spinlock_lock_t lock = {0};
+            transaction_spinlock_init(&lock);
+
+            lock.transaction_id = transaction_id->id;
+
+#if DEBUG == 1
+            transaction_spinlock_unlock_internal(&lock, &transaction);
+#else
+            transaction_spinlock_unlock_internal(&lock);
+#endif
+            REQUIRE(lock.transaction_id == TRANSACTION_SPINLOCK_UNLOCKED);
+        }
+    }
+
+    SECTION("transaction_spinlock_lock") {
+        SECTION("fail already locked") {
+            transaction_t transaction = { .transaction_id = { }, .locks = { .count = 0 , .size = 0, .list = nullptr, } };
+            transaction_id_t *transaction_id = &transaction.transaction_id;
+            transaction_id->worker_index = 0; transaction_id->transaction_index = 0;
+            transaction_spinlock_lock_t lock = {0};
+            transaction_spinlock_init(&lock);
+
+            transaction_acquire(&transaction);
+
+            REQUIRE(transaction_spinlock_lock(&lock, &transaction));
+            REQUIRE(lock.transaction_id == transaction_id->id);
+            REQUIRE(!transaction_spinlock_try_lock(&lock, &transaction));
+
+            transaction_release(&transaction);
+        }
+
+        SECTION("lock") {
+            int res, pthread_return_val, *pthread_return = nullptr;
+            transaction_t transaction = { .transaction_id = { }, .locks = { .count = 0 , .size = 0, .list = nullptr, } };
+            transaction_id_t *transaction_id = &transaction.transaction_id;
+            transaction_id->worker_index = 0; transaction_id->transaction_index = 0;
+            transaction_spinlock_lock_t lock = {0};
+            pthread_t pthread;
+            pthread_attr_t attr;
+
+            transaction_spinlock_init(&lock);
+
+            res = pthread_attr_init(&attr);
+            if (res != 0) {
+                perror("pthread_attr_init");
+            }
+
+            transaction_acquire(&transaction);
+
+            // Lock
+            REQUIRE(transaction_spinlock_try_lock(&lock, &transaction));
+            REQUIRE(lock.transaction_id == transaction_id->id);
+
+            // Create the thread that wait for unlock
+            res = pthread_create(&pthread, &attr, &test_transaction_spinlock_lock_lock_retry_try_lock_thread_func, (void*)&lock);
+            if (res != 0) {
+                perror("pthread_create");
+            }
+
+            // Wait
+            usleep(100000);
+
+            // Unlock
+            transaction_release(&transaction);
+
+            // Wait
+            usleep(1000000);
+
+            // TODO: would be better to come up with a better mechanism to check if the thread has actually finished or
+            //       not but the above sleep seems reasonable, the thread doesn't finish within 1 second the spinlock
+            //       is buggy and stuck
+            int pthread_tryjoin_np_res = pthread_tryjoin_np(pthread, (void**)&pthread_return);
+            pthread_return_val = (int)(uint64_t)pthread_return;
+
+            REQUIRE(pthread_tryjoin_np_res == 0);
+            REQUIRE(pthread_return_val == 2);
+
+            // The difference between this test and the plain spinlock one is that the thread does a transaction release
+            // therefore the lock gets unlocked
+            REQUIRE(lock.transaction_id == TRANSACTION_SPINLOCK_UNLOCKED);
+        }
+
+        SECTION("test lock parallelism") {
+            void* ret;
+            bool start_flag;
+            transaction_spinlock_lock_volatile_t lock = { 0 };
+            pthread_attr_t attr;
+            uint64_t increments_per_thread_sum = 0, increments_per_thread;
+
+            uint32_t cores_count = utils_cpu_count();
+            uint32_t threads_count = cores_count * 2;
+
+            // Magic numbers to run enough thread in parallel for 1-2s after the thread creation.
+            // The test can be quite time-consuming when with an attached debugger.
+            increments_per_thread = (uint64_t)(20000 /  ((float)threads_count / 48.0));
+
+            REQUIRE(pthread_attr_init(&attr) == 0);
+
+            struct test_transaction_spinlock_lock_counter_thread_func_data* threads_info =
+                    (struct test_transaction_spinlock_lock_counter_thread_func_data*)calloc(
+                            threads_count,
+                            sizeof(test_transaction_spinlock_lock_counter_thread_func_data));
+
+            REQUIRE(threads_info != NULL);
+
+            start_flag = false;
+            transaction_spinlock_init(&lock);
+            for(uint32_t thread_num = 0; thread_num < threads_count; thread_num++) {
+                threads_info[thread_num].thread_num = thread_num;
+                threads_info[thread_num].start_flag = &start_flag;
+                threads_info[thread_num].lock = &lock;
+                threads_info[thread_num].increments = increments_per_thread;
+                threads_info[thread_num].counter = &increments_per_thread_sum;
+
+                REQUIRE(pthread_create(
+                        &threads_info[thread_num].thread_id,
+                        &attr,
+                        &test_transaction_spinlock_lock_counter_thread_func,
+                        &threads_info[thread_num]) == 0);
+            }
+
+            start_flag = true;
+            MEMORY_FENCE_STORE();
+
+            // Wait for all the threads to finish
+            for(uint32_t thread_num = 0; thread_num < threads_count; thread_num++) {
+                REQUIRE(pthread_join(threads_info[thread_num].thread_id, &ret) == 0);
+            }
+
+            free(threads_info);
+
+            REQUIRE(increments_per_thread_sum == increments_per_thread * threads_count);
+        }
+    }
+}
---
+++ tests/unit_tests/test-transaction.cpp
@@ -0,0 +1,225 @@
+/**
+ * Copyright (C) 2018-2022 Daniele Salvatore Albano
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ **/
+
+#include <catch2/catch.hpp>
+
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#include "misc.h"
+#include "exttypes.h"
+#include "memory_fences.h"
+#include "utils_cpu.h"
+#include "thread.h"
+#include "data_structures/double_linked_list/double_linked_list.h"
+#include "data_structures/queue_mpmc/queue_mpmc.h"
+#include "slab_allocator.h"
+#include "fiber.h"
+#include "fiber_scheduler.h"
+#include "clock.h"
+#include "config.h"
+
+#include "spinlock.h"
+#include "transaction.h"
+#include "transaction_spinlock.h"
+
+#include "data_structures/hashtable/mcmp/hashtable.h"
+#include "worker/worker_stats.h"
+#include "worker/worker_context.h"
+#include "worker/worker.h"
+
+TEST_CASE("transaction.c", "[transaction]") {
+    worker_context_t worker_context = { 0 };
+    worker_context.worker_index = UINT16_MAX;
+    worker_context_set(&worker_context);
+    transaction_set_worker_index(worker_context.worker_index);
+
+    SECTION("transaction_expand_locks_list") {
+        transaction_t transaction = { };
+        transaction.locks.count = 0;
+        transaction.locks.size = 8;
+
+        transaction_spinlock_lock_volatile_t** initial_list = (transaction_spinlock_lock_volatile_t**)slab_allocator_mem_alloc(
+                sizeof(void*) * transaction.locks.size);
+        transaction.locks.list = initial_list;
+
+        SECTION("Expand once") {
+            transaction_expand_locks_list(&transaction);
+
+            REQUIRE(transaction.locks.size == 16);
+            REQUIRE(transaction.locks.count == 0);
+            REQUIRE(transaction.locks.list != initial_list);
+        }
+
+        SECTION("Expand twice") {
+            transaction_expand_locks_list(&transaction);
+
+            transaction_spinlock_lock_volatile_t** interim_list = transaction.locks.list;
+            transaction_expand_locks_list(&transaction);
+
+            REQUIRE(transaction.locks.size == 32);
+            REQUIRE(transaction.locks.count == 0);
+            REQUIRE(transaction.locks.list != interim_list);
+        }
+
+        slab_allocator_mem_free(transaction.locks.list);
+    }
+
+    SECTION("transaction_needs_expand_locks_list") {
+        SECTION("Needs expand") {
+            transaction_t transaction = { };
+            transaction.locks.count = 8;
+            transaction.locks.size = 8;
+
+            REQUIRE(transaction_needs_expand_locks_list(&transaction));
+        }
+
+        SECTION("Doesn't needs expand") {
+            transaction_t transaction = { };
+            transaction.locks.count = 7;
+            transaction.locks.size = 8;
+
+            REQUIRE(!transaction_needs_expand_locks_list(&transaction));
+        }
+    }
+
+    SECTION("transaction_locks_list_add") {
+        transaction_t transaction = { };
+        transaction.locks.count = 0;
+        transaction.locks.size = 8;
+        transaction.locks.list = (transaction_spinlock_lock_volatile_t**)slab_allocator_mem_alloc(
+                sizeof(void*) * transaction.locks.size);
+
+        SECTION("Add one lock") {
+            transaction_spinlock_lock_volatile_t lock = { 0 };
+
+            REQUIRE(transaction_locks_list_add(&transaction, &lock));
+            REQUIRE(transaction.locks.count == 1);
+            REQUIRE(transaction.locks.size == 8);
+            REQUIRE(transaction.locks.list[0] == &lock);
+        }
+
+        SECTION("Add two lock") {
+            transaction_spinlock_lock_volatile_t lock[2] = { 0 };
+            REQUIRE(transaction_locks_list_add(&transaction, &lock[0]));
+            REQUIRE(transaction.locks.count == 1);
+            REQUIRE(transaction.locks.size == 8);
+            REQUIRE(transaction.locks.list[0] == &lock[0]);
+
+            REQUIRE(transaction_locks_list_add(&transaction, &lock[1]));
+            REQUIRE(transaction.locks.count == 2);
+            REQUIRE(transaction.locks.size == 8);
+            REQUIRE(transaction.locks.list[1] == &lock[1]);
+        }
+
+        SECTION("Force expansions") {
+            transaction_spinlock_lock_volatile_t lock[10] = { 0 };
+
+            for(int index = 0; index < ARRAY_SIZE(lock); index++) {
+                REQUIRE(transaction_locks_list_add(&transaction, &lock[index]));
+            }
+
+            REQUIRE(transaction.locks.count == ARRAY_SIZE(lock));
+            REQUIRE(transaction.locks.size == 16);
+            REQUIRE(transaction.locks.list[ARRAY_SIZE(lock) - 1] == &lock[ARRAY_SIZE(lock) - 1]);
+        }
+
+        slab_allocator_mem_free(transaction.locks.list);
+    }
+
+    SECTION("transaction_acquire") {
+        SECTION("Acquire one") {
+            transaction_t transaction = { 0 };
+
+            uint16_t current_transaction_index = transaction_peek_current_thread_index();
+
+            REQUIRE(transaction_acquire(&transaction));
+
+            REQUIRE(transaction.transaction_id.worker_index == UINT16_MAX);
+            REQUIRE(transaction.transaction_id.transaction_index == current_transaction_index + 1);
+
+            REQUIRE(transaction.locks.count == 0);
+            REQUIRE(transaction.locks.size == 8);
+            REQUIRE(transaction.locks.list != nullptr);
+
+            slab_allocator_mem_free(transaction.locks.list);
+        }
+
+        SECTION("Acquire two") {
+            transaction_t transaction1 = { 0 };
+            transaction_t transaction2 = { 0 };
+
+            uint16_t current_transaction_index = transaction_peek_current_thread_index();
+
+            REQUIRE(transaction_acquire(&transaction1));
+            REQUIRE(transaction_acquire(&transaction2));
+
+            REQUIRE(transaction2.transaction_id.worker_index == UINT16_MAX);
+            REQUIRE(transaction2.transaction_id.transaction_index == current_transaction_index + 2);
+
+            slab_allocator_mem_free(transaction1.locks.list);
+            slab_allocator_mem_free(transaction2.locks.list);
+        }
+    }
+
+    SECTION("transaction_release") {
+        transaction_t transaction = { 0 };
+        REQUIRE(transaction_acquire(&transaction));
+
+        SECTION("Empty transaction") {
+            transaction_release(&transaction);
+
+            REQUIRE(transaction.locks.list == nullptr);
+            REQUIRE(transaction.transaction_id.id == TRANSACTION_ID_NOT_ACQUIRED);
+        }
+
+        SECTION("With one lock") {
+            transaction_spinlock_lock_volatile_t lock = { transaction.transaction_id.id };
+
+            REQUIRE(transaction_locks_list_add(&transaction, &lock));
+
+            transaction_release(&transaction);
+
+            REQUIRE(lock.transaction_id == TRANSACTION_SPINLOCK_UNLOCKED);
+            REQUIRE(transaction.locks.list == nullptr);
+            REQUIRE(transaction.transaction_id.id == TRANSACTION_ID_NOT_ACQUIRED);
+        }
+
+        SECTION("With two locks") {
+            transaction_spinlock_lock_volatile_t lock1 = { transaction.transaction_id.id };
+            transaction_spinlock_lock_volatile_t lock2 = { transaction.transaction_id.id };
+
+            REQUIRE(transaction_locks_list_add(&transaction, &lock1));
+            REQUIRE(transaction_locks_list_add(&transaction, &lock2));
+
+            transaction_release(&transaction);
+
+            REQUIRE(lock1.transaction_id == TRANSACTION_SPINLOCK_UNLOCKED);
+            REQUIRE(lock2.transaction_id == TRANSACTION_SPINLOCK_UNLOCKED);
+            REQUIRE(transaction.locks.list == nullptr);
+            REQUIRE(transaction.transaction_id.id == TRANSACTION_ID_NOT_ACQUIRED);
+        }
+
+        SECTION("Expanded") {
+            transaction_spinlock_lock_volatile_t lock[10] = { 0 };
+
+            for(int index = 0; index < ARRAY_SIZE(lock); index++) {
+                lock[index].transaction_id = transaction.transaction_id.id;
+                REQUIRE(transaction_locks_list_add(&transaction, &lock[index]));
+            }
+
+            transaction_release(&transaction);
+
+            for(int index = 0; index < ARRAY_SIZE(lock); index++) {
+                REQUIRE(lock[index].transaction_id == TRANSACTION_SPINLOCK_UNLOCKED);
+            }
+            REQUIRE(transaction.locks.list == nullptr);
+            REQUIRE(transaction.transaction_id.id == TRANSACTION_ID_NOT_ACQUIRED);
+        }
+    }
+}
