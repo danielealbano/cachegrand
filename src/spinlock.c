ec33fcb74d98cfeb72a6c5ea445ea9ef24861884
Daniele Salvatore Albano <d.albano@gmail.com>
2022-09-02 22:48:16 +0200

    Refactor the spinlock to move the fast-path code to the header and force the inline

--- src/spinlock.c
+++ src/spinlock.c
@@ -6,85 +6,13 @@
  * of the BSD license.  See the LICENSE file for details.
  **/
 
-#include <stdlib.h>
 #include <stdint.h>
-#include <stdbool.h>
-#include <stdatomic.h>
-#include <pthread.h>
 
-#if DEBUG == 1
-#include <assert.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/syscall.h>
-#endif
-
-#include "memory_fences.h"
-#include "misc.h"
-#include "exttypes.h"
 #include "spinlock.h"
-#include "log/log.h"
-#include "fatal.h"
 
 #define TAG "spinlock"
 
 void spinlock_init(
         spinlock_lock_volatile_t* spinlock) {
     spinlock->lock = SPINLOCK_UNLOCKED;
-}
-
-void spinlock_unlock(
-        spinlock_lock_volatile_t* spinlock) {
-#if DEBUG == 1
-    long thread_id = syscall(__NR_gettid);
-    uint32_t expected_lock = (uint32_t)thread_id;
-    assert(spinlock->lock == expected_lock);
-#endif
-
-    spinlock->lock = SPINLOCK_UNLOCKED;
-    MEMORY_FENCE_STORE();
-}
-
-bool spinlock_is_locked(
-        spinlock_lock_volatile_t *spinlock) {
-    MEMORY_FENCE_LOAD();
-    return spinlock->lock != SPINLOCK_UNLOCKED;
-}
-
-bool spinlock_try_lock(
-        spinlock_lock_volatile_t *spinlock) {
-#if DEBUG == 1
-    long thread_id = syscall(__NR_gettid);
-    uint32_t new_value = thread_id;
-    uint32_t expected_value = SPINLOCK_UNLOCKED;
-#else
-    uint32_t new_value = SPINLOCK_LOCKED;
-    uint32_t expected_value = SPINLOCK_UNLOCKED;
-#endif
-
-    return __sync_bool_compare_and_swap(&spinlock->lock, expected_value, new_value);
-}
-
-bool spinlock_lock_internal(
-        spinlock_lock_volatile_t *spinlock,
-        bool retry,
-        const char* src_path,
-        const char* src_func,
-        uint32_t src_line) {
-    bool res = false;
-    uint32_t max_spins_before_probably_stuck = UINT32_MAX >> 1;
-
-    while (unlikely(!(res = spinlock_try_lock(spinlock)) && retry)) {
-        uint64_t spins = 0;
-        while (likely(spinlock_is_locked(spinlock) && spins < max_spins_before_probably_stuck)) {
-            spins++;
-        }
-
-        if (spins == max_spins_before_probably_stuck) {
-            LOG_E(TAG, "Possible stuck spinlock detected for thread %lu in %s at %s:%u",
-                    pthread_self(), src_func, src_path, src_line);
-        }
-    }
-
-    return res;
 }
--- src/spinlock.h
+++ src/spinlock.h
@@ -2,18 +2,33 @@
 #define CACHEGRAND_SPINLOCK_H
 
 #ifdef __cplusplus
+#include <atomic>
+#else
+#include <stdatomic.h>
+#endif
+
+#ifdef __cplusplus
 extern "C" {
 #endif
 
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#if DEBUG == 1
+#include <assert.h>
+#include <sys/types.h>
+#endif
+
 #include "cmake_config.h"
+#include "misc.h"
+#include "exttypes.h"
+#include "memory_fences.h"
+#include "log/log.h"
 
 #define SPINLOCK_UNLOCKED   0
 #define SPINLOCK_LOCKED     1
-
-#define SPINLOCK_FLAG_POTENTIALLY_STUCK     0x01
-
-typedef uint8_t spinlock_flag_t;
-typedef _Volatile(uint8_t) spinlock_flag_volatile_t;
 
 typedef struct spinlock_lock spinlock_lock_t;
 typedef _Volatile(spinlock_lock_t) spinlock_lock_volatile_t;
@@ -24,39 +39,59 @@ struct spinlock_lock {
 void spinlock_init(
         spinlock_lock_volatile_t* spinlock);
 
-bool spinlock_try_lock(
-        spinlock_lock_volatile_t* spinlock);
+static inline __attribute__((always_inline)) void spinlock_unlock(
+        spinlock_lock_volatile_t* spinlock) {
+#if DEBUG == 1
+    long thread_id = syscall(__NR_gettid);
+    uint32_t expected_lock = (uint32_t)thread_id;
+    assert(spinlock->lock == expected_lock);
+#endif
 
-bool spinlock_is_locked(
-        spinlock_lock_volatile_t* spinlock);
+    spinlock->lock = SPINLOCK_UNLOCKED;
+    MEMORY_FENCE_STORE();
+}
 
-bool spinlock_lock_internal(
-        spinlock_lock_volatile_t* spinlock,
-        bool retry,
+static inline __attribute__((always_inline)) bool spinlock_is_locked(
+        spinlock_lock_volatile_t *spinlock) {
+    MEMORY_FENCE_LOAD();
+    return spinlock->lock != SPINLOCK_UNLOCKED;
+}
+
+static inline __attribute__((always_inline)) bool spinlock_try_lock(
+        spinlock_lock_volatile_t *spinlock) {
+#if DEBUG == 1
+    long thread_id = syscall(__NR_gettid);
+    uint32_t new_value = thread_id;
+    uint32_t expected_value = SPINLOCK_UNLOCKED;
+#else
+    uint32_t new_value = SPINLOCK_LOCKED;
+    uint32_t expected_value = SPINLOCK_UNLOCKED;
+#endif
+
+    return __sync_bool_compare_and_swap(&spinlock->lock, expected_value, new_value);
+}
+
+static inline __attribute__((always_inline)) void spinlock_lock_internal(
+        spinlock_lock_volatile_t *spinlock,
         const char* src_path,
-        const char* src_func,
-        uint32_t src_line);
+        uint32_t src_line) {
+    uint32_t max_spins_before_probably_stuck = 1 << 23;
 
-void spinlock_unlock(
-        spinlock_lock_volatile_t* spinlock);
-
-void spinlock_set_flag(
-        spinlock_lock_volatile_t *spinlock,
-        spinlock_flag_t flag);
-
-void spinlock_unset_flag(
-        spinlock_lock_volatile_t *spinlock,
-        spinlock_flag_t flag);
-
-bool spinlock_has_flag(
-        spinlock_lock_volatile_t *spinlock,
-        spinlock_flag_t flag);
+    uint64_t spins = 0;
+    while (unlikely(!spinlock_try_lock(spinlock))) {
+        if (unlikely(spins++ == max_spins_before_probably_stuck)) {
+            LOG_E("spinlock", "Possible stuck spinlock detected for thread %lu in %s:%u",
+                  syscall(__NR_gettid), src_path, src_line);
+            spins = 0;
+        }
+    }
+}
 
 /**
- * Uses a macro to wrap _spinlock_lock to automatically define the path, func and line args
+ * Uses a macro to wrap spinlock_lock_internal to automatically define the path and the line args
  */
-#define spinlock_lock(spinlock, retry) \
-    spinlock_lock_internal(spinlock, retry, CACHEGRAND_SRC_PATH, __func__, __LINE__)
+#define spinlock_lock(spinlock) \
+    spinlock_lock_internal(spinlock, CACHEGRAND_SRC_PATH, __LINE__)
 
 #ifdef __cplusplus
 }
