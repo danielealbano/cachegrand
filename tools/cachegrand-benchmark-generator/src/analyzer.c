dc9e3a52335e1fb5abf08b6b6420ac11b3e8339d
Vito Castellano <castellano.vito@gmail.com>
2022-09-28 10:05:02 +0200

    Renamed some pointers

--- tools/cachegrand-benchmark-generator/src/analyzer.c
+++ tools/cachegrand-benchmark-generator/src/analyzer.c
@@ -31,57 +31,34 @@ test_t* analyzer_analyze(
     return test;
 }
 
-void analyzer_recursive_print(
-        section_t **sections,
-        size_t n_sections) {
-        for (int i = 0; i < n_sections; ++i) {
-            section_t *section = sections[i];
-            printf("SECTION: %s\n", section->name);
-            if (section->n_commands > 0) {
-                for (int j = 0; j < section->n_commands; ++j) {
-                    printf("\tCOMMAND: %s\n", section->commands[j]);
-                }
-            }
-
-            if (section->n_subsections > 0) {
-                analyzer_recursive_print(
-                        section->subsections,
-                        section->n_subsections);
-            }
-        }
-
-        // Check Subsection
-        printf("\n");
-}
-
 int analyzer_recursive_match(
         const char *body,
         int padding,
         test_t *current_test,
         section_t *father_section) {
     // Match SECTIONS
-    matcher_t *match_results = matcher_get_sections(body, padding);
-    if (match_results->n_matches <= 0) {
-        free(match_results);
+    matcher_t *sections = matcher_get_sections(body, padding);
+    if (sections->n_matches <= 0) {
+        free(sections);
         return 0;
     }
 
-    for (int i = 0; i < match_results->n_matches; ++i) {
-        section_t *new_current_section_p = builder_new_section_p();
+    for (int i = 0; i < sections->n_matches; ++i) {
+        section_t *current_section = builder_new_section_p();
         // Match SECTION name
-        char *section_name = matcher_get_section_name(match_results->matches[i]);
+        char *section_name = matcher_get_section_name(sections->matches[i]);
         if (NULL == section_name) continue;
-        new_current_section_p->name = section_name;
+        current_section->name = section_name;
 
         // Match REQUIRE section
-        matcher_t *section_requires = matcher_get_requires_section(match_results->matches[i], padding);
+        matcher_t *section_requires = matcher_get_requires_section(sections->matches[i], padding);
         if (section_requires->n_matches > 0) {
             for (int j = 0; j < section_requires->n_matches; ++j) {
                 // Match COMMAND
                 char *command = matcher_get_require_command(section_requires->matches[j]);
                 if (NULL == command) continue;
                 builder_section_append_command(
-                        new_current_section_p, command);
+                        current_section, command);
             }
         }
         free(section_requires);
@@ -89,19 +66,41 @@ int analyzer_recursive_match(
         if (NULL != father_section) {
             builder_section_append_subsection(
                     father_section,
-                    new_current_section_p);
+                    current_section);
         } else {
             builder_test_append_section(
                     current_test,
-                    new_current_section_p);
+                    current_section);
         }
 
         analyzer_recursive_match(
-                match_results->matches[i],
+                sections->matches[i],
                 padding*2,
                 current_test,
-                new_current_section_p);
+                current_section);
     }
 
-    free(match_results);
+    free(sections);
+}
+
+void analyzer_recursive_print(
+        section_t **sections,
+        size_t n_sections) {
+    for (int i = 0; i < n_sections; ++i) {
+        section_t *section = sections[i];
+        printf("SECTION: %s\n", section->name);
+        if (section->n_commands > 0) {
+            for (int j = 0; j < section->n_commands; ++j) {
+                printf("\tCOMMAND: %s\n", section->commands[j]);
+            }
+        }
+
+        if (section->n_subsections > 0) {
+            analyzer_recursive_print(
+                    section->subsections,
+                    section->n_subsections);
+        }
+    }
+
+    printf("\n");
 }
