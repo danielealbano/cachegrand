c7e02b205b377c3470c412d0ce4e1ad8da8c2399
Vito Castellano <castellano.vito@gmail.com>
2022-09-28 23:46:27 +0200

    Implemented arguments reader

--- tools/cachegrand-benchmark-generator/src/main.c
+++ tools/cachegrand-benchmark-generator/src/main.c
@@ -18,24 +18,34 @@
 #include "support.h"
 #include "output.h"
 
-char *test_lists[] = {
-        "../../../../tests/unit_tests/modules/redis/command/test-modules-redis-command-del.cpp",
-        "../../../../tests/unit_tests/modules/redis/command/test-modules-redis-command-decrby.cpp",
-        "../../../../tests/unit_tests/modules/redis/command/test-modules-redis-command-del.cpp",
-        "../../../../tests/unit_tests/modules/redis/command/test-modules-redis-command-append.cpp",
-};
+int main(int argc, char **argv) {
+    printf("### Cachegrand Benchmark Generator ###\n");
+    size_t n_tests = 0;
+    char **test_lists = malloc(sizeof(char*));
 
-int main() {
-    tests_t *test_collections = builder_new_tests_p();
-
-    size_t n_tests = sizeof(test_lists) / sizeof(char*);
-    for (int i = 0; i < n_tests; ++i) {
-        test_t* test = analyzer_analyze(test_lists[i]);
-        builder_tests_append_test(test_collections, test);
-//        output_stdout_print(test->sections, test->n_sections);
+    for (int i = 1; i < argc; ++i) {
+        test_lists = realloc(test_lists, i*sizeof(char*));
+        test_lists[i-1] = argv[i];
+        ++n_tests;
     }
 
-    output_json_print(test_collections);
+    if (n_tests > 0) {
+        printf("[%zu] Total tests!\n", n_tests);
+        tests_t *test_collections = builder_new_tests_p();
+        for (int i = 0; i < n_tests; ++i) {
+            printf("[%i/%zu]", i+1, n_tests);
+            test_t* test = analyzer_analyze(test_lists[i]);
+            printf(" \"%s\" ", test->name);
+            builder_tests_append_test(test_collections, test);
+            printf("...OK\n");
+            //output_stdout_print(test->sections, test->n_sections);
+        }
 
-    return EXIT_SUCCESS;
+        output_json_print(test_collections);
+        printf("Generation done. Bye.");
+        return EXIT_SUCCESS;
+    }
+
+    printf("No tests loaded. Bye.");
+    return EXIT_FAILURE;
 }
