dc598f40d58cc532f1db51a008417211483ce04a
Daniele Salvatore Albano <d.albano@gmail.com>
2022-11-04 21:33:03 +0100

    Remove unused header

--- src/data_structures/ring_bounded_spsc/ring_bounded_spsc.c
+++ src/data_structures/ring_bounded_spsc/ring_bounded_spsc.c
@@ -11,9 +11,7 @@
 #include <stdbool.h>
 #include <assert.h>
 
-#include "misc.h"
 #include "exttypes.h"
-#include "spinlock.h"
 #include "data_structures/double_linked_list/double_linked_list.h"
 #include "data_structures/queue_mpmc/queue_mpmc.h"
 #include "memory_allocator/ffma.h"
