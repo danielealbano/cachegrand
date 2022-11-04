2e4b19c2fd4b998280a7a1d8f591e2f36c44c917
Daniele Salvatore Albano <d.albano@gmail.com>
2022-11-04 22:06:39 +0100

    Fix variable names, rename scq in rb

--- src/data_structures/ring_bounded_spsc/ring_bounded_spsc.h
+++ src/data_structures/ring_bounded_spsc/ring_bounded_spsc.h
@@ -19,26 +19,26 @@ ring_bounded_spsc_t* ring_bounded_spsc_init(
         int16_t length);
 
 void ring_bounded_spsc_free(
-        ring_bounded_spsc_t *scq);
+        ring_bounded_spsc_t *rb);
 
 int16_t ring_bounded_spsc_count(
-        ring_bounded_spsc_t *scq);
+        ring_bounded_spsc_t *rb);
 
 bool ring_bounded_spsc_is_empty(
-        ring_bounded_spsc_t *scq);
+        ring_bounded_spsc_t *rb);
 
 bool ring_bounded_spsc_is_full(
-        ring_bounded_spsc_t *scq);
+        ring_bounded_spsc_t *rb);
 
 void *ring_bounded_spsc_peek(
-        ring_bounded_spsc_t *scq);
+        ring_bounded_spsc_t *rb);
 
 bool ring_bounded_spsc_enqueue(
-        ring_bounded_spsc_t *scq,
+        ring_bounded_spsc_t *rb,
         void *value);
 
 void *ring_bounded_spsc_dequeue(
-        ring_bounded_spsc_t *scq);
+        ring_bounded_spsc_t *rb);
 
 #ifdef __cplusplus
 }
