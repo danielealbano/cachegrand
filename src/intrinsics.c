4651af2b742ddda960f0a5007057d3646affa8d9
Daniele Salvatore Albano <d.albano@gmail.com>
2022-11-06 19:15:03 +0100

    Move the intrinsics_tsc to static inline in intrinsics.h

--- src/intrinsics.c
+++ src/intrinsics.c
@@ -9,21 +9,3 @@
 #include <stdint.h>
 
 #include "intrinsics.h"
-
-uint64_t intrinsics_tsc() {
-#if defined(__x86_64__)
-    uint64_t rax, rdx;
-    __asm__ __volatile__ (
-            "rdtsc"
-            : "=a" (rax), "=d" (rdx));
-    return (rdx << 32) + rax;
-#elif defined(__aarch64__)
-    int64_t tsc;
-    asm volatile (
-            "mrs %0, cntvct_el0"
-            : "=r"(tsc));
-    return (uint64_t) tsc;
-#else
-#error "unsupported platform"
-#endif
-}
