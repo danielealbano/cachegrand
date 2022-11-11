2ace1680a928e9947286f175cad595f251a53226
Daniele Salvatore Albano <d.albano@gmail.com>
2022-11-11 22:51:17 +0100

    Initial implementation of the epoch operation queue

---
+++ src/epoch_operation_queue.c
@@ -0,0 +1,75 @@
+/**
+ * Copyright (C) 2018-2022 Daniele Salvatore Albano
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ **/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include "misc.h"
+#include "xalloc.h"
+#include "intrinsics.h"
+#include "data_structures/ring_bounded_queue_spsc/ring_bounded_queue_spsc_uint128.h"
+
+#include "epoch_operation_queue.h"
+
+epoch_operation_queue_t *epoch_operation_queue_init() {
+    epoch_operation_queue_t *epoch_operation_queue = xalloc_alloc_zero(sizeof(epoch_operation_queue_t));
+    epoch_operation_queue->ring = ring_bounded_queue_spsc_uint128_init(EPOCH_OPERATION_QUEUE_RING_SIZE);
+
+    return epoch_operation_queue;
+}
+
+void epoch_operation_queue_free(
+        epoch_operation_queue_t *epoch_operation_queue) {
+    ring_bounded_queue_spsc_uint128_free(epoch_operation_queue->ring);
+    xalloc_free(epoch_operation_queue);
+}
+
+epoch_operation_queue_operation_t *epoch_operation_queue_enqueue(
+        epoch_operation_queue_t *epoch_operation_queue) {
+    epoch_operation_queue_operation_t operation = {
+            .data = {
+                    .start_epoch = intrinsics_tsc(),
+                    .end_epoch = 0,
+            }
+    };
+
+    epoch_operation_queue_operation_t *operation_ptr =
+            (epoch_operation_queue_operation_t *)ring_bounded_queue_spsc_uint128_enqueue_ptr(
+                    epoch_operation_queue->ring, operation._packed);
+
+    return operation_ptr;
+}
+
+void epoch_operation_queue_mark_completed(
+        epoch_operation_queue_operation_t *epoch_operation_queue_operation) {
+    epoch_operation_queue_operation->data.end_epoch = intrinsics_tsc();
+}
+
+uint64_t epoch_operation_queue_get_latest_epoch(
+        epoch_operation_queue_t *epoch_operation_queue) {
+    bool found;
+    epoch_operation_queue_operation_t operation;
+
+    do {
+        operation._packed = ring_bounded_queue_spsc_uint128_peek(epoch_operation_queue->ring, &found);
+
+        if (!found) {
+            break;
+        }
+
+        if (operation.data.end_epoch == 0) {
+            break;
+        }
+
+        ring_bounded_queue_spsc_uint128_dequeue(epoch_operation_queue->ring, NULL);
+        epoch_operation_queue->latest_epoch = operation.data.end_epoch;
+    } while(true);
+
+    return epoch_operation_queue->latest_epoch;
+}
---
+++ src/epoch_operation_queue.h
@@ -0,0 +1,43 @@
+#ifndef CACHEGRAND_EPOCH_OPERATION_QUEUE_H
+#define CACHEGRAND_EPOCH_OPERATION_QUEUE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define EPOCH_OPERATION_QUEUE_RING_SIZE (16 * 1024)
+
+typedef union epoch_operation_queue_operation epoch_operation_queue_operation_t;
+union epoch_operation_queue_operation {
+    struct {
+        uint64_t start_epoch;
+        uint64_t end_epoch;
+    } data;
+    uint128_t _packed;
+};
+
+typedef struct epoch_operation_queue epoch_operation_queue_t;
+struct epoch_operation_queue {
+    ring_bounded_queue_spsc_uint128_t *ring;
+    uint64_t latest_epoch;
+};
+
+epoch_operation_queue_t *epoch_operation_queue_init();
+
+void epoch_operation_queue_free(
+        epoch_operation_queue_t *epoch_operation_queue);
+
+epoch_operation_queue_operation_t *epoch_operation_queue_enqueue(
+        epoch_operation_queue_t *epoch_operation_queue);
+
+void epoch_operation_queue_mark_completed(
+        epoch_operation_queue_operation_t *epoch_operation_queue_operation);
+
+uint64_t epoch_operation_queue_get_latest_epoch(
+        epoch_operation_queue_t *epoch_operation_queue);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CACHEGRAND_EPOCH_OPERATION_QUEUE_H
