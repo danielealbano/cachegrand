df96d4721c5b442ecc0ecaf8c8a79ff36b1d9953
Daniele Salvatore Albano <d.albano@gmail.com>
2022-11-29 23:50:22 +0100

    Style fix

--- benches/bench-hashtable-mpmc-new-op-set.cpp
+++ benches/bench-hashtable-mpmc-new-op-set.cpp
@@ -37,15 +37,16 @@
 #include "mimalloc.h"
 
 #define TEST_VALIDATE_KEYS 0
+
 #define BENCH_HASHTABLE_MPMC_NEW_OP_SET_KEY_MIN_LENGTH 7
 #define BENCH_HASHTABLE_MPMC_NEW_OP_SET_KEY_MAX_LENGTH 8
 
 // Keys generation
 #define TEST_HASHTABLE_MPMC_FUZZY_TESTING_KEYS_CHARACTER_SET \
-    'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k', 'l','z','x','c','v','b','n','m', \
-    'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k', 'l','z','x','c','v','b','n','m', \
-    'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K', 'L','Z','X','C','V','B','N','M', \
-    'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K', 'L','Z','X','C','V','B','N','M', \
+    'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m', \
+    'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m', \
+    'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M', \
+    'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M', \
     '1','2','3','4','5','6','7','8','9','0', '1','2','3','4','5','6','7','8','9','0', \
     '.',',','/','|','\'',';',']','[','<','>','?',':','"','{','}','!','@','$','%','^','&','*','(',')','_','-','=','+','#'
 
