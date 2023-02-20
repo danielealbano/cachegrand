f9180821c4ae9649eeb489940f6392b90d5ccb34
Daniele Salvatore Albano <d.albano@gmail.com>
2023-02-20 22:55:35 +0100

    Rework the algorithm to leverage a table to map the first bit set to 0 in a  uint16_t number to perform only 4 checks instead of 64, improve documentation

--- src/data_structures/slots_bitmap_spsc/slots_bitmap_spsc.c
+++ src/data_structures/slots_bitmap_spsc/slots_bitmap_spsc.c
@@ -6,12 +6,13 @@
  * of the BSD license. See the LICENSE file for details.
  **/
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 #include <string.h>
-#include <stdatomic.h>
 #include <assert.h>
 
 #include "exttypes.h"
@@ -21,13 +22,19 @@
 #include "data_structures/queue_mpmc/queue_mpmc.h"
 #include "memory_allocator/ffma.h"
 
+#include "slots_bitmap_spsc_first_free_bit_table.h"
 #include "slots_bitmap_spsc.h"
 
 #define TAG "slots_bitmap_spsc"
 
 uint64_t slots_bitmap_spsc_calculate_shard_count(
         uint64_t size) {
-    return (size / SLOTS_BITMAP_SPSC_SHARD_SIZE) + 1;
+    return (uint64_t)ceil((double)size / (double)SLOTS_BITMAP_SPSC_SHARD_SIZE);
+}
+
+uint64_t slots_bitmap_spsc_calculate_shard_full_count(
+        uint64_t shards_count) {
+    return (uint64_t)ceil((double)shards_count / (double)sizeof(uint64_t));
 }
 
 slots_bitmap_spsc_t *slots_bitmap_spsc_init(
@@ -37,103 +44,157 @@ slots_bitmap_spsc_t *slots_bitmap_spsc_init(
     size_t allocation_size =
             sizeof(slots_bitmap_spsc_t) +
             (sizeof(slots_bitmap_spsc_shard_t) * shards_count) +
-            (sizeof(uint8_t) * shards_count);
+            (sizeof(uint8_t) * shards_count) +
+            (sizeof(uint64_t) * (size_t)slots_bitmap_spsc_calculate_shard_full_count(shards_count));
 
-    slots_bitmap_spsc_t *bitmap = (slots_bitmap_spsc_t *)ffma_mem_alloc(allocation_size);
+    slots_bitmap_spsc_t *slots_bitmap = (slots_bitmap_spsc_t *)ffma_mem_alloc(allocation_size);
 
-    if (bitmap == NULL) {
+    if (slots_bitmap == NULL) {
         return NULL;
     }
 
-    memset(bitmap, 0, allocation_size);
+    memset(slots_bitmap, 0, allocation_size);
 
-    bitmap->size = size;
-    bitmap->shards_count = shards_count;
+    slots_bitmap->size = shards_count * SLOTS_BITMAP_SPSC_SHARD_SIZE;
+    slots_bitmap->shards_count = shards_count;
 
-    return bitmap;
+    return slots_bitmap;
 }
 
 void slots_bitmap_spsc_free(
-        slots_bitmap_spsc_t *bitmap) {
-    ffma_mem_free(bitmap);
+        slots_bitmap_spsc_t *slots_bitmap) {
+    ffma_mem_free(slots_bitmap);
 }
 
 slots_bitmap_spsc_shard_t* slots_bitmap_spsc_get_shard_ptr(
-        slots_bitmap_spsc_t *bitmap,
+        slots_bitmap_spsc_t *slots_bitmap,
         uint64_t shard_index) {
+    uintptr_t bitmap_ptr = (uintptr_t)slots_bitmap;
+    uintptr_t bitmap_shards_ptr = bitmap_ptr + offsetof(slots_bitmap_spsc_t, shards);
+    uintptr_t bitmap_shard_ptr = bitmap_shards_ptr + (sizeof(slots_bitmap_spsc_shard_t) * shard_index);
 
-    return (slots_bitmap_spsc_shard_t*)
-                   bitmap +
-           offsetof(slots_bitmap_spsc_t, shards) +
-           (sizeof(slots_bitmap_spsc_shard_t) * shard_index);
+    return (slots_bitmap_spsc_shard_t*)bitmap_shard_ptr;
 }
 
 uint8_t* slots_bitmap_spsc_get_shard_used_count_ptr(
-        slots_bitmap_spsc_t *bitmap,
+        slots_bitmap_spsc_t *slots_bitmap,
         uint64_t shard_index) {
+    uintptr_t bitmap_ptr = (uintptr_t)slots_bitmap;
+    uintptr_t bitmap_shards_ptr = bitmap_ptr + offsetof(slots_bitmap_spsc_t, shards);
+    uintptr_t bitmap_shards_used_slots_ptr =
+            bitmap_shards_ptr + (sizeof(slots_bitmap_spsc_shard_t) * slots_bitmap->shards_count);
+    uintptr_t bitmap_shard_used_slots_ptr =
+            bitmap_shards_used_slots_ptr + (sizeof(uint8_t) * shard_index);
 
-    return (uint8_t*)
-                   bitmap +
-           offsetof(slots_bitmap_spsc_t, shards) +
-           (sizeof(slots_bitmap_spsc_shard_t) * bitmap->shards_count) +
-           (sizeof(uint8_t) * shard_index);
+    return (uint8_t*)bitmap_shard_used_slots_ptr;
 }
 
-bool slots_bitmap_spsc_get_first_available(
-        slots_bitmap_spsc_t *bitmap,
-        uint64_t *return_bitmap_index) {
-    uint8_t *shard_used_count = slots_bitmap_spsc_get_shard_used_count_ptr(bitmap, 0);
-    for(uint64_t bitmap_shard_index = 0; bitmap_shard_index < bitmap->shards_count; bitmap_shard_index++) {
-        if (shard_used_count[bitmap_shard_index] == SLOTS_BITMAP_SPSC_SHARD_SIZE) {
+uint64_t* slots_bitmap_spsc_get_shard_full_ptr(
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t shard_full_index) {
+    uintptr_t bitmap_ptr = (uintptr_t)slots_bitmap;
+    uintptr_t bitmap_shards_ptr = bitmap_ptr + offsetof(slots_bitmap_spsc_t, shards);
+    uintptr_t bitmap_shards_used_slots_ptr =
+            bitmap_shards_ptr + (sizeof(slots_bitmap_spsc_shard_t) * slots_bitmap->shards_count);
+    uintptr_t bitmap_shard_full_ptr =
+            bitmap_shards_used_slots_ptr + (sizeof(uint8_t) * slots_bitmap->shards_count);
+    uintptr_t bitmap_shard_used_slots_ptr =
+            bitmap_shard_full_ptr + (sizeof(uint64_t) * shard_full_index);
+
+    return (uint64_t*)bitmap_shard_used_slots_ptr;
+}
+
+void slots_bitmap_spsc_set_shard_full_bit(
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t shard_index,
+        bool full) {
+    uint64_t shards_full_index = shard_index / sizeof(slots_bitmap->shards_full[0]);
+    uint64_t shards_full_bit_index = shard_index % sizeof(slots_bitmap->shards_full[0]);
+
+    if (full) {
+        *slots_bitmap_spsc_get_shard_full_ptr(slots_bitmap, shards_full_index) |= (1 << shards_full_bit_index);
+    } else {
+        *slots_bitmap_spsc_get_shard_full_ptr(slots_bitmap, shards_full_index) &= ~(1 << shards_full_bit_index);
+    }
+}
+
+bool slots_bitmap_spsc_is_shard_full(
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t shard_index) {
+    uint64_t *bitmap_shards_full_base_ptr = slots_bitmap_spsc_get_shard_full_ptr(slots_bitmap, 0);
+
+    uint64_t shard_full_index = shard_index / sizeof(slots_bitmap->shards_full[0]);
+    uint64_t shard_full_bit_index = shard_index % sizeof(slots_bitmap->shards_full[0]);
+    bool full = (bitmap_shards_full_base_ptr[shard_full_index] >> shard_full_bit_index) & (uint16_t)0x01;
+
+    return full;
+}
+
+uint8_t slots_bitmap_spsc_find_first_zero(
+        slots_bitmap_spsc_shard_t slots_bitmap_shard) {
+    for(int16_t uint16_bytes_index = 0; uint16_bytes_index < 8; uint16_bytes_index += 2) {
+        uint16_t uint16_bytes = (uint16_t)((slots_bitmap_shard >> (uint16_bytes_index * 8)) & 0xFFFF);
+        uint8_t first_free_zero_bit_index = slots_bitmap_spsc_first_free_bit_table[uint16_bytes];
+
+        if (first_free_zero_bit_index == UINT8_MAX) {
             continue;
         }
 
-        slots_bitmap_spsc_shard_t *bitmap_shard_ptr =
-                slots_bitmap_spsc_get_shard_ptr(bitmap, bitmap_shard_index);
-        for(
-                uint64_t bitmap_shard_bit_index = 0;
-                bitmap_shard_bit_index < SLOTS_BITMAP_SPSC_SHARD_SIZE;
-                bitmap_shard_bit_index++) {
-            uint64_t value =
-                    (*bitmap_shard_ptr >> bitmap_shard_bit_index) & (SLOTS_BITMAP_SPSC_SHARD_DATA_TYPE)0x01;
+        return first_free_zero_bit_index + (uint16_bytes_index * 8);
+    }
 
-            // If the shard bit is already set, skip the bit
-            if (value != 0) {
-                continue;
-            }
+    return UINT8_MAX;
+}
 
-            // Update the counter
-            shard_used_count[bitmap_shard_index]++;
+bool slots_bitmap_spsc_get_first_available(
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t *return_slots_bitmap_index) {
+    uint8_t *shard_used_count_base_ptr = slots_bitmap_spsc_get_shard_used_count_ptr(slots_bitmap, 0);
+    slots_bitmap_spsc_shard_t *bitmap_shard_base_ptr = slots_bitmap_spsc_get_shard_ptr(slots_bitmap, 0);
 
-            // Calculate the new value of the shard
-            *bitmap_shard_ptr =
-                    *bitmap_shard_ptr | ((slots_bitmap_spsc_shard_t)1 << bitmap_shard_bit_index);
-
-            // Update the return value
-            *return_bitmap_index = (bitmap_shard_index * SLOTS_BITMAP_SPSC_SHARD_SIZE) + bitmap_shard_bit_index;
-
-            return true;
+    for(uint64_t shard_index = 0; shard_index < slots_bitmap->shards_count; shard_index++) {
+        if (slots_bitmap_spsc_is_shard_full(slots_bitmap, shard_index)) {
+            continue;
         }
 
-        // Should never ever get here as the shard_used_count was initially smaller than
-        // SLOTS_BITMAP_SPSC_SHARD_SIZE and therefore there should be at least one bit available
-        assert(false);
+        slots_bitmap_spsc_shard_t *bitmap_shard_ptr = &bitmap_shard_base_ptr[shard_index];
+        int16_t bitmap_shard_bit_index = slots_bitmap_spsc_find_first_zero(*bitmap_shard_ptr);
+
+        // There should always be a bit available if the shard is not full
+        assert(bitmap_shard_bit_index < UINT8_MAX);
+
+        // Update the counter
+        shard_used_count_base_ptr[shard_index]++;
+
+        // Calculate the new value of the shard
+        *bitmap_shard_ptr = *bitmap_shard_ptr | ((slots_bitmap_spsc_shard_t)1 << bitmap_shard_bit_index);
+
+        // Update the return value
+        *return_slots_bitmap_index = (shard_index * SLOTS_BITMAP_SPSC_SHARD_SIZE) + bitmap_shard_bit_index;
+
+        // If the shard is full after acquiring this bit, update the shards_full bitmap
+        if (shard_used_count_base_ptr[shard_index] == SLOTS_BITMAP_SPSC_SHARD_SIZE) {
+            slots_bitmap_spsc_set_shard_full_bit(slots_bitmap, shard_index, true);
+        }
+
+        return true;
     }
 
     return false;
 }
 
 void slots_bitmap_spsc_release(
-        slots_bitmap_spsc_t *bitmap,
+        slots_bitmap_spsc_t *slots_bitmap,
         uint64_t bitmap_index) {
-    uint8_t *shard_used_count = slots_bitmap_spsc_get_shard_used_count_ptr(bitmap, 0);
-    uint16_t bitmap_shard_index = bitmap_index / SLOTS_BITMAP_SPSC_SHARD_SIZE;
+    uint8_t *shard_used_count = slots_bitmap_spsc_get_shard_used_count_ptr(slots_bitmap, 0);
+    uint16_t shard_index = bitmap_index / SLOTS_BITMAP_SPSC_SHARD_SIZE;
     uint8_t bitmap_shard_bit_index = bitmap_index % SLOTS_BITMAP_SPSC_SHARD_SIZE;
 
     // Update the counter
-    shard_used_count[bitmap_shard_index]--;
+    shard_used_count[shard_index]--;
+    slots_bitmap_spsc_set_shard_full_bit(slots_bitmap, shard_index, false);
 
     // Update the shard value
-    slots_bitmap_spsc_shard_t *bitmap_shard_ptr = slots_bitmap_spsc_get_shard_ptr(bitmap, bitmap_shard_index);
-    *bitmap_shard_ptr = *bitmap_shard_ptr - ((SLOTS_BITMAP_SPSC_SHARD_DATA_TYPE)1 << bitmap_shard_bit_index);
+    slots_bitmap_spsc_shard_t *bitmap_shard_ptr = slots_bitmap_spsc_get_shard_ptr(slots_bitmap, shard_index);
+    *bitmap_shard_ptr = *bitmap_shard_ptr - ((slots_bitmap_spsc_shard_t)1 << bitmap_shard_bit_index);
 }
--- src/data_structures/slots_bitmap_spsc/slots_bitmap_spsc.h
+++ src/data_structures/slots_bitmap_spsc/slots_bitmap_spsc.h
@@ -10,38 +10,117 @@ extern "C" {
 
 typedef SLOTS_BITMAP_SPSC_SHARD_DATA_TYPE slots_bitmap_spsc_shard_t;
 
+/**
+ * Bitmap structure
+ *
+ * The bitmap is divided in shards, each shard is a 64 bit integer and the amount of occupied slots is tracked in
+ * shards_used_slots. When a shard is full, the corresponding bit in shards_full is set.
+ * The algorithm will use the shards_full bitmap to find the first shard that is not full and then search a free slot
+ * in that shard.
+ * When a free bit is found the counter in shards_used_slots is incremented and the new value will match the amount of
+ * available slots in the shard then the shard is marked as full.
+ */
 typedef struct slots_bitmap_spsc slots_bitmap_spsc_t;
 struct slots_bitmap_spsc {
     size_t size;
     size_t shards_count;
     slots_bitmap_spsc_shard_t shards[0];
     uint8_t shards_used_slots[0];
+    uint64_t shards_full[0];
 };
 
+/**
+ * Calculate the number of shards needed to store the given size
+ * @param size Size of the bitmap
+ * @return Number of shards needed to store the given size
+ */
 uint64_t slots_bitmap_spsc_calculate_shard_count(
         uint64_t size);
 
+/**
+ * Initialize a new bitmap
+ * @param size Size of the bitmap
+ * @return Pointer to the bitmap
+ */
 slots_bitmap_spsc_t *slots_bitmap_spsc_init(
         uint64_t size);
 
+/**
+ * Free the bitmap
+ * @param slots_bitmap Pointer to the bitmap
+ */
 void slots_bitmap_spsc_free(
-        slots_bitmap_spsc_t *bitmap);
+        slots_bitmap_spsc_t *slots_bitmap);
 
+/**
+ * Get the pointer to the shard
+ * @param slots_bitmap Pointer to the bitmap
+ * @param shard_index Index of the shard
+ * @return Pointer to the shard
+ */
 slots_bitmap_spsc_shard_t* slots_bitmap_spsc_get_shard_ptr(
-        slots_bitmap_spsc_t *bitmap,
+        slots_bitmap_spsc_t *slots_bitmap,
         uint64_t shard_index);
 
+/**
+ * Get the pointer to the shard used count of a shard
+ * @param slots_bitmap Pointer to the bitmap
+ * @param shard_index Index of the shard
+ * @return Pointer to the shard used count
+ */
 uint8_t* slots_bitmap_spsc_get_shard_used_count_ptr(
-        slots_bitmap_spsc_t *bitmap,
+        slots_bitmap_spsc_t *slots_bitmap,
         uint64_t shard_index);
 
-bool slots_bitmap_spsc_get_first_available(
-        slots_bitmap_spsc_t *bitmap,
-        uint64_t *return_bitmap_index);
+/**
+ * Get the pointer to the shard full bitmap
+ * @param slots_bitmap Pointer to the bitmap
+ * @param shard_full_index Index of the shard full bitmap (index of the shard / 64)
+ * @return Pointer to the shard full bitmap
+ */
+uint64_t* slots_bitmap_spsc_get_shard_full_ptr(
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t shard_full_index);
 
+/**
+ * Set the full bit of a shard
+ * @param slots_bitmap Pointer to the bitmap
+ * @param shard_index Index of the shard
+ * @param full True if the shard is full, false otherwise
+ */
+void slots_bitmap_spsc_set_shard_full_bit(
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t shard_index,
+        bool full);
+
+/**
+ * Get the full bit of a shard
+ * @param slots_bitmap Pointer to the bitmap
+ * @param shard_index Index of the shard
+ * @return True if the shard is full, false otherwise
+ */
+bool slots_bitmap_spsc_is_shard_full(
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t shard_index);
+
+/**
+ * Get the first available slot in the bitmap
+ * @param slots_bitmap Pointer to the bitmap
+ * @param return_slots_bitmap_index Pointer to the variable that will contain the index of the first available slot
+ * @return True if a slot was found, false otherwise
+ */
+bool slots_bitmap_spsc_get_first_available(
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t *return_slots_bitmap_index);
+
+/**
+ * Release a slot in the bitmap
+ * @param slots_bitmap Pointer to the bitmap
+ * @param slots_bitmap_index Index of the slot to release
+ */
 void slots_bitmap_spsc_release(
-        slots_bitmap_spsc_t *bitmap,
-        uint64_t bitmap_index);
+        slots_bitmap_spsc_t *slots_bitmap,
+        uint64_t slots_bitmap_index);
 
 #ifdef __cplusplus
 }
