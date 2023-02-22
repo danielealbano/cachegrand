4039857791d2c7634ff492b29ab7c2590c95c537
Daniele Salvatore Albano <d.albano@gmail.com>
2023-02-22 19:35:09 +0100

    Improve the search algorithm using a pre-computed table for the first free bit and start/step params for the search on the shard, useful to spread the searches across the table depending on the id of the thread

--- src/data_structures/slots_bitmap_mpmc/slots_bitmap_mpmc.c
+++ src/data_structures/slots_bitmap_mpmc/slots_bitmap_mpmc.c
@@ -23,6 +23,7 @@
 #include "data_structures/queue_mpmc/queue_mpmc.h"
 #include "memory_allocator/ffma.h"
 
+#include "slots_bitmap_mpmc_first_free_bit_table.h"
 #include "slots_bitmap_mpmc.h"
 
 #define TAG "slots_bitmap_mpmc"
@@ -77,6 +78,7 @@ slots_bitmap_mpmc_shard_t* slots_bitmap_mpmc_get_shard_ptr(
     uintptr_t bitmap_shards_ptr = bitmap_ptr + offsetof(slots_bitmap_mpmc_t, shards);
     uintptr_t bitmap_shard_ptr = bitmap_shards_ptr + (sizeof(slots_bitmap_mpmc_shard_t) * shard_index);
 
+    // Return a pointer to the shard
     return (slots_bitmap_mpmc_shard_t*)bitmap_shard_ptr;
 }
 
@@ -91,124 +93,202 @@ uint8_t* slots_bitmap_mpmc_get_shard_used_count_ptr(
     uintptr_t bitmap_shard_used_slots_ptr =
             bitmap_shards_used_slots_ptr + (sizeof(uint8_t) * shard_index);
 
+    // Return a pointer to the shard's used count
     return (uint8_t*)bitmap_shard_used_slots_ptr;
 }
 
-bool slots_bitmap_mpmc_get_next_available_ptr(
-        slots_bitmap_mpmc_t *bitmap,
+uint8_t slots_bitmap_mpmc_shard_find_first_zero(
+        slots_bitmap_mpmc_shard_t slots_bitmap_shard) {
+    // Iterate over the 8 bytes of the shard
+    for(int16_t uint16_bytes_index = 0; uint16_bytes_index < 8; uint16_bytes_index += 2) {
+        // Get the 2 bytes of the shard
+        uint16_t uint16_bytes = (uint16_t)((slots_bitmap_shard >> (uint16_bytes_index * 8)) & 0xFFFF);
+
+        // Fetch from the mapping table the index of the first zero bit
+        uint8_t first_free_zero_bit_index = slots_bitmap_mpmc_first_free_bit_table[uint16_bytes];
+
+        // If the index is UINT8_MAX, then there are no zero bits in the 2 bytes
+        if (unlikely(first_free_zero_bit_index == UINT8_MAX)) {
+            continue;
+        }
+
+        // Return the index of the first zero bit
+        return first_free_zero_bit_index + (uint16_bytes_index * 8);
+    }
+
+    // If no zero bits were found, return UINT8_MAX
+    return UINT8_MAX;
+}
+
+bool slots_bitmap_mpmc_shard_is_full(
+        slots_bitmap_mpmc_t *slots_bitmap,
+        uint64_t shard_index) {
+    // Get the shard's used count
+    uint8_volatile_t *shard_used_count_ptr = slots_bitmap_mpmc_get_shard_used_count_ptr(slots_bitmap, shard_index);
+    MEMORY_FENCE_LOAD();
+    uint8_volatile_t shard_used_count = *shard_used_count_ptr;
+
+    // If the shard's used count is equal to the shard's size, then the shard is full
+    return shard_used_count == SLOTS_BITMAP_MPMC_SHARD_SIZE;
+}
+
+void slots_bitmap_mpmc_shard_increase_used_count(
+        slots_bitmap_mpmc_t *slots_bitmap,
+        uint64_t shard_index) {
+    // Get the shard's used count
+    uint8_volatile_t *shard_used_count_ptr =
+            slots_bitmap_mpmc_get_shard_used_count_ptr(slots_bitmap, shard_index);
+
+    // Increase the shard's used count
+    __atomic_fetch_add(shard_used_count_ptr, 1, __ATOMIC_ACQ_REL);
+}
+
+void slots_bitmap_mpmc_shard_decrease_used_count(
+        slots_bitmap_mpmc_t *slots_bitmap,
+        uint64_t shard_index) {
+    // Get the shard's used count
+    uint8_volatile_t *shard_used_count_ptr =
+            slots_bitmap_mpmc_get_shard_used_count_ptr(slots_bitmap, shard_index);
+
+    // Increase the shard's used count
+    __atomic_fetch_sub(shard_used_count_ptr, 1, __ATOMIC_ACQ_REL);
+}
+
+bool slots_bitmap_mpmc_get_next_available_ptr_with_step(
+        slots_bitmap_mpmc_t *slots_bitmap,
+        uint16_t shard_index_start,
+        uint16_t shard_index_step,
         uint64_t *return_slots_bitmap_index) {
     bool should_restart_loop = false;
-    uint8_t *shard_used_count = slots_bitmap_mpmc_get_shard_used_count_ptr(bitmap, 0);
+    // Get a pointer to the first shard
+    slots_bitmap_mpmc_shard_t *bitmap_shard_base_ptr = slots_bitmap_mpmc_get_shard_ptr(slots_bitmap, 0);
 
+    // For performance reasons if a shard is initially found free but later was when it tries to be update the operation
+    // fail because another thread has changed it, instead of trying again to search on that one it moves forward to
+    // search for another slot in another shard.
+    // This approach will reduce the amount of concurrency issues and improve performance, but it will also increase
+    // the probability of having to restart the loop.
     do {
+        // Reset the restart loop flag
         should_restart_loop = false;
 
-        for(uint64_t bitmap_shard_index = 0; bitmap_shard_index < bitmap->shards_count; bitmap_shard_index++) {
+        // Iterate over all shards
+        for(
+                uint64_t shard_index = shard_index_start;
+                likely(shard_index < slots_bitmap->shards_count);
+                shard_index += shard_index_step) {
             MEMORY_FENCE_LOAD();
-            if (unlikely(shard_used_count[bitmap_shard_index] == SLOTS_BITMAP_MPMC_SHARD_SIZE)) {
+
+            // Check if the shard is full
+            if (unlikely(slots_bitmap_mpmc_shard_is_full(slots_bitmap, shard_index))) {
                 continue;
             }
 
-            bool found = false;
-            slots_bitmap_mpmc_shard_t *bitmap_shard_ptr = slots_bitmap_mpmc_get_shard_ptr(bitmap, bitmap_shard_index);
-            for(
-                    uint64_t bitmap_shard_bit_index = 0;
-                    bitmap_shard_bit_index < SLOTS_BITMAP_MPMC_SHARD_SIZE;
-                    bitmap_shard_bit_index++) {
-                MEMORY_FENCE_LOAD();
-                slots_bitmap_mpmc_shard_t bitmap_shard = *bitmap_shard_ptr;
-                uint64_t value =
-                        (bitmap_shard >> bitmap_shard_bit_index) & (SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE)0x01;
+            // Fetch the shard pointer and try to find a free slot
+            slots_bitmap_mpmc_shard_t *shard_ptr = &bitmap_shard_base_ptr[shard_index];
+            slots_bitmap_mpmc_shard_t shard_value = *shard_ptr;
+            uint8_t shard_bit_index = slots_bitmap_mpmc_shard_find_first_zero(shard_value);
 
-                // If the shard bit is already set, skip the bit
-                if (value != 0) {
-                    continue;
-                }
-
-                // Calculate the new value of the shard
-                slots_bitmap_mpmc_shard_t bitmap_shard_new =
-                        *bitmap_shard_ptr | ((slots_bitmap_mpmc_shard_t)1 << bitmap_shard_bit_index);
-
-                // Try to set the bit to 1
-                if (!__atomic_compare_exchange_n(
-                        bitmap_shard_ptr,
-                        (SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE*)&bitmap_shard,
-                        bitmap_shard_new,
-                        true,
-                        __ATOMIC_ACQ_REL,
-                        __ATOMIC_ACQUIRE)) {
-                    // The CAS operation failed, another thread might have changed the bitmap shard, try again.
-                    // For performance reasons the search doesn't start from the beginning again but instead continue
-                    // looking at the next bit (or shard) but set the should_restart_loop flag to true so the loop
-                    // will restart from the beginning if nothing is found.
-                    should_restart_loop = true;
-                    continue;
-                }
-
-                // Update the counter
-                __atomic_fetch_add(&shard_used_count[bitmap_shard_index], 1, __ATOMIC_ACQ_REL);
-
-                // The CAS operation succeeded, the bit has been set to 1, return the bitmap index
-                *return_slots_bitmap_index = (bitmap_shard_index * SLOTS_BITMAP_MPMC_SHARD_SIZE) + bitmap_shard_bit_index;
-
-                return true;
+            // Check if the find first zero function was able to find a free slot
+            if (unlikely(shard_bit_index == UINT8_MAX)) {
+                continue;
             }
 
-            // If the initial expectation was that there should have been space but there wasn't
-            if (unlikely(!found)) {
-                break;
+            // Calculate the new value of the shard
+            slots_bitmap_mpmc_shard_t shard_value_new =
+                    *shard_ptr | ((slots_bitmap_mpmc_shard_t)1 << shard_bit_index);
+
+            // Try to set the bit to 1
+            if (unlikely(!__atomic_compare_exchange_n(
+                    shard_ptr,
+                    (SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE*)&shard_value,
+                    shard_value_new,
+                    true,
+                    __ATOMIC_ACQ_REL,
+                    __ATOMIC_ACQUIRE))) {
+                // The CAS operation failed, another thread might have changed the bitmap shard, try again.
+                // For performance reasons the search doesn't start from the beginning again but instead continue
+                // looking at the next bit (or shard) but set the should_restart_loop flag to true so the loop
+                // will restart from the beginning if nothing is found.
+                should_restart_loop = true;
+                continue;
             }
+
+            // Update the counter
+            slots_bitmap_mpmc_shard_increase_used_count(slots_bitmap, shard_index);
+
+            // The CAS operation succeeded, the bit has been set to 1, return the bitmap index
+            *return_slots_bitmap_index = (shard_index * SLOTS_BITMAP_MPMC_SHARD_SIZE) + shard_bit_index;
+
+            return true;
         }
-    } while(should_restart_loop);
+    } while(likely(should_restart_loop));
 
     return false;
+}
+
+bool slots_bitmap_mpmc_get_next_available_ptr(
+        slots_bitmap_mpmc_t *slots_bitmap,
+        uint64_t *return_slots_bitmap_index) {
+    return slots_bitmap_mpmc_get_next_available_ptr_with_step(
+            slots_bitmap,
+            0,
+            1,
+            return_slots_bitmap_index);
 }
 
 uint64_t slots_bitmap_mpmc_get_next_available(
         slots_bitmap_mpmc_t *slots_bitmap) {
     uint64_t index;
 
-    if (slots_bitmap_mpmc_get_next_available_ptr(slots_bitmap, &index)) {
+    // Try to get the next available slot
+    if (likely(slots_bitmap_mpmc_get_next_available_ptr(slots_bitmap, &index))) {
+        // Return the index
         return index;
     }
 
+    // No slot was found, return UINT64_MAX
     return UINT64_MAX;
 }
 
 void slots_bitmap_mpmc_release(
         slots_bitmap_mpmc_t *bitmap,
         uint64_t slots_bitmap_index) {
-    uint8_t *shard_used_count = slots_bitmap_mpmc_get_shard_used_count_ptr(bitmap, 0);
-    uint16_t bitmap_shard_index = slots_bitmap_index / SLOTS_BITMAP_MPMC_SHARD_SIZE;
-    uint8_t bitmap_shard_bit_index = slots_bitmap_index % SLOTS_BITMAP_MPMC_SHARD_SIZE;
+    uint16_t shard_index = slots_bitmap_index / SLOTS_BITMAP_MPMC_SHARD_SIZE;
+    uint8_t shard_bit_index = slots_bitmap_index % SLOTS_BITMAP_MPMC_SHARD_SIZE;
 
     // Update the shard value
-    slots_bitmap_mpmc_shard_t bitmap_shard;
-    slots_bitmap_mpmc_shard_t bitmap_shard_new;
-    slots_bitmap_mpmc_shard_t *bitmap_shard_ptr = slots_bitmap_mpmc_get_shard_ptr(bitmap, bitmap_shard_index);
+    slots_bitmap_mpmc_shard_t shard_value;
+    slots_bitmap_mpmc_shard_t shard_value_new;
+    slots_bitmap_mpmc_shard_t *shard_ptr = slots_bitmap_mpmc_get_shard_ptr(bitmap, shard_index);
 
+    // Other threads might try to change the shard value, so the loop will try to set the bit to 0 until it succeeds
+    // using the CAS operation.
     do {
         MEMORY_FENCE_LOAD();
-        bitmap_shard = *bitmap_shard_ptr;
+
+        // Get the shard value
+        shard_value = *shard_ptr;
+
+        // Get the current value of the shard bit
         uint64_t value =
-                (bitmap_shard >> bitmap_shard_bit_index) & (SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE)0x01;
+                (shard_value >> shard_bit_index) & (SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE)0x01;
 
         // If the shard bit is already set to 0, break the loop so the outer loop can move forward
         if (unlikely(value == 0)) {
-            break;
+            return;
         }
 
         // Calculate the new value of the shard
-        bitmap_shard_new =
-                bitmap_shard - ((SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE)1 << bitmap_shard_bit_index);
-    } while (!__atomic_compare_exchange_n(
-            bitmap_shard_ptr,
-            (SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE*)&bitmap_shard,
-            bitmap_shard_new,
+        shard_value_new = shard_value - ((SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE)1 << shard_bit_index);
+    } while (unlikely(!__atomic_compare_exchange_n(
+            shard_ptr,
+            (SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE*)&shard_value,
+            shard_value_new,
             true,
             __ATOMIC_ACQ_REL,
-            __ATOMIC_ACQUIRE));
+            __ATOMIC_ACQUIRE)));
 
     // Update the counter
-    __atomic_fetch_sub(&shard_used_count[bitmap_shard_index], 1, __ATOMIC_ACQ_REL);
+    slots_bitmap_mpmc_shard_decrease_used_count(bitmap, shard_index);
 }
--- src/data_structures/slots_bitmap_mpmc/slots_bitmap_mpmc.h
+++ src/data_structures/slots_bitmap_mpmc/slots_bitmap_mpmc.h
@@ -11,6 +11,21 @@ extern "C" {
 
 typedef SLOTS_BITMAP_MPMC_SHARD_DATA_TYPE_VOLATILE slots_bitmap_mpmc_shard_t;
 
+/**
+ * Bitmap structure
+ *
+ * The bitmap is divided in shards, each shard is a 64 bit integer and the amount of occupied slots is tracked in
+ * shards_used_slots. When a shard is full, the corresponding value in shards_used_slots will match the amount of
+ * available slots in the shard (SLOTS_BITMAP_MPMC_SHARD_SIZE).
+ * The algorithm will use check shards_used_slots to find the first shard that is not full and then search a free slot
+ * in that shard, although the shard may be full by the time the slot is found because another thread may have
+ * allocated a slot in the same shard.
+ * The algorithm uses CAS operations to update the shard, if the CAS fails the shard the algorithm will stop checking
+ * that shard and will move to the next one marking that it might have to restart from the beginning if nothing is
+ * found.
+ * When a free bit is found the counter in shards_used_slots is incremented and the new value will match the amount of
+ * available slots in the shard then the shard is marked as full.
+ */
 typedef struct slots_bitmap_mpmc slots_bitmap_mpmc_t;
 struct slots_bitmap_mpmc {
     size_t size;
@@ -21,14 +36,16 @@ struct slots_bitmap_mpmc {
 
 /**
  * Calculate the number of shards needed to store the given size
- * @param size
- * @return
+ *
+ * @param size Size of the bitmap
+ * @return The number of shards needed to store the given size
  */
 uint64_t slots_bitmap_mpmc_calculate_shard_count(
         uint64_t size);
 
 /**
  * Initialize a new concurrent bitmap with the given size
+ *
  * @param size Size of the bitmap
  * @return A pointer to the new concurrent bitmap, or NULL if memory allocation fails or size is 0
  */
@@ -37,6 +54,7 @@ slots_bitmap_mpmc_t *slots_bitmap_mpmc_init(
 
 /**
  * Free the bitmap
+ *
  * @param slots_bitmap Pointer to the bitmap
  */
 void slots_bitmap_mpmc_free(
@@ -44,6 +62,7 @@ void slots_bitmap_mpmc_free(
 
 /**
  * Get the pointer to the shard
+ *
  * @param slots_bitmap Pointer to the bitmap
  * @param shard_index Index of the shard
  * @return Pointer to the shard
@@ -54,6 +73,7 @@ slots_bitmap_mpmc_shard_t* slots_bitmap_mpmc_get_shard_ptr(
 
 /**
  * Get the pointer to the shard used count of a shard
+ *
  * @param slots_bitmap Pointer to the bitmap
  * @param shard_index Index of the shard
  * @return Pointer to the shard used count
@@ -65,17 +85,44 @@ uint8_t* slots_bitmap_mpmc_get_shard_used_count_ptr(
 /**
  * Get the first available slot in the bitmap, the slot is returned as a pointer to the slot, a boolean is returned
  * to indicate if a slot was found or not.
+ * The main difference between slots_bitmap_mpmc_get_next_available_ptr_with_step and
+ * slots_bitmap_mpmc_get_next_available_ptr is that this function allows to specify the shard to start the search
+ * from and the step to use when searching for the next shard.
+ * This mechanism allows to implement a more efficient search strategy when using multiple threads, for example
+ * each thread can start the search from a different shard and use a step of 1, this way each thread will search
+ * a different shard and will clash less with other threads.
+ * When using this strategy though it's suggested to invoke the function again with start 0 and step 1 if the
+ * first invocation returns false, as this will allow to search the shards and handle the case in which all the shards
+ * of a thread are full.
+ *
+ * @param slots_bitmap Pointer to the bitmap
+ * @param shard_index_start Index of the shard to start the search from
+ * @param shard_index_step Step to use when searching for the next shard
+ * @param return_slots_bitmap_index Pointer to the variable that will contain the index of the first available slot
+ * @return True if a slot was found, false otherwise
+ */
+bool slots_bitmap_mpmc_get_next_available_ptr_with_step(
+        slots_bitmap_mpmc_t *slots_bitmap,
+        uint16_t shard_index_start,
+        uint16_t shard_index_step,
+        uint64_t *return_slots_bitmap_index);
+
+/**
+ * Get the first available slot in the bitmap, the slot is returned as a pointer to the slot, a boolean is returned
+ * to indicate if a slot was found or not.
+ *
  * @param slots_bitmap Pointer to the bitmap
  * @param return_slots_bitmap_index Pointer to the variable that will contain the index of the first available slot
  * @return True if a slot was found, false otherwise
  */
 bool slots_bitmap_mpmc_get_next_available_ptr(
-        slots_bitmap_mpmc_t *bitmap,
+        slots_bitmap_mpmc_t *slots_bitmap,
         uint64_t *return_slots_bitmap_index);
 
 /**
  * Get the first available slot in the bitmap, an index is returned to indicate the slot, in case of failure UINT64_MAX
  * is returned.
+ *
  * @param slots_bitmap Pointer to the bitmap
  * @return Index of the first available slot
  */
@@ -84,6 +131,7 @@ uint64_t slots_bitmap_mpmc_get_next_available(
 
 /**
  * Release a slot in the bitmap
+ *
  * @param slots_bitmap Pointer to the bitmap
  * @param slots_bitmap_index Index of the slot to release
  */
