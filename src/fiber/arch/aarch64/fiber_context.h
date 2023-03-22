1fd8732007088f4a56b64288b0c638f87b7e49f3
Daniele Salvatore Albano <d.albano@gmail.com>
2023-03-22 22:33:22 +0100

    Simplify fiber_context.h header for aarch64 and drop incorrect code (causing syntax errors)

--- src/fiber/arch/aarch64/fiber_context.h
+++ src/fiber/arch/aarch64/fiber_context.h
@@ -1,15 +1,6 @@
 #ifndef CACHEGRAND_FIBER_CONTEXT_H
 #define CACHEGRAND_FIBER_CONTEXT_H
 
-typedef struct fiber_context fiber_context_t;
-
-#if __ARM_PCS_VFP
-vpush {d8-d15}
-    #define FIBER_CONTEXT_NUM_REGISTRIES (9 + 8 * 2)
-#else
-#define FIBER_CONTEXT_NUM_REGISTRIES 9
-#endif
-
-#define FIBER_CONTEXT_NUM_REGISTRIES 6
+#define FIBER_CONTEXT_NUM_REGISTRIES (9 + 8 * 2)
 
 #endif //CACHEGRAND_FIBER_CONTEXT_H
