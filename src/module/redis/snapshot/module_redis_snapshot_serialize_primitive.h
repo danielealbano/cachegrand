4c32d63422ac8b35a1e9232c9d4e518d493b9986
Daniele Salvatore Albano <d.albano@gmail.com>
2023-04-05 14:58:22 +0200

    Add the primitives required to serialize the Key-Value DB in rdb format

---
+++ src/module/redis/snapshot/module_redis_snapshot_serialize_primitive.h
@@ -0,0 +1,70 @@
+#ifndef CACHEGRAND_MODULE_REDIS_SNAPSHOT_SERIALIZE_PRIMITIVE_H
+#define CACHEGRAND_MODULE_REDIS_SNAPSHOT_SERIALIZE_PRIMITIVE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum module_redis_snapshot_serialize_primitive_result {
+    MODULE_REDIS_SNAPSHOT_SERIALIZE_PRIMITIVE_RESULT_OK,
+    MODULE_REDIS_SNAPSHOT_SERIALIZE_PRIMITIVE_RESULT_BUFFER_OVERFLOW,
+    MODULE_REDIS_SNAPSHOT_SERIALIZE_PRIMITIVE_RESULT_INVALID_INTEGER,
+    MODULE_REDIS_SNAPSHOT_SERIALIZE_PRIMITIVE_RESULT_COMPRESSION_FAILED,
+    MODULE_REDIS_SNAPSHOT_SERIALIZE_PRIMITIVE_RESULT_INVALID_VALUE_TYPE,
+};
+typedef enum module_redis_snapshot_serialize_primitive_result module_redis_snapshot_serialize_primitive_result_t;
+
+/**
+ * The LZF_MAX_COMPRESSED_SIZE macro below is imported from from https://github.com/nemequ/liblzf/blob/master/lzf.h,
+ * which is licensed under the following terms
+ *
+ * Copyright (c) 2000-2008 Marc Alexander Lehmann <schmorp@schmorp.de>
+ *
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ *
+ *   1.  Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *   2.  Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MER-
+ * CHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPE-
+ * CIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTH-
+ * ERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * the GNU General Public License ("GPL") version 2 or any later version,
+ * in which case the provisions of the GPL are applicable instead of
+ * the above. If you wish to allow the use of your version of this file
+ * only under the terms of the GPL and not to allow others to use your
+ * version of this file under the BSD license, indicate your decision
+ * by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL. If you do not delete the
+ * provisions above, a recipient may use your version of this file under
+ * either the BSD or the GPL.
+ */
+/**
+ * The maximum out_len that needs to be allocated to make sure
+ * any input data can be compressed without overflowing the output
+ * buffer, i.e. maximum out_len = LZF_MAX_COMPRESSED_SIZE (in_len).
+ * This is useful if you don't want to bother with the case of
+ * incompressible data and just want to provide a buffer that is
+ * guaranteeed to be big enough.
+ * This macro can be used at preprocessing time.
+ */
+#define LZF_MAX_COMPRESSED_SIZE(n) ((((n) * 33) >> 5 ) + 1)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CACHEGRAND_MODULE_REDIS_SNAPSHOT_SERIALIZE_PRIMITIVE_H
