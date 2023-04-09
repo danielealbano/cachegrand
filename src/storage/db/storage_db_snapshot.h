d722f71c2131044acc099609b1d627900e044e21
Daniele Salvatore Albano <d.albano@gmail.com>
2023-04-10 00:27:39 +0200

    Expose storage_db_snapshot functions

--- src/storage/db/storage_db_snapshot.h
+++ src/storage/db/storage_db_snapshot.h
@@ -1,18 +1,114 @@
 #ifndef CACHEGRAND_STORAGE_DB_SNAPSHOT_H
 #define CACHEGRAND_STORAGE_DB_SNAPSHOT_H
 
+#include "memory_allocator/ffma.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-bool storage_db_snapshot_lock_try_acquire(
+
+#define STORAGE_DB_SNAPSHOT_RDB_VERSION (9)
+#define STORAGE_DB_SNAPSHOT_BLOCK_SIZE (64 * 1024)
+#define STORAGE_DB_SNAPSHOT_REPORT_PROGRESS_EVERY_S (3)
+
+struct storage_db_snapshot_entry_index_to_be_deleted {
+    char *key;
+    hashtable_bucket_index_t bucket_index;
+    hashtable_key_size_t key_length;
+    storage_db_entry_index_t *entry_index;
+};
+typedef struct storage_db_snapshot_entry_index_to_be_deleted storage_db_snapshot_entry_index_to_be_deleted_t;
+
+bool storage_db_snapshot_rdb_write_buffer(
+        storage_db_t *db,
+        uint8_t *buffer,
+        size_t buffer_size);
+
+void storage_db_snapshot_completed(
+        storage_db_t *db,
+        storage_db_snapshot_status_t status);
+
+void storage_db_snapshot_failed(
         storage_db_t *db);
 
-void storage_db_snapshot_release_lock(
-            storage_db_t *db);
+bool storage_db_snapshot_should_run(
+        storage_db_t *db);
+
+void storage_db_snapshot_update_next_run_time(
+        storage_db_t *db);
+
+void storage_db_snapshot_wait_for_prepared(
+        storage_db_t *db);
+
+bool storage_db_snapshot_rdb_prepare(
+        storage_db_t *db);
+
+bool storage_db_snapshot_rdb_ensure_prepared(
+        storage_db_t *db);
+
+bool storage_db_snapshot_rdb_write_value_header(
+        storage_db_t *db,
+        char *key,
+        size_t key_length,
+        storage_db_entry_index_t *entry_index);
+
+bool storage_db_snapshot_rdb_write_value_string(
+        storage_db_t *db,
+        storage_db_entry_index_t *entry_index);
+
+bool storage_db_snapshot_rdb_process_entry_index(
+        storage_db_t *db,
+        char *key,
+        size_t key_length,
+        storage_db_entry_index_t *entry_index);
+
+void storage_db_snapshot_mark_as_being_finalized(
+        storage_db_t *db);
+
+bool storage_db_snapshot_completed_successfully(
+        storage_db_t *db);
+
+bool storage_db_snapshot_rdb_completed_successfully(
+        storage_db_t *db);
+
+bool storage_db_snapshot_rdb_process_entry_index_to_be_deleted_queue(
+        storage_db_t *db);
+
+void storage_db_snapshot_rdb_flush_entry_index_to_be_deleted_queue(
+        storage_db_t *db);
 
 bool storage_db_snapshot_rdb_process_block(
         storage_db_t *db,
         bool *last_block);
+
+static inline bool storage_db_snapshot_lock_try_acquire(
+        storage_db_t *db) {
+    // Try to acquire the lock
+    if (likely(spinlock_is_locked(&db->snapshot.spinlock))) {
+        return false;
+    }
+
+    return spinlock_try_lock(&db->snapshot.spinlock);
+}
+
+static inline void storage_db_snapshot_release_lock(
+        storage_db_t *db) {
+    spinlock_unlock(&db->snapshot.spinlock);
+}
+
+static inline bool storage_db_snapshot_is_in_progress(
+        storage_db_t *db) {
+    return db->snapshot.status == STORAGE_DB_SNAPSHOT_STATUS_IN_PROGRESS;
+}
+
+void storage_db_snapshot_report_progress(
+        storage_db_t *db);
+
+static inline bool storage_db_snapshot_should_report_progress(
+        storage_db_t *db) {
+    return clock_monotonic_int64_ms() - db->snapshot.progress_reported_at_ms >
+           STORAGE_DB_SNAPSHOT_REPORT_PROGRESS_EVERY_S * 1000;
+}
 
 static inline bool storage_db_snapshot_queue_entry_index_to_be_deleted(
         storage_db_t *db,
