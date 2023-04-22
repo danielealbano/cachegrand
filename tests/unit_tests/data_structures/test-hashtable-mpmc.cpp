2cbdf7e3f687c933d9fadf21ec3b938160de0b75
Daniele Salvatore Albano <d.albano@gmail.com>
2023-04-22 18:51:32 +0200

    Update tests copyright

--- tests/unit_tests/data_structures/test-hashtable-mpmc.cpp
+++ tests/unit_tests/data_structures/test-hashtable-mpmc.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018-2022 Daniele Salvatore Albano
+ * Copyright (C) 2018-2023 Daniele Salvatore Albano
  * All rights reserved.init
  *
  * This software may be modified and distributed under the terms
