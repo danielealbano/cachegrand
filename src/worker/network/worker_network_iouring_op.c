8f03a0318d1d2b51de795bb09520465fbc6b13ba
Daniele Salvatore Albano <d.albano@gmail.com>
2023-04-22 11:13:42 +0200

    Ensure that the networking code mark the request to enqueue an fd to the map as a network channel

--- src/worker/network/worker_network_iouring_op.c
+++ src/worker/network/worker_network_iouring_op.c
@@ -216,6 +216,7 @@ network_channel_t* worker_network_iouring_op_network_accept_setup_new_channel(
     if (unlikely(!worker_iouring_fds_map_add_and_enqueue_files_update(
             worker_iouring_context_get()->ring,
             new_channel->fd,
+            WORKER_FDS_MAP_FILES_FD_TYPE_NETWORK_CHANNEL,
             &new_channel->has_mapped_fd,
             &new_channel->base_sqe_flags,
             &new_channel->wrapped_channel.fd))) {
