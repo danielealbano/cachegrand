0f0737c259a74346c85636b511676e4e50ac8453
Daniele Salvatore Albano <d.albano@gmail.com>
2023-04-28 22:15:05 +0200

    Drop inline support from the benchmarks

--- benches/bench-hashtable-mpmc-old-op-get.cpp
+++ benches/bench-hashtable-mpmc-old-op-get.cpp
@@ -75,66 +75,6 @@ static void hashtable_op_get_not_found_key(benchmark::State& state) {
     }
 }
 
-#if HASHTABLE_FLAG_ALLOW_KEY_INLINE == 1
-static void hashtable_op_get_single_key_inline(benchmark::State& state) {
-    static hashtable_t* hashtable;
-    static hashtable_bucket_index_t bucket_index;
-    static hashtable_chunk_index_t chunk_index;
-    static hashtable_chunk_slot_index_t chunk_slot_index;
-    hashtable_value_data_t value;
-    bool result;
-    char error_message[150] = {0};
-    worker_context_t worker_context = { 0 };
-
-    worker_context.worker_index = state.thread_index();
-    worker_context_set(&worker_context);
-    transaction_set_worker_index(worker_context.worker_index);
-
-    if (state.thread_index() == 0) {
-        hashtable = test_support_init_hashtable(state.range(0));
-
-        bucket_index = test_key_1_hash % hashtable->ht_current->buckets_count;
-        chunk_index = HASHTABLE_TO_CHUNK_INDEX(bucket_index);
-        chunk_slot_index = 0;
-
-        HASHTABLE_SET_KEY_INLINE_BY_INDEX(
-                chunk_index,
-                chunk_slot_index,
-                test_key_1_hash,
-                test_key_1,
-                test_key_1_len,
-                test_value_1);
-    }
-
-    test_support_set_thread_affinity(state.thread_index());
-
-    for (auto _ : state) {
-        benchmark::DoNotOptimize((result = hashtable_mcmp_op_get(
-                hashtable,
-                test_key_1,
-                test_key_1_len,
-                &value)));
-
-        if (!result) {
-            sprintf(
-                    error_message,
-                    "Unable to get the key <%s> with bucket index <%lu>, chunk index <%lu> and chunk slot index <%u> for the thread <%d>",
-                    test_key_1,
-                    bucket_index,
-                    chunk_index,
-                    chunk_slot_index,
-                    state.thread_index());
-            state.SkipWithError(error_message);
-            break;
-        }
-    }
-
-    if (state.thread_index() == 0) {
-        hashtable_mcmp_free(hashtable);
-    }
-}
-#endif
-
 static void hashtable_op_get_single_key_external(benchmark::State& state) {
     static hashtable_t* hashtable;
     static hashtable_bucket_index_t bucket_index;
@@ -158,7 +98,7 @@ static void hashtable_op_get_single_key_external(benchmark::State& state) {
         char *test_key_1_clone = (char*)xalloc_alloc(test_key_1_len + 1);
         strncpy(test_key_1_clone, test_key_1, test_key_1_len);
 
-        HASHTABLE_SET_KEY_EXTERNAL_BY_INDEX(
+        HASHTABLE_SET_KEY_DB_0_BY_INDEX(
                 chunk_index,
                 chunk_slot_index,
                 test_key_1_hash,
@@ -205,11 +145,6 @@ static void BenchArguments(benchmark::internal::Benchmark* b) {
 
 BENCHMARK(hashtable_op_get_not_found_key)
         ->Apply(BenchArguments);
-
-#if HASHTABLE_FLAG_ALLOW_KEY_INLINE == 1
-BENCHMARK(hashtable_op_get_single_key_inline)
-        ->Apply(BenchArguments);
-#endif
 
 BENCHMARK(hashtable_op_get_single_key_external)
         ->Apply(BenchArguments);
