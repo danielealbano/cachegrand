8633600bbc9734119f6a356e0da0d8c913da554a
Daniele Salvatore Albano <d.albano@gmail.com>
2023-04-28 22:27:02 +0200

    Implement the redis command SELECT to change database

---
+++ src/module/redis/command/module_redis_command_select.c
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2018-2023 Daniele Salvatore Albano
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ **/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#include "misc.h"
+#include "exttypes.h"
+#include "clock.h"
+#include "spinlock.h"
+#include "transaction.h"
+#include "transaction_spinlock.h"
+#include "data_structures/ring_bounded_queue_spsc/ring_bounded_queue_spsc_voidptr.h"
+#include "data_structures/double_linked_list/double_linked_list.h"
+#include "data_structures/slots_bitmap_mpmc/slots_bitmap_mpmc.h"
+#include "data_structures/hashtable/mcmp/hashtable.h"
+#include "data_structures/hashtable/spsc/hashtable_spsc.h"
+#include "protocol/redis/protocol_redis.h"
+#include "protocol/redis/protocol_redis_reader.h"
+#include "protocol/redis/protocol_redis_writer.h"
+#include "module/module.h"
+#include "network/io/network_io_common.h"
+#include "config.h"
+#include "network/channel/network_channel.h"
+#include "storage/io/storage_io_common.h"
+#include "storage/channel/storage_channel.h"
+#include "storage/db/storage_db.h"
+#include "module/redis/module_redis.h"
+#include "module/redis/module_redis_connection.h"
+
+#define TAG "module_redis_command_select"
+
+MODULE_REDIS_COMMAND_FUNCPTR_COMMAND_END(select) {
+    module_redis_command_select_context_t *context = connection_context->command.context;
+
+    if (context->index.value > connection_context->db->config->max_user_databases) {
+        return module_redis_connection_error_message_printf_noncritical(
+                connection_context,
+                "ERR invalid DB index");
+    }
+
+    connection_context->database_number = context->index.value;
+
+    return module_redis_connection_send_ok(connection_context);
+}
