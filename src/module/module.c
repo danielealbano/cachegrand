13ec08ac40aae8b947ca09e04069d1319793c275
Daniele Salvatore Albano <d.albano@gmail.com>
2023-04-30 13:38:09 +0200

    Initial built-in module implementation

--- src/module/module.c
+++ src/module/module.c
@@ -6,6 +6,45 @@
  * of the BSD license.  See the LICENSE file for details.
  **/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
 #include "misc.h"
+#include "config.h"
+#include "xalloc.h"
+#include "data_structures/double_linked_list/double_linked_list.h"
+#include "network/io/network_io_common.h"
+#include "network/channel/network_channel.h"
+#include "network/network.h"
 
 #include "module.h"
+
+module_t *modules_registered_list = NULL;
+uint32_t modules_registered_list_size = 0;
+
+module_id_t module_register(
+        const char *name,
+        const char *config_section_name,
+        module_config_validate_after_load_t *config_validate_after_load,
+        module_connection_accept_t *connection_accept) {
+    modules_registered_list_size++;
+    modules_registered_list = xalloc_realloc(
+            modules_registered_list,
+            sizeof(module_t) * modules_registered_list_size);
+
+    modules_registered_list[modules_registered_list_size - 1] = (module_t) {
+            .id = modules_registered_list_size - 1,
+            .name = name,
+            .config_type_name = config_section_name,
+            .config_validate_after_load = config_validate_after_load,
+            .connection_accept = connection_accept,
+    };
+
+    return modules_registered_list_size - 1;
+}
--- src/module/module.h
+++ src/module/module.h
@@ -5,21 +5,41 @@
 extern "C" {
 #endif
 
-enum module_types {
-    MODULE_TYPE_UNKNOWN = 0,
-    MODULE_TYPE_REDIS,
-    MODULE_TYPE_PROMETHEUS
-};
-typedef enum module_types module_types_t;
+typedef struct config_module config_module_t;
+typedef struct network_channel network_channel_t;
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wwrite-strings"
-static char *module_types_text[] = {
-        "Unknown",
-        "Redis",
-        "Prometheus",
+typedef uint32_t module_id_t;
+typedef bool (module_config_validate_after_load_t)(
+        config_module_t *module);
+typedef void (module_connection_accept_t)(
+        network_channel_t *network_channel);
+
+struct module {
+    module_id_t id;
+    const char *name;
+    const char *config_type_name;
+    module_config_validate_after_load_t *config_validate_after_load;
+    module_connection_accept_t *connection_accept;
 };
-#pragma GCC diagnostic pop
+typedef struct module module_t;
+
+extern module_t *modules_registered_list;
+extern uint32_t modules_registered_list_size;
+
+static inline __attribute__((always_inline)) module_t* module_get_by_id(
+        module_id_t module_id) {
+    if (module_id >= modules_registered_list_size) {
+        return NULL;
+    }
+
+    return &modules_registered_list[module_id];
+}
+
+module_id_t module_register(
+        const char *name,
+        const char *config_section_name,
+        module_config_validate_after_load_t *config_validate_after_load,
+        module_connection_accept_t *connection_accept);
 
 #ifdef __cplusplus
 }
