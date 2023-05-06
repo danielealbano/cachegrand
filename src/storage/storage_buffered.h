93ecef6084ae1d3953958af9243da7e10380dbd6
Daniele Salvatore Albano <d.albano@gmail.com>
2023-05-06 11:56:52 +0200

    Move the hot path code to the header for inilining

--- src/storage/storage_buffered.h
+++ src/storage/storage_buffered.h
@@ -17,14 +17,6 @@ void storage_buffered_set_offset(
 bool storage_buffered_flush_write(
         storage_buffered_channel_t *storage_buffered_channel);
 
-storage_buffered_channel_buffer_data_t *storage_buffered_write_buffer_acquire_slice(
-        storage_buffered_channel_t *storage_buffered_channel,
-        size_t slice_length);
-
-void storage_buffered_write_buffer_release_slice(
-        storage_buffered_channel_t *storage_buffered_channel,
-        size_t slice_used_length);
-
 size_t storage_buffered_read_buffer_acquire_slice(
         storage_buffered_channel_t *storage_buffered_channel,
         size_t slice_length,
@@ -32,6 +24,45 @@ size_t storage_buffered_read_buffer_acquire_slice(
 
 void storage_buffered_read_buffer_release_slice(
         storage_buffered_channel_t *storage_buffered_channel);
+
+static inline __attribute__((always_inline)) storage_buffered_channel_buffer_data_t *storage_buffered_write_buffer_acquire_slice(
+        storage_buffered_channel_t *storage_buffered_channel,
+        size_t slice_length) {
+    // Ensure that the slice requested can fit into the buffer and that there isn't already a slice acquired
+    assert(slice_length <= storage_buffered_channel->buffers.write.buffer.length);
+    assert(storage_buffered_channel->buffers.write.slice_acquired_length == 0);
+
+    // Check if there is enough space on the buffer, if not flush it
+    if (unlikely(storage_buffered_channel->buffers.write.buffer.data_size + slice_length >
+                 storage_buffered_channel->buffers.write.buffer.length)) {
+        if (unlikely(!storage_buffered_flush_write(storage_buffered_channel))) {
+            return NULL;
+        }
+    }
+
+#if DEBUG == 1
+    storage_buffered_channel->buffers.write.slice_acquired_length = slice_length;
+#endif
+
+    return storage_buffered_channel->buffers.write.buffer.data +
+           storage_buffered_channel->buffers.write.buffer.data_offset;
+}
+
+static inline __attribute__((always_inline))void storage_buffered_write_buffer_release_slice(
+        storage_buffered_channel_t *storage_buffered_channel,
+        size_t slice_used_length) {
+    // Ensure that when the slice is released, the amount of data used is always the same or smaller than the length
+    // acquired. Also ensure that there was a slice acquired.
+    assert(storage_buffered_channel->buffers.write.slice_acquired_length > 0);
+    assert(slice_used_length <= storage_buffered_channel->buffers.write.slice_acquired_length);
+
+    storage_buffered_channel->buffers.write.buffer.data_size += slice_used_length;
+    storage_buffered_channel->buffers.write.buffer.data_offset += slice_used_length;
+
+#if DEBUG == 1
+    storage_buffered_channel->buffers.write.slice_acquired_length = 0;
+#endif
+}
 
 #ifdef __cplusplus
 }
