97b792eb742ad5da3cb3e6d61b730ac4ef461f15
Daniele Salvatore Albano <d.albano@gmail.com>
2023-05-06 13:51:23 +0200

    Split out the storage buffered read ahead code and move the hot code into the header to be inlined

--- src/storage/storage_buffered.h
+++ src/storage/storage_buffered.h
@@ -14,16 +14,57 @@ void storage_buffered_set_offset(
         storage_buffered_channel_t *storage_buffered_channel,
         off_t offset);
 
+bool storage_buffered_read_ahead(
+        storage_buffered_channel_t *storage_buffered_channel,
+        size_t data_size_to_read);
+
 bool storage_buffered_flush_write(
         storage_buffered_channel_t *storage_buffered_channel);
 
-size_t storage_buffered_read_buffer_acquire_slice(
+static inline __attribute__((always_inline)) size_t storage_buffered_read_buffer_acquire_slice(
         storage_buffered_channel_t *storage_buffered_channel,
         size_t slice_length,
-        storage_buffered_channel_buffer_data_t **buffer_out);
+        storage_buffered_channel_buffer_data_t **buffer_out) {
+    assert(storage_buffered_channel->buffers.read.slice_acquired_length > 0);
+    assert(slice_length <= storage_buffered_channel->buffers.read.slice_acquired_length);
 
-void storage_buffered_read_buffer_release_slice(
-        storage_buffered_channel_t *storage_buffered_channel);
+    *buffer_out = NULL;
+
+    size_t data_offset = storage_buffered_channel->buffers.read.buffer.data_offset;
+    size_t data_available = storage_buffered_channel->buffers.read.buffer.data_size - data_offset;
+
+    // Check if more data are needed
+    if (unlikely(slice_length > data_available)) {
+        storage_buffered_read_ahead(
+                storage_buffered_channel,
+                slice_length - data_available);
+
+        data_available = storage_buffered_channel->buffers.read.buffer.data_size;
+        data_offset = 0;
+    }
+
+    // Calculate the slice length that can actually be acquired
+    slice_length = MIN(data_available, slice_length);
+
+    // Set the buffer output
+    *buffer_out = storage_buffered_channel->buffers.read.buffer.data + data_offset;
+
+    // Update the data offset
+    storage_buffered_channel->buffers.read.buffer.data_offset += slice_length;
+
+#if DEBUG == 1
+    storage_buffered_channel->buffers.read.slice_acquired_length = slice_length;
+#endif
+
+    return slice_length;
+}
+
+static inline __attribute__((always_inline)) void storage_buffered_read_buffer_release_slice(
+        storage_buffered_channel_t *storage_buffered_channel) {
+#if DEBUG == 1
+    storage_buffered_channel->buffers.read.slice_acquired_length = 0;
+#endif
+}
 
 static inline __attribute__((always_inline)) storage_buffered_channel_buffer_data_t *storage_buffered_write_buffer_acquire_slice(
         storage_buffered_channel_t *storage_buffered_channel,
