c9122880332275d0b66ed164c5905284dab171f1
Daniele Salvatore Albano <d.albano@gmail.com>
2023-05-27 22:47:58 +0200

    Implement testing for the disabled commands

---
+++ tests/unit_tests/modules/redis/command/test-modules-redis-command-disabled.cpp
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2018-2023 Daniele Salvatore Albano
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ **/
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <cstdbool>
+#include <cstring>
+#include <memory>
+#include <string>
+
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+
+#include "clock.h"
+#include "exttypes.h"
+#include "spinlock.h"
+#include "transaction.h"
+#include "transaction_spinlock.h"
+#include "data_structures/ring_bounded_queue_spsc/ring_bounded_queue_spsc_voidptr.h"
+#include "data_structures/ring_bounded_queue_spsc/ring_bounded_queue_spsc_uint128.h"
+#include "data_structures/double_linked_list/double_linked_list.h"
+#include "data_structures/slots_bitmap_mpmc/slots_bitmap_mpmc.h"
+#include "data_structures/hashtable/mcmp/hashtable.h"
+#include "data_structures/hashtable/spsc/hashtable_spsc.h"
+#include "config.h"
+#include "fiber/fiber.h"
+#include "worker/worker_stats.h"
+#include "worker/worker_context.h"
+#include "signal_handler_thread.h"
+#include "storage/io/storage_io_common.h"
+#include "storage/channel/storage_channel.h"
+#include "storage/db/storage_db.h"
+#include "epoch_gc.h"
+#include "epoch_gc_worker.h"
+
+#include "program.h"
+
+#include "test-modules-redis-command-fixture.hpp"
+
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+
+class TestModulesRedisCommandDisabledFixture: public TestModulesRedisCommandFixture {
+public:
+    TestModulesRedisCommandDisabledFixture() :
+        TestModulesRedisCommandFixture(std::vector<char*>{ "GET", "UNLINK" }) {
+    }
+};
+
+TEST_CASE_METHOD(TestModulesRedisCommandDisabledFixture, "Redis - command - disabled", "[redis][command][disabled]") {
+    SECTION("Disabled command") {
+        REQUIRE(send_recv_resp_command_text_and_validate_recv(
+                std::vector<std::string>{"SET", "a_key", "a_value"},
+                "+OK\r\n"));
+
+        REQUIRE(send_recv_resp_command_text_and_validate_recv(
+                std::vector<std::string>{"GET", "a_key"},
+                "-ERR command `GET` is disabled\r\n"));
+
+        REQUIRE(send_recv_resp_command_text_and_validate_recv(
+                std::vector<std::string>{"MGET", "a_key"},
+                "*1\r\n$7\r\na_value\r\n"));
+
+        REQUIRE(send_recv_resp_command_text_and_validate_recv(
+                std::vector<std::string>{"UNLINK", "a_key"},
+                "-ERR command `UNLINK` is disabled\r\n"));
+
+        REQUIRE(send_recv_resp_command_text_and_validate_recv(
+                std::vector<std::string>{"DEL", "a_key"},
+                ":1\r\n"));
+    }
+}
