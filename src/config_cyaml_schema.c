8652031b019c664be1b0a789dadb9f4c6a2f81a2
Daniele Salvatore Albano <d.albano@gmail.com>
2023-05-27 11:25:44 +0200

    Implement a new configuration option to disable commands

--- src/config_cyaml_schema.c
+++ src/config_cyaml_schema.c
@@ -113,6 +113,10 @@ const cyaml_schema_field_t config_module_redis_schema[] = {
         CYAML_FIELD_STRING_PTR(
                 "password", CYAML_FLAG_DEFAULT | CYAML_FLAG_OPTIONAL,
                 config_module_redis_t, password, 0, CYAML_UNLIMITED),
+        CYAML_FIELD_SEQUENCE(
+                "disabled_commands", CYAML_FLAG_POINTER | CYAML_FLAG_OPTIONAL,
+                config_module_redis_t, disabled_commands,
+                &config_generic_string_schema, 0, CYAML_UNLIMITED),
         CYAML_FIELD_END
 };
 
